b724c234bf6e5dae3726dd33de1d940fa4220ab4
Andrew Lee <andrew.lee@netl.doe.gov>
2018-10-23 11:47:06 -0400

    Adding stuff into core __init__.py

---
+++ idaes/core/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import #disable implicit relative imports
+from .process_base import ProcessBlockData, declare_process_block_class
+from .process_block import ProcessBlock
