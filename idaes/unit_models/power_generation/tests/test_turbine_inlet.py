3715a30e3a44815b0e71d7741ac0de4761fa4be6
eslickj <john.eslick@netl.doe.gov>
2019-02-06 13:12:49 -0500

    Adding turbine inlet model

--- idaes/unit_models/power_generation/tests/test_turbine_inlet.py
+++ idaes/unit_models/power_generation/tests/test_turbine_inlet.py
@@ -15,15 +15,70 @@ Tests for 0D heat exchanger models.
 
 Author: John Eslick
 """
+import pytest
 
+from pyomo.environ import ConcreteModel, SolverFactory, TransformationFactory
+
+from idaes.core import FlowsheetBlock
 from idaes.unit_models.power_generation import TurbineInletStage
 from idaes.property_models import iapws95_ph
 from idaes.ui.report import degrees_of_freedom
 
+# See if ipopt is available and set up solver
+if SolverFactory('ipopt').available():
+    solver = SolverFactory('ipopt')
+    solver.options = {'tol': 1e-6}
+else:
+    solver = None
+
 @pytest.fixture()
-def build_heater():
+def build_turbine():
     m = ConcreteModel()
     m.fs = FlowsheetBlock(default={"dynamic": False})
     m.fs.properties = iapws95_ph.Iapws95ParameterBlock()
-    m.fs.heater = TurbineInletStage(default={"property_package": m.fs.properties})
+    m.fs.turb = TurbineInletStage(default={"property_package": m.fs.properties})
     return m
+
+@pytest.fixture()
+def build_turbine_dyn():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": True})
+    m.fs.properties = iapws95_ph.Iapws95ParameterBlock()
+    m.fs.turb = TurbineInletStage(default={
+        "dynamic": False,
+        "property_package": m.fs.properties})
+    return m
+
+def test_basic_build(build_turbine):
+    """Make a turbine model and make sure it doesn't trhow exception"""
+    m = build_turbine
+
+@pytest.mark.skipif(solver is None, reason="Solver not available")
+def test_initialize(build_turbine):
+    """Initialize a turbine model"""
+    m = build_turbine
+    hin = iapws95_ph.htpx(T=880, P=2.4233e7)
+    # set inlet
+    m.fs.turb.inlet[:].enth_mol.value = hin
+    m.fs.turb.inlet[:].flow_mol.value = 26000/4.0
+    m.fs.turb.inlet[:].pressure.value = 2.4233e7
+
+    m.fs.turb.initialize(outlvl=3)
+    assert(degrees_of_freedom(m)==3) #inlet was't fixed and still shouldn't be
+
+@pytest.mark.skipif(solver is None, reason="Solver not available")
+def test_initialize_dyn(build_turbine_dyn):
+    """Initialize a turbine model"""
+    m = build_turbine_dyn
+    hin = iapws95_ph.htpx(T=880, P=2.4233e7)
+    """
+    discretizer = TransformationFactory('dae.finite_difference')
+    discretizer.apply_to(m, nfe=4, wrt=m.fs.time, scheme='BACKWARD')
+
+    # fix inlet
+    m.fs.turb.inlet[:].enth_mol.fix(hin)
+    m.fs.turb.inlet[:].flow_mol.fix(26000/4.0)
+    m.fs.turb.inlet[:].pressure.fix(2.4233e7)
+
+    m.fs.turb.initialize(outlvl=4)
+    """
--- idaes/unit_models/power_generation/turbine_inlet.py
+++ idaes/unit_models/power_generation/turbine_inlet.py
@@ -17,17 +17,19 @@ Liese, (2014). "Modeling of a Steam Turbine Including Partial Arc Admission
     for Use in a Process Simulation Software Environment." Journal of Engineering
     for Gas Turbines and Power. v136, November
 """
-from __future__ import division
-
 __Author__ = "John Eslick"
 
 import logging
 _log = logging.getLogger(__name__)
 
 from pyomo.common.config import In
-from pyomo.environ import Var, Expression, Constraint, sqrt
+from pyomo.environ import Var, Expression, Constraint, sqrt, SolverFactory, value
+from pyomo.opt import TerminationCondition
+
 from idaes.core import declare_process_block_class
 from idaes.unit_models.pressure_changer import PressureChangerData
+from idaes.core.util import from_json, to_json, StoreSpec
+from idaes.ui.report import degrees_of_freedom
 
 @declare_process_block_class("TurbineInletStage")
 class TurbineInletStageData(PressureChangerData):
@@ -40,12 +42,12 @@ class TurbineInletStageData(PressureChangerData):
     CONFIG.thermodynamic_assumption = 'isentropic'
     CONFIG.get('thermodynamic_assumption')._default = 'isentropic'
     CONFIG.get('thermodynamic_assumption')._domain = In(['isentropic'])
-    def buid(self):
+    def build(self):
         super(TurbineInletStageData, self).build()
 
-        self.flow_coeff = Var(self.time, initialize=1.053/3600.0,
+        self.flow_coeff = Var(self.time_ref, initialize=1.053/3600.0,
             doc="Turbine flow coefficient [kg*C^0.5/Pa/s]")
-        self.delta_enth_isentropic = Var(self.time, initialize=2.2e5,
+        self.delta_enth_isentropic = Var(self.time_ref, initialize=-1000,
             doc="Specific enthalpy change of isentropic process [J/mol/K]")
         self.blade_reaction = Var(initialize=0.9,
             doc="Blade reaction parameter")
@@ -54,23 +56,24 @@ class TurbineInletStageData(PressureChangerData):
         self.eff_nozzle = Var(initialize=0.95, bounds=(0.0, 1.0),
             doc="Nozzel efficiency (typically 0.90 to 0.95)")
         self.eff_nozzle.fix()
-        self.blade_reaction()
+        self.blade_reaction.fix()
         self.flow_coeff.fix()
         self.blade_velocity.fix()
+        self.control_volume.deltaP[:] = -1000
 
-        @self.Expression(self.time,
+        @self.Expression(self.time_ref,
             doc="Entering steam velocity calculation [m/s]")
         def steam_entering_velocity(b, t):
             # 1.414 = 44.72/sqrt(1000) for SI if comparing to Liese (2014)
-            # b.delta_enth_isentropic[t] = hin - hiesn, the mw converts enthalpy
-            # to a mass basis
-            return (1.414*sqrt((1-b.blade_reaction)*(b.delta_enth_isentropic[t]))/
-                    sqrt(b.holdup.properties_in[t].mw))
+            # b.delta_enth_isentropic[t] = -(hin - hiesn), the mw converts
+            # enthalpy to a mass basis
+            return 1.414*sqrt(-(1-b.blade_reaction)*b.delta_enth_isentropic[t]/
+                    b.control_volume.properties_in[t].mw*self.eff_nozzle)
 
-        @self.Constraint(self.time, doc="Turbine inlet flow constraint")
+        @self.Constraint(self.time_ref, doc="Equation: Turbine inlet flow")
         def inlet_flow_constraint(b, t):
             # Some local vars to make the euqation more readable
-            cp_ratio = b.control_volume.properties_in[t].heat_capacity_ratio
+            g = b.control_volume.properties_in[t].heat_capacity_ratio
             mw = b.control_volume.properties_in[t].mw
             flow = b.control_volume.properties_in[t].flow_mol
             Tin = b.control_volume.properties_in[t].temperature
@@ -78,17 +81,86 @@ class TurbineInletStageData(PressureChangerData):
             Pin = b.control_volume.properties_in[t].pressure
             Pratio = b.ratioP[t]
             return (flow*mw*sqrt(Tin - 273.15) ==
-                Cf*Pin*sqrt(g/(g - 1)*(Pratio**(2/g) - Pratio**((g + 1)/g))))
+                cf*Pin*sqrt(g/(g - 1)*(Pratio**(2.0/g) - Pratio**((g + 1)/g))))
 
-        @self.Constraint(self.time, doc="Equation: Isentropic enthalpy change")
+        @self.Constraint(self.time_ref, doc="Equation: Isentropic enthalpy change")
         def isentropic_enthalpy(b, t):
-            return b.work_isentropic[t] == (-b.delta_enth_isentropic[t]*
-                b.holdup.properties_in[t].flow_mol)
+            return b.work_isentropic[t] == (b.delta_enth_isentropic[t]*
+                b.control_volume.properties_in[t].flow_mol)
 
-        @self.Constraint(self.time, doc="Equation: Efficiency")
+        @self.Constraint(self.time_ref, doc="Equation: Efficiency")
         def efficiency_correlation(b, t):
             Vr = b.blade_velocity/b.steam_entering_velocity[t]
             eff = b.efficiency_isentropic[t]
             R = b.blade_reaction
             return eff == 2*Vr*((sqrt(1 - R) - Vr) +
                                  sqrt((sqrt(1 - R) - Vr)**2 + R))
+
+    def initialize(self, state_args={}, outlvl=0, solver='ipopt',
+        optarg={'tol': 1e-6}):
+        """
+        Initialize the inlet turbine stage model.  This deactivates the
+        specialized constraints, then does the isentropic turbine initialization,
+        the reactivates the constraints and solves.
+
+        Args:
+            state_args (dict): Initial state for property initialization
+            outlvl (int): Amount of output (0 to 3) 0 is lowest
+            solver (str): Solver to use for initialization
+            optarg (dict): Solver arguments dictionary
+        """
+        stee = True if outlvl >= 3 else False
+        # sp is What to save the make sure state after init is same as start
+        #   saves value, fixed, and active state, doesn't load originally free
+        #   values, this makes sure original problem spec is same but initalizes
+        #   the values of free vars
+        sp = StoreSpec.value_isfixed_isactive(only_fixed=True)
+        istate = to_json(self, return_dict=True, wts=sp)
+        # Deactivate special constraints
+        self.inlet_flow_constraint.deactivate()
+        self.isentropic_enthalpy.deactivate()
+        self.efficiency_correlation.deactivate()
+
+        # Fix turbine parameters + eff_isen
+        self.efficiency_isentropic.fix(0.8)
+        self.eff_nozzle.fix()
+        self.blade_reaction()
+        self.flow_coeff.fix()
+        self.blade_velocity.fix()
+
+        # fix inlet and free outlet
+        for t in self.time_ref:
+            for k, v in self.inlet[t].vars.items():
+                v.fix()
+            for k, v in self.outlet[t].vars.items():
+                v.unfix()
+            self.outlet[t].pressure.fix(value(self.inlet[t].pressure)*0.8)
+        dof = degrees_of_freedom(self)
+        try:
+            assert(dof == 0)
+        except:
+            _log.exception("degrees_of_freedom = {}".format(dof))
+            raise
+
+        super(TurbineInletStageData, self).initialize(state_args=state_args,
+            outlvl=outlvl, solver=solver, optarg=optarg)
+
+        # Free eff_isen and activate sepcial constarints
+        self.efficiency_isentropic.unfix()
+        self.outlet[:].pressure.unfix()
+        self.inlet_flow_constraint.activate()
+        self.isentropic_enthalpy.activate()
+        self.efficiency_correlation.activate()
+
+        slvr = SolverFactory(solver)
+        slvr.options = optarg
+        res = slvr.solve(self, tee=stee)
+
+        if outlvl > 0:
+            if res.solver.termination_condition == TerminationCondition.optimal:
+                _log.info('{} Initialization Complete.'.format(self.name))
+            else:
+                _log.warning('{} Initialization Failed.'.format(self.name))
+
+        # reload original spec
+        from_json(self, sd=istate, wts=sp)
