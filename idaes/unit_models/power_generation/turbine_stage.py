01e5ae6a1f4ba38d04d57422f97118f09ff1966f
eslickj <john.eslick@netl.doe.gov>
2019-02-12 15:00:17 -0500

    Minor reference edit

--- idaes/unit_models/power_generation/turbine_stage.py
+++ idaes/unit_models/power_generation/turbine_stage.py
@@ -17,7 +17,7 @@ between the inlet and outlet. This model is based on:
 
 Liese, (2014). "Modeling of a Steam Turbine Including Partial Arc Admission
     for Use in a Process Simulation Software Environment." Journal of Engineering
-    for Gas Turbines and Power. v136, November
+    for Gas Turbines and Power. v136.
 """
 from __future__ import division
 
