64540fa1a1feda1e836d4a2e7f442dcaff5ff622
John Eslick <jeslick@AD.SCILAN.PGH>
2019-05-03 08:00:58 -0400

    Add mass flow to meta data and fix MW cap.

--- idaes/property_models/iapws95/iapws95_wrap_ph.py
+++ idaes/property_models/iapws95/iapws95_wrap_ph.py
@@ -226,6 +226,7 @@ class Iapws95ParameterBlockData(PhysicalParameterBlock):
             'mw': {'method': None, 'units': 'kg/mol'},
             'temperature_sat': {'method': 'None', 'units': 'K'},
             'flow_mol': {'method': None, 'units': 'mol/s'},
+            'flow_mass': {'method': None, 'units': 'kg/s'},
             'temperature': {'method': None, 'units': 'K'},
             'pressure': {'method': None, 'units': 'Pa'},
             'vapor_frac': {'method': None, 'units': None},
@@ -400,7 +401,7 @@ class Iapws95StateBlockData(StateBlockData):
         mw = self.mw
         mw.latex_symbol = "M"
 
-        self.flow_mass = Expression(expr=self.MW*self.flow_mol,
+        self.flow_mass = Expression(expr=self.mw*self.flow_mol,
             doc="mass flow rate [kg/s]")
 
         self.enth_mass = Expression(expr = self.enth_mol/mw,
