d1e7f397e56b67bab4d977e0c2ba89f464003dd7
John Eslick <jeslick@AD.SCILAN.PGH>
2019-06-17 14:53:34 -0400

    Fix import again.

--- idaes/property_models/iapws95/__init__.py
+++ idaes/property_models/iapws95/__init__.py
@@ -4,4 +4,4 @@ def iapws95_available():
     plib = os.path.join(os.path.dirname(__file__), "iapws95.so")
     return os.path.isfile(plib)
 
-from idaes.property_models.iapws.iapws95 import *
+from idaes.property_models.iapws95.iapws95 import *
