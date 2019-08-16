55850bb9005a5436b6149c0cbf564c5bb6f34bac
Makayla Shepherd <mrshepherd@lbl.gov>
2019-08-16 14:49:56 -0700

    Renamed link position mapping to be consistent with icon mapping

---
+++ idaes/dmf/ui/link_position_mapping.py
@@ -0,0 +1,8 @@
+link_position_mapping = {
+    "mixer": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "outlet_anchors":{"name":"right","args":{"rotate":"false","padding":0}}},
+    "heater": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "outlet_anchors":{"name":"right","args":{"rotate":"false","padding":0}}},
+    "stoichiometric_reactor": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "outlet_anchors":{"name":"right","args":{"rotate":"false","padding":0}}},
+    "flash": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "vapor_outlet_anchor":{"name":"top","args":{"rotate":"false","padding":0}}, "liquid_outlet_anchor":{"name":"bottom","args":{"rotate":"false","padding":0}}},
+    "separator": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "outlet_anchors":{"name":"right","args":{"rotate":"false","padding":0}}},
+    "pressure_changer": {"inlet_anchors":{"name":"left","args":{"rotate":"false","padding":0}}, "outlet_anchors":{"name":"right","args":{"rotate":"false","padding":0}}}
+}
