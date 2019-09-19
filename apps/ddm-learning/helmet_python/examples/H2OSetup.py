e73b192b83646ef82ffbe07ea0a23f1c1b1b0883
mengleCMU <mengle@andrew.cmu.edu>
2019-09-19 11:08:13 -0400

    H2OSetup example small change

--- apps/ddm-learning/helmet_python/examples/H2OSetup.py
+++ apps/ddm-learning/helmet_python/examples/H2OSetup.py
@@ -1,15 +1,4 @@
-
-import sys
 import os
-import numpy as np
-from sys import argv
-import math
-from pylab import *
-import matplotlib.pyplot as plt
-import time
-
-
-import helmet
 import helmet.Helmet as Helmet
 
 # Push information for the molecule
@@ -19,17 +8,10 @@ num_terms = 12
 max_time = 1000
 constraints = 0
 
-Fluids = {'TOL': (591.75, 4.126, 3.169, 92.13842, 178.0, 0.2657),
-		  'CO': (132.86, 3.494, 10.85, 28.0101, 68.16, 0.0497),
-		  'H2O': (647.096, 22.064, 17.8737279956, 18.015268, 273.16, 0.344 ), 
-		  'CO2': (304.1282, 7.3773, 10.62497, 44.0095, 215.692, 0.228)}
-
-
+Fluids = {'H2O': (647.096, 22.064, 17.8737279956, 18.015268, 273.16, 0.344 )}
 
 molecule = 'H2O'
 (critT, critP, critD, M, triple, acc) = Fluids[molecule]
-Zcrit = critP*1000/critT/critD -1
-# print(Zcrit)
 R = 8.314472; # J mol^-1 K^-1 
 
 # Constants for a molecule 
@@ -39,13 +21,15 @@ Helmet.initialize(molecule=molecule, fluid_data = Fluids[molecule], filename = o
 Helmet.prepareAncillaryEquations(plot = True)  # plot=True
 
 # View data used for regression
-#Helmet.viewPropertyData()
+Helmet.viewPropertyData()
 
 # Write GAMS Gdx data file and regression file
 Helmet.setupRegression(numTerms = 18, gams=True)
-#Helmet.runRegression(gams=True)
+
+# runs the GAMS gdx and regression file
+# Helmet.runRegression(gams=True)
 			 	
 
 # View Results by importing the data
-Helmet.viewResults("H2Omain.lst", plot=True)
+#Helmet.viewResults("H2Omain.lst", plot=True)
 
