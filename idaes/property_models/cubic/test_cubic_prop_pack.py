d1d7b3931c7ee24d0e3b0527492873c1c459e109
Andrew Lee <andrew.lee@netl.doe.gov>
2019-09-25 17:31:51 -0400

    GEtting tests wokring again and adding more

--- idaes/property_models/cubic/test_cubic_prop_pack.py
+++ idaes/property_models/cubic/test_cubic_prop_pack.py
@@ -40,8 +40,8 @@ def test_T_sweep():
         m.fs.obj.deactivate()
 
         m.fs.state.flow_mol.fix(100)
-        m.fs.state.mole_frac["benzene"].fix(0.5)
-        m.fs.state.mole_frac["toluene"].fix(0.5)
+        m.fs.state.mole_frac_comp["benzene"].fix(0.5)
+        m.fs.state.mole_frac_comp["toluene"].fix(0.5)
         m.fs.state.temperature.fix(300)
         m.fs.state.pressure.fix(10**(0.5*logP))
 
@@ -72,8 +72,8 @@ def test_P_sweep():
 
     for T in range(370, 500, 25):
         m.fs.state.flow_mol.fix(100)
-        m.fs.state.mole_frac["benzene"].fix(0.5)
-        m.fs.state.mole_frac["toluene"].fix(0.5)
+        m.fs.state.mole_frac_comp["benzene"].fix(0.5)
+        m.fs.state.mole_frac_comp["toluene"].fix(0.5)
         m.fs.state.temperature.fix(T)
         m.fs.state.pressure.fix(1e5)
 
@@ -107,10 +107,14 @@ def test_T350_P1_x5():
                      'defined_state': True})
 
     m.fs.state.flow_mol.fix(100)
-    m.fs.state.mole_frac["benzene"].fix(0.5)
-    m.fs.state.mole_frac["toluene"].fix(0.5)
+    m.fs.state.mole_frac_comp["benzene"].fix(0.5)
+    m.fs.state.mole_frac_comp["toluene"].fix(0.5)
     m.fs.state.temperature.fix(350)
     m.fs.state.pressure.fix(1e5)
+
+    # Trigger build of enthalpy and entropy
+    m.fs.state.enth_mol_phase
+    m.fs.state.entr_mol_phase
 
     m.fs.state.initialize(outlvl=0)
 
@@ -118,6 +122,109 @@ def test_T350_P1_x5():
     solver.solve(m)
 
     assert pytest.approx(value(m.fs.state._teq), abs=1e-1) == 365
-    assert pytest.approx(value(m.fs.state.compress_fact["Liq"]), 1e-5) == 0.0035346
-    assert pytest.approx(value(m.fs.state.compress_fact["Vap"]), 1e-5) == 0.966749
-    assert pytest.approx(value(m.fs.state.fug_coeff_phase["Liq"]), 1e-5) == 0.0035346
+    assert pytest.approx(
+            value(m.fs.state.compress_fact_phase["Liq"]), 1e-5) == 0.0035346
+    assert pytest.approx(
+            value(m.fs.state.compress_fact_phase["Vap"]), 1e-5) == 0.966749
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Liq", "benzene"]),
+            1e-5) == 0.894676
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Liq", "toluene"]),
+            1e-5) == 0.347566
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Vap", "benzene"]),
+            1e-5) == 0.971072
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Vap", "toluene"]),
+            1e-5) == 0.959791
+
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Liq", "benzene"]),
+            1e-5) == 0.5
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Liq", "toluene"]),
+            1e-5) == 0.5
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Vap", "benzene"]),
+            1e-5) == 0.70584
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Vap", "toluene"]),
+            1e-5) == 0.29416
+
+    assert pytest.approx(
+            value(m.fs.state.enth_mol_phase["Liq"]), 1e-5) == 38942.8
+    assert pytest.approx(
+            value(m.fs.state.enth_mol_phase["Vap"]), 1e-5) == 78048.7
+    assert pytest.approx(
+            value(m.fs.state.entr_mol_phase["Liq"]), 1e-5) == -367.558
+    assert pytest.approx(
+            value(m.fs.state.entr_mol_phase["Vap"]), 1e-5) == -269.0553
+
+
+def test_T350_P5_x5():
+    m = ConcreteModel()
+
+    m.fs = FlowsheetBlock(default={'dynamic': False})
+
+    m.fs.props = BT_PR.BTParameterBlock(
+            default={'valid_phase': ('Vap', 'Liq')})
+
+    m.fs.state = m.fs.props.state_block_class(
+            default={'parameters': m.fs.props,
+                     'defined_state': True})
+
+    m.fs.state.flow_mol.fix(100)
+    m.fs.state.mole_frac_comp["benzene"].fix(0.5)
+    m.fs.state.mole_frac_comp["toluene"].fix(0.5)
+    m.fs.state.temperature.fix(350)
+    m.fs.state.pressure.fix(5e5)
+
+    # Trigger build of enthalpy and entropy
+    m.fs.state.enth_mol_phase
+    m.fs.state.entr_mol_phase
+
+    m.fs.state.initialize(outlvl=0)
+
+    solver = SolverFactory('ipopt')
+    solver.solve(m)
+
+    assert pytest.approx(value(m.fs.state._teq), 1e-5) == 431.47
+    assert pytest.approx(
+            value(m.fs.state.compress_fact_phase["Liq"]), 1e-5) == 0.01766
+    assert pytest.approx(
+            value(m.fs.state.compress_fact_phase["Vap"]), 1e-5) == 0.80245
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Liq", "benzene"]),
+            1e-5) == 0.181229
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Liq", "toluene"]),
+            1e-5) == 0.070601
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Vap", "benzene"]),
+            1e-5) == 0.856523
+    assert pytest.approx(
+            value(m.fs.state.fug_coeff_phase_comp["Vap", "toluene"]),
+            1e-5) == 0.799237
+
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Liq", "benzene"]),
+            1e-5) == 0.5
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Liq", "toluene"]),
+            1e-5) == 0.5
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Vap", "benzene"]),
+            1e-5) == 0.65415
+    assert pytest.approx(
+            value(m.fs.state.mole_frac_phase_comp["Vap", "toluene"]),
+            1e-5) == 0.34585
+
+    assert pytest.approx(
+            value(m.fs.state.enth_mol_phase["Liq"]), 1e-5) == 38966.9
+    assert pytest.approx(
+            value(m.fs.state.enth_mol_phase["Vap"]), 1e-5) == 75150.7
+    assert pytest.approx(
+            value(m.fs.state.entr_mol_phase["Liq"]), 1e-5) == -367.6064
+    assert pytest.approx(
+            value(m.fs.state.entr_mol_phase["Vap"]), 1e-5) == -287.3318
