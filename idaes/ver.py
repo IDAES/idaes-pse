805fd9985e0677c76647a9e8e917d888c56e4a9a
Keith Beattie <KSBeattie@lbl.gov>
2019-09-27 17:32:51 -0700

    1.3.0rc1 release

--- idaes/ver.py
+++ idaes/ver.py
@@ -192,7 +192,7 @@ except NameError:  # eg, if invoked from setup.py
     pass
 
 #: Package's version as an object
-package_version = Version(1, 4, 0, "development", 0, gh)
+package_version = Version(1, 3, 0, "candidate", 1, gh)
 
 #: Package's version as a simple string
 __version__ = str(package_version)
