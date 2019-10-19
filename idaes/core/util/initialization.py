d93ae33dbc9f460d1fb7758b0f8f75240eef5a24
Andrew Lee <andrew.lee@netl.doe.gov>
2019-10-19 12:53:33 +1000

    Adding methods to fix and revert state variables

--- idaes/core/util/initialization.py
+++ idaes/core/util/initialization.py
@@ -18,7 +18,82 @@ This module contains utility functions for initialization of IDAES models.
 from pyomo.environ import Block, Var
 from pyomo.network import Arc
 
+from idaes.core.util.exceptions import ConfigurationError
+
 __author__ = "Andrew Lee, John Siirola"
+
+
+def fix_state_vars(blk, state_args={}):
+    """
+    Method for fixing state variables within StateBlocks. Method takes an
+    optional argument of values to use when fixing variables.
+
+    Args:
+        blk : An IDAES StateBlock object in which to fix the state variables
+        state_args : a dict containing values to use when fixing state
+                variables. Keys must match with names used in the
+                define_state_vars method, and indices of any variables must
+                agree.
+
+    Returns:
+        A dict keyed by block index, state variable name (as defined by
+        define_state_variables) and variable index indicating the fixed status
+        of each variable before the fix_state_vars method was applied.
+    """
+    flags = {}
+    for k in blk.keys():
+        for n, v in blk[k].define_state_vars().items():
+            for i in v:
+                flags[k, n, i] = v[i].is_fixed()
+
+                # If not fixed, fix at either guess provided or current value
+                if not v[i].is_fixed():
+                    if n in state_args:
+                        # Try to get initial guess from state_args
+                        try:
+                            if i is None:
+                                val = state_args[n]
+                            else:
+                                val = state_args[n][i]
+                        except KeyError:
+                            raise ConfigurationError(
+                                'Indexes in state_args did not agree with '
+                                'those of state variable {}. Please ensure '
+                                'that indexes for initial guesses are correct.'
+                                .format(n))
+                        v[i].fix(val)
+                    else:
+                        # No guess, use current value
+                        v[i].fix()
+
+    return flags
+
+
+def revert_state_vars(blk, flags):
+    """
+    Method to revert the fixed state of the state variables within an IDAES
+    StateBlock based on a set of flags of the previous state.
+
+    Args:
+        blk : an IDAES StateBlock
+        flags : a dict of bools indicating previous state with keys in the form
+                (StateBlock index, state variable name (as defined by
+                define_state_vars), var indices).
+
+    Returns:
+        None
+    """
+    for k in blk.keys():
+        for n, v in blk[k].define_state_vars().items():
+            for i in v:
+                try:
+                    if not flags[k, n, i]:
+                        v[i].unfix()
+                except KeyError:
+                    raise ConfigurationError(
+                        'Indices of flags proved do not match with indices of'
+                        'the StateBlock. Please make sure you are using the '
+                        'correct StateBlock.')
 
 
 def propagate_state(stream, direction="forward"):
