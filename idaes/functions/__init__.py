e294a927650408b685c211383dd57a49ab7f3ed6
John Eslick <john.eslick@netl.doe.gov>
2019-11-07 17:19:25 -0500

    Removing c code

--- idaes/functions/__init__.py
+++ idaes/functions/__init__.py
@@ -1,8 +1,7 @@
-from pyomo.common.fileutils import this_file_dir as _this_file_dir
-
 def functions_lib():
     import os
-    plib = os.path.join(_this_file_dir(), "functions.so")
+    import idaes
+    plib = os.path.join(idaes.bin_directory, "functions.so")
     return plib
 
 def functions_available():
