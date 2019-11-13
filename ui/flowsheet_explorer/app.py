4d8aff5173bd2b56e8dad8421f839f18bfb9b5e3
Perren Yang <44816826+perrenyang@users.noreply.github.com>
2019-11-13 10:47:00 -0800

    Flowsheet explorer: Add zoom functionality (#491)
    * Barebones flowsheet serializer. Needs more functionality and to be prettied up
    * Added date as a header
    * Kind of working slider value label
    * Commiting because it kind of works and I don't want to lose this when I blast over it
    * Working prototype
    * Flowsheet explorer: working zoom and scrolling
    * Flowsheet explorer: friendlier date/time display format
    Changing the YYYY-MM-DD to a wordier format is easily doable as well, of
    course.
    * Added arrows for more fine changes
    * Moved flowsheet explorer to a directory that makes more sense
    * Removed unused library
    * Removed print statements
    * Added preliminary README
    * Renamed README
    * Stylistic tweaks to README
    * Added title, changed the arrows, fixed the slider hover offset, and changed it to start on the left
    * Addressing PR comments. Added step numbers and labels to images.
    * Removed redundant requirements
    * Added file pattern recognition for finding the svgs, added cacheing so the files are only retrieved once, added comments.
    * Flowsheet Explorer: Merge changes from master.
    * Fixed a bug with the back button going back by two and the forward button not working

--- ui/flowsheet_explorer/app.py
+++ ui/flowsheet_explorer/app.py
@@ -30,16 +30,14 @@ def index():
         if not slider_val:
             slider_val = 0
         else:
-            slider_val = int(slider_val) - 1
+            slider_val = int(slider_val)
 
     if file_count != 0:
         date = os.path.splitext(cached_files[slider_val])[0]
+        day, hour = date.split("_")
+        date = day + " " + hour + ":00"
         return render_template("index.html", date=date, slider_value=slider_val, range=file_count, raw_img="/static/raw/" + cached_files[slider_val], modeled_img="/static/modeled/" + cached_files[slider_val], slider_labels=json.dumps(cached_files_dict))
 
 
 if __name__ == '__main__':
     app.run()
-
-
-
-    # return render_template("index.html", start_time=start_time, end_time=end_time)
