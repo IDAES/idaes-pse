9302ee7859346bd43342451c7f6dd49c79195e47
John Eslick <john.eslick@netl.doe.gov>
2019-11-18 17:57:14 -0500

    Move a bunch of config stuff out of __init__.py.  Will clean more later want to add a config utility.

--- idaes/__init__.py
+++ idaes/__init__.py
@@ -8,63 +8,14 @@ import logging.config
 import importlib
 import toml
 import pyomo.common.plugin
-import pyomo.common.config
+import idaes.config
 
 from .ver import __version__  # noqa
 
 _log = logging.getLogger(__name__)
 
 # Create the general IDAES configuration block
-_config = pyomo.common.config.ConfigBlock("idaes", implicit=False)
-_config.declare(
-    "logging",
-    pyomo.common.config.ConfigBlock(
-        implicit=True,
-        description="Logging configuration dictionary",
-        doc="This stores the logging configuration. See the Python "
-        "logging.config.dictConfig() documentation for details.",
-    ),
-)
-_config.declare(
-    "plugins",
-    pyomo.common.config.ConfigBlock(
-        implicit=False,
-        description="Plugin search configuration",
-        doc="Plugin search configuration",
-    ),
-)
-_config.plugins.declare(
-    "required",
-    pyomo.common.config.ConfigValue(
-        default=[],
-        description="Modules with required plugins",
-        doc="This is a string list of modules from which to load plugins. "
-        "This will look in {module}.plugins for things to load. Exceptions"
-        "raised while attempting to load these plugins are considered fatal. "
-        "This is used for core plugins.",
-    ),
-)
-_config.plugins.declare(
-    "optional",
-    pyomo.common.config.ConfigValue(
-        default=[],
-        description="Modules with optional plugins to load",
-        doc="This is a string list of modules from which to load plugins. "
-        "This will look in {module}.plugins for things to load. Exceptions "
-        "raised while attempting to load these plugins will be logged but "
-        "are nonfatal. This is used for contrib plugins.",
-    ),
-)
-
-_config.declare(
-    "use_idaes_solvers",
-    pyomo.common.config.ConfigValue(
-        default=True,
-        description="Add the IDAES bin directory to the path.",
-        doc="Add the IDAES bin directory to the path such that solvers provided "
-        "by IDAES will be used in preference to previously installed solvers.",
-    ),
-)
+_config = config.new_idaes_config_block()
 
 # Standard locations for config file, binary libraries and executables, ...
 try:
@@ -114,32 +65,6 @@ def _create_lib_dir():
 # something that requires them.  For now the commandline utility commends will
 # cause the directories to be made.
 
-def _read_config(config):
-    """Read either a TOML formatted config file or a configuration dictionary.
-    Args:
-        config: A config file path or dict
-    Returns:
-        None
-    """
-    config_file = None
-    if config is None:
-        return
-    elif isinstance(config, dict):
-        pass  # don't worry this catches ConfigBlock too it seems
-    else:
-        config_file = config
-        try:
-            with open(config_file, "r") as f:
-                config = toml.load(f)
-        except IOError:  # don't require config file
-            _log.debug("Config file {} not found (this is okay)".format(config))
-            return
-    _config.set_value(config)
-    logging.config.dictConfig(_config["logging"])
-    if config_file is not None:
-        _log.debug("Read config {}".format(config_file))
-
-
 def _import_packages(packages, optional=True):
     """Import plugin package, condensed from pyomo.environ.__init__.py
     Args:
@@ -159,32 +84,9 @@ def _import_packages(packages, optional=True):
         if hasattr(pkg, 'load'):  # run load function for a module if it exists
             pkg.load()
 
-
 # Set default configuration.  Used TOML string to serve as an example for
 # and definitive guide for IDAES configuration files.
-_read_config(
-    toml.loads(
-        """
-use_idaes_solvers = true
-[plugins]
-  required = []
-  optional = []
-[logging]
-  version = 1
-  disable_existing_loggers = false
-  [logging.formatters.f1]
-    format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
-    datefmt = "%Y-%m-%d %H:%M:%S"
-  [logging.handlers.console]
-    class = "logging.StreamHandler"
-    formatter = "f1"
-    stream = "ext://sys.stdout"
-  [logging.loggers.idaes]
-    level = "INFO"
-    handlers = ["console"]
-"""
-    )
-)
+config.read_config(toml.loads(config.default_config), _config)
 
 # Try to read the global IDAES config file.
 # Set where to look for config files
@@ -192,11 +94,11 @@ _global_config_file = os.path.join(data_directory, "idaes.conf")
 _local_config_file = "idaes.conf"
 
 # Try to read global config then local
-_read_config(_global_config_file)
-_read_config(_local_config_file)
+config.read_config(_global_config_file, _config)
+config.read_config(_local_config_file, _config)
 _log.debug("'idaes' logger debug test")
 
-if _config["use_idaes_solvers"]:
+if _config.use_idaes_solvers:
     # Add IDAES stuff to the path unless you configure otherwise
     os.environ['PATH'] = os.pathsep.join([bin_directory, os.environ['PATH']])
     if os.name == 'nt':  # Windows (this is to find MinGW libs)
