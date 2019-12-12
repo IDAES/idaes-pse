5d917637f56b74ee7b0f99b755a959797ab79b42
Andrew Lee <andrew.lee@netl.doe.gov>
2019-12-12 16:17:23 -0500

    Adding basic test for availablity of IPOPT

---
+++ idaes/tests/test_ipopt.py
@@ -0,0 +1,26 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+from pyomo.environ import SolverFactory
+
+
+def test_ipopt_available():
+    """
+    Tries to set-up the IPOPT and returns exception if not available
+    """
+    try:
+        SolverFactory('ipopt')
+    except:
+        raise Exception(
+            "Could not find IPOPT. Users are strongly encouraged to have a "
+            "version of IPOPT available ,as it is the deault solver assuemd "
+            "by many IDAES examples and tests.")
