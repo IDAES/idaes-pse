db67af176a9b9aecf7daac3485637ebcb24e61d1
Dan Gunter <dkgunter@lbl.gov>
2019-12-20 08:09:26 -0800

    get-examples command is working

--- idaes/commands/examples.py
+++ idaes/commands/examples.py
@@ -3,6 +3,7 @@ Command to get IDAES examples
 """
 # stdlib
 import logging
+from operator import itemgetter
 import os
 from pathlib import Path
 import sys
@@ -20,13 +21,21 @@ from idaes.ver import package_version as V
 _log = logging.getLogger("idaes.commands")
 
 
+GITHUB = "https://github.com"
+GITHUB_API = "https://api.github.com"
 REPO_ORG = "idaes"
 REPO_NAME = "examples-pse"
 REPO_DIR = "src"
-VERSION = f"{V.major}.{V.minor}.{V.micro}"
+PKG_VERSION = f"{V.major}.{V.minor}.{V.micro}"
+
+# XXX: probably don't *really* want pre-releases, but they are
+# XXX: handy for testing this script.
+_skip_prereleases = False
 
 
 class DownloadError(Exception):
+    """Used for errors downloading the release files.
+    """
     pass
 
 
@@ -37,12 +46,44 @@ class DownloadError(Exception):
     "--dir", "-d", "directory", help="installation target directory", default="examples"
 )
 @click.option(
+    "--list-releases",
+    "-l",
+    help="List all available released versions, and stop",
+    flag_value="list",
+)
+@click.option(
     "--version",
     "-V",
-    help=f"Version of examples to download (default={VERSION})",
-    default=VERSION,
+    help=f"Version of examples to download (default={PKG_VERSION})",
+    default=PKG_VERSION,
 )
-def get_examples(directory, version):
+def get_examples(directory, list_releases, version):
+    """Get the examples from Github and put them in a local directory.
+    """
+    # check version
+    releases = get_releases()
+    if list_releases == "list":
+        print_releases(releases)
+        sys.exit(0)
+    matched_release = None
+    for rel in releases:
+        if version == rel[1]:  # matches tag
+            matched_release = rel
+            break
+    if matched_release is None:
+        # modify message slightly depending on whether they selected a version
+        if version == PKG_VERSION:
+            how = "installed"
+            how_ver = " and -V/--version to choose a desired version"
+        else:
+            how = "selected"
+            how_ver = ""  # they already did this!
+        click.echo(
+            f"No release found matching {how} IDAES package version '{version}'."
+        )
+        click.echo(f"Use -l/--list-releases to see all{how_ver}.")
+        sys.exit(-1)
+    # check target directory
     target_dir = Path(directory)
     if target_dir.exists():
         _log.error("target directory {target_dir} already exists")
@@ -57,7 +98,11 @@ def get_examples(directory, version):
 
 
 def download_contents(version, target_dir):
-    """Download Github contents.
+    """Download the given version from the Github releases and make
+    its `REPO_DIR` subdirectory be the `target_dir`.
+
+    Raises:
+        DownloadError: if the GET on the release URL returns non-200 status
     """
     url = archive_file_url(version)
     _log.info(f"get examples from: {url}")
@@ -68,6 +113,7 @@ def download_contents(version, target_dir):
             raise DownloadError(f"file not found")
         raise DownloadError(f"status={req.status_code}")
     tmpdir = TemporaryDirectory()
+    _log.debug(f"created temporary directory '{tmpdir.name}'")
     tmpfile = Path(tmpdir.name) / "examples.zip"
     with tmpfile.open("wb") as f:
         for chunk in req.iter_content(chunk_size=65536):
@@ -81,8 +127,54 @@ def download_contents(version, target_dir):
     subdir = Path(tmpdir.name) / f"{REPO_NAME}-{version}" / REPO_DIR
     _log.debug(f"move {subdir} -> {target_dir}")
     os.rename(str(subdir), str(target_dir))
+    _log.debug(f"removing temporary directory '{tmpdir.name}'")
     del tmpdir
 
 
 def archive_file_url(version, org=REPO_ORG, repo=REPO_NAME):
-    return f"https://github.com/{org}/{repo}/archive/{version}.zip"
+    """Build & return URL for a given release version.
+    """
+    return f"{GITHUB}/{org}/{repo}/archive/{version}.zip"
+
+
+def get_releases():
+    """Returns a list of releases, with a tuple for each of (date, tag, info).
+    The list is sorted in ascending order by date.
+    """
+    releases = []
+    url = f"{GITHUB_API}/repos/{REPO_ORG}/{REPO_NAME}/releases"
+    req = requests.get(url)
+    for rel in req.json():
+        if _skip_prereleases and rel["prerelease"]:
+            continue
+        releases.append((rel["published_at"], rel["tag_name"], rel["name"]))
+    releases.sort(key=itemgetter(0))  # sort by publication date
+    return releases
+
+
+def print_releases(releases):
+    """Print the releases, as returned by `get_releases()`, as a table
+    to standard output.
+    """
+    if len(releases) == 0:
+        print("No releases to list")
+        return
+    # determine column widths
+    widths = [4, 7, 7]  # widths of column titles: date,version,details
+    widths[0] = len(releases[0][0])  # dates are all the same
+    # tags and names can have different widths
+    for rel in releases:
+        for i in range(1, 3):
+            widths[i] = max(widths[i], len(rel[i]))
+    # make row format
+    pad = "  "
+    fmt = f"{{date:{widths[0]}s}}{pad}{{tag:{widths[1]}s}}{pad}{{name:{widths[2]}s}}"
+    # print header
+    print("")
+    print(fmt.format(date="Date", tag="Version", name="Details"))
+    print(fmt.format(date="-" * widths[0], tag="-" * widths[1], name="-" * widths[2]))
+    # print rows
+    for rel in releases:
+        print(fmt.format(date=rel[0], tag=rel[1], name=rel[2]))
+    # print footer
+    print("")
