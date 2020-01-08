981069cfd42130ec8c712b616c39590653499ede
Andrew Lee <andrew.lee@netl.doe.gov>
2020-01-08 15:38:07 -0500

    Adding constants module to idaes.core.util and documenting

---
+++ idaes/core/util/constants.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+This module contains common constants of use in process systems engineering.
+
+All units are SI
+"""
+
+__author__ = "Andrew Lee"
+
+from math import pi
+
+accel_gravity = 9.80665  # m⋅s^-2
+
+avogadro_number = 6.02214076e23  # unitless or mol^-1
+
+boltzmann_const = 1.380649e-23  # J⋅K^-1
+
+elemental_charge = 1.602176634e-19  # C
+
+faraday_const = 96485.33212  # C⋅mol^-1
+
+gas_const = 8.31446261815324  # J⋅mol^-1⋅K^-1
+
+gravitational_const = 6.67430e-11  # m^3⋅kg^−1⋅s^−2
+
+mass_electron = 9.1093837015e-31  # kg
+
+pi = pi
+
+planck_const = 6.62607015e-34  # J⋅s
+
+stefan_const = 5.670374419e-8  # W⋅m^−2⋅K^−4
+
+speed_light = 299792458  # in a vacuum m⋅s^-1
