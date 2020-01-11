56b7739990f4bbcacaf50cef1779163950f3ee98
OOAmusat <47539353+OOAmusat@users.noreply.github.com>
2020-01-10 18:14:54 -0800

    Added copyright statement to all files
    - Modified annotate_source - added f.close() [ script failing without it)
    - Added copyright statement

--- surrogates/examples/polynomial_regression_example_02.py
+++ surrogates/examples/polynomial_regression_example_02.py
@@ -1,3 +1,15 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
 from pyomo.contrib.polynomial_regression import *
 from pyomo.contrib.sampling import sampling as sp
 from pyomo.common.fileutils import PYOMO_ROOT_DIR
