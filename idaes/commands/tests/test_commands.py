aacb892bb1016916b0cf29f1f7d3c3df9daa606b
Dan Gunter <dkgunter@lbl.gov>
2020-01-12 15:26:47 -0800

    test bug

--- idaes/commands/tests/test_commands.py
+++ idaes/commands/tests/test_commands.py
@@ -1,3 +1,15 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
 """
 Tests for idaes.commands
 """
@@ -20,7 +32,7 @@ from idaes.commands import examples
 def test_examples_list_releases():
     releases = examples.get_releases(True)
     assert len(releases) > 0
-    examples.print_releases(releases)
+    examples.print_releases(releases, True)
 
 
 def test_examples_download_bad_version():
