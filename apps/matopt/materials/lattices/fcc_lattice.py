70332020d8f8eb46da6f8ee0d6d7d593be2d5d14
Christopher Hanselman <christopher.hanselman@lilly.com>
2020-01-28 17:56:23 -0500

    Addressing comments on fcc_lattice.py

--- apps/matopt/materials/lattices/fcc_lattice.py
+++ apps/matopt/materials/lattices/fcc_lattice.py
@@ -35,7 +35,6 @@ class FCCLattice(UnitCellLattice):
                                      np.array([0.0, 0.5, -0.5]),
                                      np.array([0.5, 0.5, 0.0])]
         self.applyTransF(ScaleFunc(IAD / FCCLattice.RefIAD))
-        # import code; code.interact(local=dict(locals(),**globals()));
         assert (self.isConsistentWithDesign())
 
     # === CONSTRUCTOR - Aligned with FCC {100}
@@ -75,22 +74,18 @@ class FCCLattice(UnitCellLattice):
         UnitCellLattice.applyTransF(self, TransF)
 
     # === PROPERTY EVALUATION METHODS
+    # NOTE: inherited from UnitCellLattice
     # def isOnLattice(self,P):
 
     def areNeighbors(self, P1, P2):
-        # return distEucPoint2Point(P1,P2) <= self.IAD
         return np.linalg.norm(P2 - P1) <= self.IAD
 
     def getNeighbors(self, P):
         RefP = self._getConvertToReference(P)
         result = deepcopy(self._RefNeighborsPattern)
-        # print(result)
         for NeighP in result:
             NeighP += RefP
             self._convertFromReference(NeighP)
-        # print
-        # print(result)
-        # import code; code.interact(local=dict(locals(),**globals()));
         return result
 
     # === BASIC QUERY METHODS
@@ -109,49 +104,3 @@ class FCCLattice(UnitCellLattice):
     @property
     def FCC110LayerSpacing(self):
         return self.IAD * sqrt(2) / 2
-
-
-def getPossibleRotAndRefls111():
-    result = []
-    thetaX = -np.pi * 0.25
-    thetaY = -np.arctan2(-sqrt(2), 2)
-    thetaZ = np.pi * 0.5
-    S = Cuboctahedron(1)
-    ThreeFoldAxis1 = S.FacetDirections[6]
-    ThreeFoldAxis2 = S.FacetDirections[7]
-    ThreeFoldAxis3 = S.FacetDirections[8]
-    ThreeFoldAxis4 = S.FacetDirections[9]
-    FourFoldAxis1 = S.FacetDirections[0]
-    FourFoldAxis2 = S.FacetDirections[1]
-    FourFoldAxis3 = S.FacetDirections[2]
-
-    Rot1ThreeFold1 = RotateFunc.fromAxisAngle(ThreeFoldAxis1, np.pi * 2.0 / 3.0)
-    Rot2ThreeFold1 = RotateFunc.fromAxisAngle(ThreeFoldAxis1, np.pi * 4.0 / 3.0)
-    Rot1ThreeFold2 = RotateFunc.fromAxisAngle(ThreeFoldAxis2, np.pi * 2.0 / 3.0)
-    Rot2ThreeFold2 = RotateFunc.fromAxisAngle(ThreeFoldAxis2, np.pi * 4.0 / 3.0)
-    Rot1ThreeFold3 = RotateFunc.fromAxisAngle(ThreeFoldAxis3, np.pi * 2.0 / 3.0)
-    Rot2ThreeFold3 = RotateFunc.fromAxisAngle(ThreeFoldAxis3, np.pi * 4.0 / 3.0)
-    Rot1ThreeFold4 = RotateFunc.fromAxisAngle(ThreeFoldAxis4, np.pi * 2.0 / 3.0)
-    Rot2ThreeFold4 = RotateFunc.fromAxisAngle(ThreeFoldAxis4, np.pi * 4.0 / 3.0)
-
-    Rot1FourFold1 = RotateFunc.fromAxisAngle(FourFoldAxis1, np.pi * 1.0 / 2.0)
-    Rot2FourFold1 = RotateFunc.fromAxisAngle(FourFoldAxis1, np.pi * 2.0 / 2.0)
-    Rot3FourFold1 = RotateFunc.fromAxisAngle(FourFoldAxis1, np.pi * 3.0 / 2.0)
-    Rot1FourFold2 = RotateFunc.fromAxisAngle(FourFoldAxis2, np.pi * 1.0 / 2.0)
-    Rot2FourFold2 = RotateFunc.fromAxisAngle(FourFoldAxis2, np.pi * 2.0 / 2.0)
-    Rot3FourFold2 = RotateFunc.fromAxisAngle(FourFoldAxis2, np.pi * 3.0 / 2.0)
-    Rot1FourFold3 = RotateFunc.fromAxisAngle(FourFoldAxis3, np.pi * 1.0 / 2.0)
-    Rot2FourFold3 = RotateFunc.fromAxisAngle(FourFoldAxis3, np.pi * 2.0 / 2.0)
-    Rot3FourFold3 = RotateFunc.fromAxisAngle(FourFoldAxis3, np.pi * 3.0 / 2.0)
-
-    ReflX = ReflectFunc.acrossX()
-    ReflY = ReflectFunc.acrossY()
-    ReflZ = ReflectFunc.acrossZ()
-    ReflXY = ReflectFunc(np.array([sqrt(2), sqrt(2), 0]), np.array([0, 0, 0], dtype=float))
-    ReflXZ = ReflectFunc(np.array([sqrt(2), 0, sqrt(2)]), np.array([0, 0, 0], dtype=float))
-    ReflYZ = ReflectFunc(np.array([0, sqrt(2), sqrt(2)]), np.array([0, 0, 0], dtype=float))
-    ReflXYneg = ReflectFunc(np.array([-sqrt(2), sqrt(2), 0]), np.array([0, 0, 0], dtype=float))
-    ReflXZneg = ReflectFunc(np.array([-sqrt(2), 0, sqrt(2)]), np.array([0, 0, 0], dtype=float))
-    ReflYZneg = ReflectFunc(np.array([0, -sqrt(2), sqrt(2)]), np.array([0, 0, 0], dtype=float))
-
-    raise NotImplementedError('TODO')
