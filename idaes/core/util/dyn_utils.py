8cffd1e39f2719c02b942b3af705428187bedc00
Robert <robbybparker@gmail.com>
2020-02-11 22:27:22 -0500

    Function for integrating flowsheet and module for helper functions

---
+++ idaes/core/util/dyn_utils.py
@@ -0,0 +1,255 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+This module contains utility functions for dynamic IDAES models.
+"""
+
+from pyomo.environ import Block, Constraint, Var
+from pyomo.dae import ContinuousSet, DerivativeVar
+from pyomo.dae.set_utils import (is_explicitly_indexed_by,
+        is_implicitly_indexed_by, get_index_set_except)
+
+from idaes.core import FlowsheetBlock
+from idaes.core.util.model_statistics import ComponentSet
+from idaes.core.util.exceptions import ConfigurationError
+import pdb
+
+__author__ = "Robert Parker"
+
+
+def get_activity_dict(b):
+    """
+    args:
+        b: a block to be searched for active components
+    returns:
+        ad: a dictionary mapping id of constraints and blocks
+            to a bool indicating if they are fixed
+    """
+    ad = {}
+    for con in b.component_data_objects(Constraint):
+        if con.active:
+            ad[id(con)] = True
+        else:
+            ad[id(con)] = False
+    for blk in b.component_data_objects(Block):
+        if blk.active:
+            ad[id(blk)] = True
+        else:
+            ad[id(blk)] = false
+    return ad
+
+def deactivate_model_at(b, cset, pt):
+    """
+    Finds any block or constraint in block b, indexed explicitly or implicitly
+    by cset, and deactivates each instance at pt
+    Args:
+        b - Block to search
+        cset - ContinuousSet of interest
+        pt - value, in ContinuousSet, to deactivate at
+    Returns:
+        deactivated - a list of component data that have been deactivated
+    """
+    if not pt in cset:
+        msg = str(pt) + ' is not in ContinuousSet ' + cset.name
+        raise Exception('')
+    deactivated = []
+    
+    for block in b.component_objects(Block):
+        if is_explicitly_indexed_by(block, cset):
+            info = get_index_set_except(block, cset)
+            non_cset_set = info['set_except']
+            index_getter = info['index_getter']
+            for non_cset_index in non_cset_set:
+                index = index_getter(non_cset_index, pt)
+                try:
+                    block[index].deactivate()
+                    deactivated.append(block[index])
+                except KeyError:
+                    # TODO: use logger to give a warning here
+                    continue                
+
+    for con in b.component_objects(Constraint):
+        if (is_explicitly_indexed_by(con, cset) and
+                not is_implicitly_indexed_by(con, cset)):
+            info = get_index_set_except(con, cset)
+            non_cset_set = info['set_except']
+            index_getter = info['index_getter']
+            for non_cset_index in non_cset_set:
+                index = index_getter(non_cset_index, pt)
+                try:
+                    con[index].deactivate()
+                    deactivated.append(con[index])
+                except KeyError:
+                    # TODO: use logger to give a warning here
+                    continue
+                 
+    return deactivated
+
+def get_derivatives_at(b, time, t):
+    """
+    Finds derivatives with respect to time at point t.
+    No distinction made for multiple derivatives or mixed partials.
+    Args:
+        b - Block to search for derivatives
+        time - ContinuousSet to look for derivatives with respect to 
+        t - point at which to return derivatives
+    Returns:
+        dvlist - list of derivatives found
+    """
+    dvlist = [] 
+    for var in ComponentSet(b.component_objects(Var)):
+        if not isinstance(var, DerivativeVar):
+            continue
+        if time not in set(var.get_continuousset_list()):
+            continue
+        n = var.index_set().dimen
+        if n == 1:
+            dvlist.append(var[t])
+        elif n >= 2:
+            info = get_index_set_except(var, time)
+            non_time_set = info['set_except']
+            index_getter = info['index_getter']
+            for non_time_index in non_time_set:
+                index = index_getter(non_time_index, t)
+                dvlist.append(var[index])
+    return dvlist
+
+# TODO: should be able to replace this function everywhere
+#       with getname and find_component
+def path_from_block(comp, blk, include_comp=False):
+    """
+    Returns a list of tuples with (local_name, index) pairs required
+    to locate comp from blk
+    Args:
+        comp - Component(Data) to locate
+        blk - Block(Data) to locate comp from
+        include_comp - bool of whether or not to include the
+                       local_name, index of the component itself
+    Returns:
+        route - A list of string, index tuples
+    """
+    parent_data = comp.parent_block()
+    route = []
+    # Walk up the hierarchy tree until blk is reached
+    while parent_data != blk:
+        parent_obj = parent_data.parent_component()
+        # Record the local name and index required to locate the current block
+        # from the parent_component of its parent_block
+        # Pre-pend to the existing (name, index) list
+        route = [(parent_obj.local_name, parent_data.index())] + route
+        # If top-levelmodel has been reached, break
+        # (This should not happen)
+        if parent_data.parent_block() == None:
+            break
+        parent_data = parent_data.parent_block()
+
+    # Append comp's name and index to the list if desired:
+    if include_comp:
+        route.append((comp.parent_component().local_name, comp.index()))
+    return route
+
+def copy_values_at_time(fs_tgt, fs_src, t_target, t_source, copy_fixed=True):
+    """
+    For all variables in fs_tgt (implicitly or explicitly) indexed by time,
+    sets the value at t_target to that of the same variable in fs_src
+    at t_source.
+
+    Currently relies on fact that input blocks are flowsheets.
+    Could extend to apply to non-flowsheet blocks, but would need to
+    pass in the time set as well - could then apply to any set.
+
+    Args:
+        fs_tgt - target flowsheet
+        fs_scr - source flowsheet
+        t_target - target time point
+        t_source - source time point
+        copy_fixed - bool of whether or not to copy over 
+                     fixed variables in target model
+    """
+    time_target = fs_tgt.time
+    for var_target in ComponentSet(fs_tgt.component_objects(Var)):
+        if not is_explicitly_indexed_by(var_target, time_target):
+            continue
+        n = var_target.index_set().dimen
+
+        local_parent = fs_src
+        for r in path_from_block(var_target, fs_tgt):
+            local_parent = getattr(local_parent, r[0])[r[1]]
+        try:
+            var_source = getattr(local_parent, var_target.local_name)
+        except AttributeError:
+            # Log a warning
+            continue
+
+        if n == 1:
+            if not copy_fixed and var_target[t_target].fixed:
+                continue
+            var_target[t_target].set_value(var_source[t_source].value)
+        elif n >= 2:
+            index_info = get_index_set_except(var_target, time_target)
+            non_time_index_set = index_info['set_except']
+            index_getter = index_info['index_getter']
+            for non_time_index in non_time_index_set:
+                source_index = index_getter(non_time_index, t_source)
+                target_index = index_getter(non_time_index, t_target)
+                if not copy_fixed and var_target[target_index].fixed:
+                    continue
+                var_target[target_index].set_value(
+                        var_source[source_index].value)
+
+    for blk_target in ComponentSet(fs_tgt.component_objects(Block)):
+        if not is_explicitly_indexed_by(blk_target, time_target):
+            continue
+        n = blk_target.index_set().dimen
+
+        local_parent = fs_src
+        for r in path_from_block(blk_target, fs_tgt):
+            local_parent = getattr(local_parent, r[0])[r[1]]
+        blk_source = getattr(local_parent, blk_target.local_name)
+
+        if n == 1:
+            target_index = t_target
+            source_index = t_source
+            for var_target in ComponentSet(
+                    blk_target[target_index].component_data_objects(Var)):
+                if not copy_fixed and var_target.fixed:
+                    continue
+                local_parent = blk_source[source_index]
+                for r in path_from_block(var_target, blk_target[target_index]):
+                    local_parent = getattr(local_parent, r[0])[r[1]]
+                var_source = getattr(local_parent,
+                        var_target.parent_component().local_name)[
+                                var_target.index()]
+                var_target.set_value(var_source.value)
+
+        elif n >= 2:
+            index_info = get_index_set_except(blk_target, time_target)
+            non_time_index_set = index_info['set_except']
+            index_getter = index_info['index_getter']
+            for non_time_index in non_time_index_set:
+                source_index = index_getter(non_time_index, t_source)
+                target_index = index_getter(non_time_index, t_target)
+                for var_target in ComponentSet(
+                        blk_target[target_index].component_data_objects(Var)):
+                    if not copy_fixed and var_target.fixed:
+                        continue
+                    local_parent = blk_source[source_index]
+                    for r in path_from_block(var_target,
+                                             blk_target[target_index]):
+                        local_parent = getattr(local_parent, r[0])[r[1]]
+                    var_source = getattr(local_parent,
+                            var_target.parent_component().local_name)[
+                                    var_target.index()]
+                    var_target.set_value(var_source.value)
+
--- idaes/core/util/initialization.py
+++ idaes/core/util/initialization.py
@@ -17,10 +17,16 @@ This module contains utility functions for initialization of IDAES models.
 
 from pyomo.environ import Block, Var
 from pyomo.network import Arc
+from pyomo.dae import ContinuousSet
 
+from idaes.core import FlowsheetBlock
 from idaes.core.util.exceptions import ConfigurationError
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.core.util.dyn_utils import (get_activity_dict,
+        deactivate_model_at, get_derivatives_at, copy_values_at_time)
+import pdb
 
-__author__ = "Andrew Lee, John Siirola"
+__author__ = "Andrew Lee, John Siirola, Robert Parker"
 
 
 def fix_state_vars(blk, state_args={}):
@@ -204,3 +210,147 @@ def solve_indexed_blocks(solver, blocks, **kwds):
 
     # Return results
     return results
+
+def integrate_flowsheet(fs, time, **kwargs):
+    """
+    Function to integrate Flowsheet fs along ContinuousSet time.
+    Assumes sufficient initialization/correct degrees of freedom such that
+    the first finite element can be solved immediately and each subsequent
+    finite element can be solved by fixing differential/derivative variables
+    Args:
+        fs - flowsheet to integrate
+        time - set along which to integrate
+    Kwargs:
+        solver - Pyomo solver object initialized with user's desired options
+    """
+    if not isinstance(fs, FlowsheetBlock):
+        raise TypeError('First arg must be a FlowsheetBlock')
+    if not isinstance(time, ContinuousSet):
+        raise TypeError('Second arg must be a ContinuousSet')
+
+    if time.get_discretization_info() == {}:
+        # What type of error goes here?
+        raise Exception('ContinuousSet must be discretied')
+
+    scheme = time.get_discretization_info()['scheme']
+    fep_list = time.get_finite_elements()
+    nfe = time.get_discretization_info()['nfe']
+
+    # What types of exceptions go here?:
+    if scheme == 'LAGRANGE-RADAU':
+        ncp = time.get_discretization_info()['ncp']
+    elif scheme == 'LAGRANGE-LEGENDRE':
+        msg = 'Integrator does not support collocation with Legendre roots'
+        raise Exception(msg)
+    elif scheme == 'BACKWARD Difference':
+        ncp = 1
+    elif scheme == 'FORWARD Difference':
+        ncp = 1
+        msg = 'Forward integration (explicit Euler) has not yet been implemented'
+        raise NotImplementedError(msg)
+    elif scheme == 'CENTRAL Difference':
+        msg = 'Integrator does not support central finite difference'
+        raise Exception(msg)
+    # Disallow Central/Legendre discretizations.
+    # Neither of these seem to be square by default for multi-finite element
+    # initial value problems.
+
+    solver = kwargs.pop('solver', None)
+    tee = True # <- use logger.tee or something instead of this
+
+    ignore_dof = kwargs.pop('ignore_dof', False)
+    if degrees_of_freedom(fs) != 0 and not ignore_dof:
+        msg = ''' 
+              Flowsheet provided is not square. Use kwarg ignore_dof 
+              to proceed anyway.
+              '''
+        raise Exception(msg)
+    
+    # Get mask of constraints/blocks that are already inactive:
+    # dict: id(compdata) -> bool (is active?)
+    was_originally_active = get_activity_dict(fs)
+
+    # Deactivate flowsheet except at t0, solve to ensure consistency
+    # of initial conditions.
+    comps_at_t = {}
+    for t in time:
+        # This loop is slower than it needs to be
+        # Should get comps_at_t all in one component_data_objects
+        if t != time.first():
+            comps_at_t[t] = deactivate_model_at(fs, time, t)
+    # Somehow log that we are solving for initial conditions
+    solver.solve(fs, tee=tee)
+
+    comps_at_t[time.first()] = deactivate_model_at(fs, time, time.first())
+    # Now model is completely inactive
+
+    # For each timestep, we need to
+    # 1. Activate model at points we're solving for
+    # 2. Fix initial conditions (differential variables at previous timestep) 
+    #    of finite element
+    # 3. Solve the (now) square system
+    # 4. Revert the model to its prior state
+
+    # Perform a solve for 1 -> nfe; i is the index of the finite element
+    for i in range(1, nfe+1):
+        t_prev = time[(i-1)*ncp+1]
+        # Non-initial time points in the finite element:
+        fe = [time[k] for k in range((i-1)*ncp+2, i*ncp+2)]
+
+        # TODO: log some message
+
+        # Activate components of model that were active in the presumably
+        # square original system
+        for t in fe:
+            for comp in comps_at_t[t]:
+                if was_originally_active[id(comp)]:
+                    comp.activate()
+
+        init_deriv_list = get_derivatives_at(fs, time, t_prev)
+        init_dv_list = [d.parent_component().get_state_var()[d.index()]
+                        for d in init_deriv_list]
+
+        # Record original fixed status of each of these variables
+        was_originally_fixed = {}
+        for drv in init_deriv_list:
+            was_originally_fixed[id(drv)] = drv.fixed
+            if not drv.value is None:
+                drv.fix()
+            #if not drv.stale:
+            #    drv.fix()
+        for dv in init_dv_list:
+            was_originally_fixed[id(dv)] = dv.fixed
+            if not drv.value is None:
+                dv.fix()
+            #if not dv.stale:
+            #    dv.fix()
+
+        # Initialize finite element from its initial conditions
+        for t in fe:
+            copy_values_at_time(fs, fs, t, t_prev, copy_fixed=False)
+        # Log that we are solving finite element {i}
+        # TODO: raise exception if solve fails
+        solver.solve(fs, tee=tee)
+
+        # Deactivate components that may have been activated
+        for t in fe:
+            for comp in comps_at_t[t]:
+                comp.deactivate()
+
+        # Unfix variables that have been fixed
+        for drv in init_deriv_list:
+            if not was_originally_fixed[id(drv)]:
+                drv.unfix()
+        for dv in init_dv_list:
+            if not was_originally_fixed[id(dv)]:
+                dv.unfix()
+
+        # Log that integration step {i} has been finished
+
+    # Reactivate components of the model that were originally active
+    for t in time:
+        for comp in comps_at_t[t]:
+            if was_originally_active[id(comp)]:
+                comp.activate()
+
+    # Logger message that integration is finished
