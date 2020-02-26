cbc8599306fbf11deb300fa1a016c7fc3e4dc60e
robbybp <robbybparker@gmail.com>
2020-02-25 18:00:15 -0700

    empty module for cappresse/nmpc

---
+++ idaes/dynamic/cappresse/nmpc.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+Placeholder for Cappresse class definitions
+"""
+
+from pyomo.environ import Block, Constraint, Var
+from pyomo.dae import ContinuousSet, DerivativeVar
+
+from idaes.core import FlowsheetBlock
+from collections import Counter
+import idaes.logger as idaeslog
+
+__author__ = "Robert Parker and David Thierry"
+
+class NMPCSim():
+    def __init__():
+        pass
