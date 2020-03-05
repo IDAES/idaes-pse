9df151ddcdc17c36c787b60ca42d423ed55bd11e
Miguel Zamarripa <miguel.zamarripa-perez@netl.doe.gov>
2020-03-05 15:51:18 -0500

    pep8 standards

--- idaes/core/util/unit_costing.py
+++ idaes/core/util/unit_costing.py
@@ -19,100 +19,102 @@ from pyomo.environ import (Constraint,
                            exp,
                            log10, log)
 
+
 def global_costing_parameters(self, year=None):
     if year is None:
-        year='2018'
+        year = '2018'
     # Cost index $/year (method argument or 2018 default)
-    ce_index_dic = {'2019':680, '2018':671.1, '2017':567.5, '2016':541.7,
-                '2015':556.8, '2014':576.1, '2013':567.3, '2012':584.6,
-                '2011':585.7, '2010':550.8}
-    
-    self.CE_index = Param(mutable = True, initialize = ce_index_dic[year],
-                          doc= 'CE cost index $ year')
+    ce_index_dic = {'2019': 680, '2018': 671.1, '2017': 567.5, '2016': 541.7,
+                    '2015': 556.8, '2014': 576.1, '2013': 567.3, '2012': 584.6,
+                    '2011': 585.7, '2010': 550.8}
+
+    self.CE_index = Param(mutable=True, initialize=ce_index_dic[year],
+                          doc='CE cost index $ year')
+
 
 def _make_vars(self):
-    #build generic costing variables (all costing models need these vars)
-    self.base_cost = Var(initialize = 1e5,
+    # build generic costing variables (all costing models need these vars)
+    self.base_cost = Var(initialize=1e5,
                          bounds=(0, 1e12),
                          doc='Unit Base Cost cost in $')
-    self.purchase_cost = Var(initialize =1e4,
-                             bounds = (0,1e12),
-                             doc = 'Unit Purchase Cost in $')
-    
-    self.FM = Param(mutable = True, initialize = 3,
-                    doc= 'construction material correction factor')
-    
-    
-def hx_costing(self, hx_type='U-tube', FM='stain_steel', 
-                 L_factor = '12ft'):
+    self.purchase_cost = Var(initialize=1e4,
+                             bounds=(0, 1e12),
+                             doc='Unit Purchase Cost in $')
+
+    self.FM = Param(mutable=True, initialize=3,
+                    doc='construction material correction factor')
+
+
+def hx_costing(self, hx_type='U-tube', FM='stain_steel', L_factor='12ft'):
     '''
     Heat exchanger costing method
-    Source: Process and Product Design Principles: Synthesis, Analysis, 
+    Source: Process and Product Design Principles: Synthesis, Analysis,
     and Evaluation
     Seider, Seader, Lewin, Windagdo, 3rd Ed. John Wiley and Sons
     Chapter 22. Cost Accounting and Capital Cost Estimation
     22.2 Cost Indexes and Capital Investment
-    
+
     This method computes the purchase cost (CP) for a shell and tube heat
-    exchanger (Eq. 22.43), the model computes the base cost (CB for 4 type 
-    of heat exchangers, such as floating head, fixed head, U-tube, and 
-    Kettle vaporizer), construction material factor (FM), pressure design 
-    factor (FP), and tube length correction factor (FL), using CE base cost 
-    index of 500. 
-    
+    exchanger (Eq. 22.43), the model computes the base cost (CB for 4 type
+    of heat exchangers, such as floating head, fixed head, U-tube, and
+    Kettle vaporizer), construction material factor (FM), pressure design
+    factor (FP), and tube length correction factor (FL), using CE base cost
+    index of 500.
+
     Cp = FP*FM*FL*CB
-    
+
     Args:
         hx_type : 'floating_head', 'fixed_head', 'U-tube'*, 'Kettle_vap'
-        
+
         material factor (FM): 'stain_steel'*, 'carb_steel'
-        
+
         tube length (FL): '8ft'*, '12ft', '16ft', '20ft'
-        
+
         * --> default options
-    
+
     Author: MZamarripa (created 9/17/2019; updated 2/21/2020)
     '''
     # ------------------------ Heat Exchanger cost ------------------------
     # heat exchanger cost
-    #build generic costing variables (base cost, purchase cost, material factor)
+    # build generic costing variables
+    # (base cost, purchase cost, material factor)
     _make_vars(self)
-    self.FL = Param(mutable = True, initialize = 1.12,
+    self.FL = Param(mutable=True, initialize=1.12,
                     doc='hx tube length correction factor')
-    
-    self.FP = Var(initialize = 100,
-                  doc = 'Pressure factor')
-    
-    self.hx_os = Param(mutable = True, initialize = 1.1,
-                       doc= 'hx oversize factor 1.1 to 1.5')
 
-    # select length correction factor 
-    c_fl = {'8ft':1.25, '12ft':1.12, '16ft':1.05, '20ft':1.00}
+    self.FP = Var(initialize=100,
+                  doc='Pressure factor')
+
+    self.hx_os = Param(mutable=True, initialize=1.1,
+                       doc='hx oversize factor 1.1 to 1.5')
+
+    # select length correction factor
+    c_fl = {'8ft': 1.25, '12ft': 1.12, '16ft': 1.05, '20ft': 1.00}
     self.FL = c_fl[L_factor]
 
     # select construction material (stainless steel default)
-    c_material = {'stain_steel':3, 'carbon_steel':2}
+    c_material = {'stain_steel': 3, 'carbon_steel': 2}
     self.FM = c_material[FM]
-    # ToDo (9/17/2019):FM can be calculated for advanced materials Eq.22.44 
-    # i.e. Carbon Steel/Cr-Mo steel, Cr-Mo steel/Cr-Mo steel, Monel/Monel, 
+    # ToDo (9/17/2019):FM can be calculated for advanced materials Eq.22.44
+    # i.e. Carbon Steel/Cr-Mo steel, Cr-Mo steel/Cr-Mo steel, Monel/Monel,
     # Titanium/titanium, etc.
-    
-    #--------------------------------------------------
+
+    # --------------------------------------------------
     # base cost calculation
 #       # select heat exchanger type:
-    alf1 = {'floating_head':11.9052,
-            'fixed_head':11.2927,
-            'U-tube':11.3852,
-            'Kettle_vap':12.2052}
-    alf2 = {'floating_head':0.8709,
-            'fixed_head':0.8228,
-            'U-tube':0.9186,
-            'Kettle_vap':0.8709}
-    alf3 = {'floating_head':0.09005,
-            'fixed_head':0.09861,
-            'U-tube':0.09790,
-            'Kettle_vap':0.09005}
-    
+    alf1 = {'floating_head': 11.9052,
+            'fixed_head': 11.2927,
+            'U-tube': 11.3852,
+            'Kettle_vap': 12.2052}
+    alf2 = {'floating_head': 0.8709,
+            'fixed_head': 0.8228,
+            'U-tube': 0.9186,
+            'Kettle_vap': 0.8709}
+    alf3 = {'floating_head': 0.09005,
+            'fixed_head': 0.09861,
+            'U-tube': 0.09790,
+            'Kettle_vap': 0.09005}
+
     if (self.parent_block().config.tube.property_package.get_metadata().
             default_units['length']) == 'm':
         area = self.parent_block().area*10.7639
@@ -124,120 +126,135 @@ def hx_costing(self, hx_type='U-tube', FM='stain_steel',
 
     def hx_cost_rule(self):
         return self.base_cost == self.FL * exp(alf1[hx_type]
-                    - alf2[hx_type]*log(area*self.hx_os)
-                    + alf3[hx_type]*log(area*self.hx_os)**2)
+                                               - alf2[hx_type]
+                                               * log(area*self.hx_os)
+                                               + alf3[hx_type]
+                                               * log(area*self.hx_os)**2)
     self.base_cost_eq = Constraint(rule=hx_cost_rule)
-    
-    #------------------------------------------------------
+
+    # ------------------------------------------------------
     # Pressure factor calculation
     # doublecheck units (higher pressure fluid should be tube side)
     if (self.parent_block().config.tube.property_package.get_metadata().
             properties['pressure']['units']) == 'Pa':
-        pressure = self.parent_block().tube.properties_in[0].pressure*14.69/1.01325e5
+        pressure = (self.parent_block().
+                    tube.properties_in[0].pressure*14.69/1.01325e5)
     elif (self.parent_block().config.tube.property_package.get_metadata().
             properties['pressure']['units']) == 'psig':
         pressure = self.tube.properties_in[0].pressure
 
-    #units must be in psig 
-    #(ToDo(9/17/2019): check inlet pressure to select correlation)
+    # units must be in psig
     def hx_P_factor(self):
         # eq valid from 600 pisg to 3000 psig
-        #    return self.hx_FP == 0.8510 + 0.1292*(pressure/600) 
+        #    return self.hx_FP == 0.8510 + 0.1292*(pressure/600)
         #                                + 0.0198*(pressure/600)**2
         # eq valid from 100 pisg to 2000 psig
         return self.FP == 0.9803 + (0.018*(pressure*14.69/1.01325e5/100)
                                     + 0.0017*(pressure*14.69/1.01325e5/100)**2)
     self.p_factor_eq = Constraint(rule=hx_P_factor)
 
-
-    #---------------------------------------------------------
+    # ---------------------------------------------------------
     # purchase cost equation
     def hx_CP_rule(self):
-        return self.purchase_cost == (self.FP*self.FM*self.FL*
-                                      (self.parent_block().flowsheet().costing.CE_index/500)*self.base_cost)
+        return self.purchase_cost == (self.FP*self.FM*self.FL
+                                      * (self.parent_block().flowsheet().
+                                         costing.CE_index/500)*self.base_cost)
     self.cp_cost_eq = Constraint(rule=hx_CP_rule)
 
 
-def pressure_changer_costing(self, FM_mat = "stain_steel", # applies for all types (pump, compressor, fan, blower)
-                             mover_type = "compressor", #fan, blower, compressor
-                             compressor_type="centrifugal", #only for compressor
-                             driver_mover_type= "electrical_motor", #only for compressors
-                             pump_type = "centrifugal", #centrifugal, external_gear, reciprocating
-                             pump_type_factor = '1.4', #1.1 to 1.4, 2.1 and 2.2 (needs to be wise-selected by user see table)
-                             pump_motor_type_factor = 'open'):
-    '''    
+def pressure_changer_costing(self, FM_mat="stain_steel",
+                             # applies for all (pump, compressor, fan, blower)
+                             mover_type="compressor",
+                             # fan, blower, compressor
+                             compressor_type="centrifugal",
+                             # only for compressor
+                             driver_mover_type="electrical_motor",
+                             # only for compressors
+                             pump_type="centrifugal",
+                             # centrifugal, external_gear, reciprocating
+                             pump_type_factor='1.4',
+                             # 1.1 to 1.4, 2.1 and 2.2
+                             # (needs to be wise-selected by user see table)
+                             pump_motor_type_factor='open'):
+    '''
     Pressure changer costing method
-    Source: Process and Product Design Principles: Synthesis, Analysis, 
-    and Evaluation
-    Seider, Seader, Lewin, Windagdo, 3rd Ed. John Wiley and Sons
+    Source: Process and Product Design Principles: Synthesis, Analysis,
+    and Evaluation Seider, Seader, Lewin, Windagdo, 3rd Ed. John Wiley and Sons
     Chapter 22. Cost Accounting and Capital Cost Estimation
     22.2 Cost Indexes and Capital Investment
-    
-    This method computes the purchase cost (CP) for a pressure changer unit, 
-    and can be used for costing of pumps, fan, blower, compressor, or Turbine.  
-    
-    Author: MZamarripa (created 2/21/2020)
-    
-    Arguments: 
 
-        if config.compressor 
+    This method computes the purchase cost (CP) for a pressure changer unit,
+    and can be used for costing of pumps, fan, blower, compressor, or Turbine.
+
+    Author: MZamarripa (created 2/21/2020)
+
+    Arguments:
+
+        if config.compressor
         if config.compressor is True:
         mover_type: Fan, Blower, Compressor*
             Fan_type:
             Blower_type:
             Compressor_type: centrifugal*, reciprocating, screw
                 drive_type: electric_motor*, steam_turbine, gas_turbine
-                
-        FM - construction material: 
+
+        FM - construction material:
             Pump: stain_steel*
-            Compressor: stain_steel*, nickel_alloy 
+            Compressor: stain_steel*, nickel_alloy
     *default option
     '''
-    #build generic costing variables (base cost, purchase cost, material factor)
+    # build generic costing variables
+    # (base cost, purchase cost, material factor)
     _make_vars(self)
-    
+
     # if compressor is == False, that means pressure changer is a Turbine
-    if self.parent_block().config.compressor == False:
+    if self.parent_block().config.compressor is False:
         # turbine purchase cost calculation W / 746 = horsepower
         if self.parent_block().config.thermodynamic_assumption.name == 'pump':
-            w = self.parent_block().work_fluid[self.parent_block().flowsheet().config.time.first()]
+            w = (self.parent_block().
+                 work_fluid[self.parent_block().
+                            flowsheet().config.time.first()])
         else:
-            w = self.parent_block().work_mechanical[self.parent_block().flowsheet().config.time.first()]
-        
+            w = (self.parent_block().
+                 work_mechanical[self.parent_block().
+                                 flowsheet().config.time.first()])
+
         if(self.parent_block().config.property_package.get_metadata().
            properties['enth_mol']['units']) == 'J/mol':
             work_hp = w/746  # assuming work is in J/s
-        
+
         elif(self.parent_block().config.property_package.get_metadata().
-           properties['enth_mol']['units']) == 'kJ/kmol':
+             properties['enth_mol']['units']) == 'kJ/kmol':
             work_hp = w*0.0003725  # assuming w is in kJ/hr
-        
+
         elif(self.parent_block().config.property_package.get_metadata().
-           properties['enth_mol']['units']) == 'cal/mol':
-            work_hp = w*0.00561 # assuming W is in cal/s
-        
+             properties['enth_mol']['units']) == 'cal/mol':
+            work_hp = w * 0.00561  # assuming W is in cal/s
+
         elif(self.parent_block().config.property_package.get_metadata().
-           properties['enth_mol']['units']) == 'cal/kmol':
+             properties['enth_mol']['units']) == 'cal/kmol':
             work_hp = w/641186.48  # assuming W is in cal/hr
-        
+
         else:
             raise Exception("units not supported")
-        
+
         def CP_rule(self):
-                return self.purchase_cost == 530*(-1*work_hp)**0.81 ## NETL Baseline report?
+            return self.purchase_cost == 530*(-1*work_hp)**0.81
         self.cp_cost_eq = Constraint(rule=CP_rule)
-    
-    #if compressor is True (the pressure changer could be a Pump, Fan, Blower, or Compressor)
+
+    # if compressor is True
+    # Then the pressure changer could be a Pump, Fan, Blower, or Compressor
     else:
-        #if ThermodynamicAssumption == Pump (the unit is a Pump)
+        # if ThermodynamicAssumption == Pump (the unit is a Pump)
         if self.parent_block().config.thermodynamic_assumption.name == 'pump':
             # assign work fluid  = to w
             w = self.parent_block().work_fluid[self.parent_block().flowsheet().config.time.first()]
-            
+
             #new variables only used by pump costing
             self.pump_head = Var(initialize = 10,
-                  doc = 'Pump Head in feet of fluid flowing (Pressure rise/dens)')
-            
+                  doc = 'Pump Head in feet of fluid '
+                  'flowing (Pressure rise/dens)')
+
             self.size_factor = Var(initialize = 10000,
                                    doc='pump size factor, f(Q,pump_head) in gpm*ft^0.5')
             
@@ -291,7 +308,8 @@ def pressure_changer_costing(self, FM_mat = "stain_steel", # applies for all typ
             if(self.parent_block().config.property_package.get_metadata().
                  properties['flow_vol']['units']) == 'm^3/s':
                 # 1 m3/s = 15850.32314 gal(US)/min
-                Q_gpm = self.parent_block().control_volume.properties_in[0].flow_vol * 15850.32314
+                Q_gpm = self.parent_block().control_volume.\
+                    properties_in[0].flow_vol * 15850.32314
                 
 #                @self.Expression(self)
 #                def Q_gpm(self):
@@ -326,10 +344,10 @@ def pressure_changer_costing(self, FM_mat = "stain_steel", # applies for all typ
                                    'nickel'      :3.50,
                                    'titanium'    :9.70}
             # material factor dictionary for Reciprocating Plunger pumps
-            material_factor_reciprocal_pumps = {'ductile_iron':1.00,
-                                                'Ni_Al_Bronze':1.15,
-                                                'carbon_steel':1.50,
-                                                'stain_steel':2.20}
+            material_factor_reciprocal_pumps = {'ductile_iron': 1.00,
+                                                'Ni_Al_Bronze': 1.15,
+                                                'carbon_steel': 1.50,
+                                                'stain_steel': 2.20}
             
             # pump type factor dictionary (only used by centrifugal pumps)
             pump_type_factor_dic = {'1.1':1.00,
