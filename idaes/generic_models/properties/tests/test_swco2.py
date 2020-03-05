17330995b3d2ddcf2ce1e7472b69e0345bbc39c9
John Eslick <john.eslick@netl.doe.gov>
2020-03-04 20:13:06 -0500

    Add transport tests.

--- idaes/generic_models/properties/tests/test_swco2.py
+++ idaes/generic_models/properties/tests/test_swco2.py
@@ -17,6 +17,7 @@ import pytest
 from pyomo.environ import ConcreteModel, value, Var
 from pyomo.environ import ExternalFunction as EF
 from pyomo.common.fileutils import this_file_dir
+import idaes.generic_models.properties.swco2 as swco2
 import csv
 import os
 import idaes
@@ -31,17 +32,20 @@ def swco2_available():
 
 def read_data(fname):
     dfile = os.path.join(this_file_dir(), fname)
-    data = {}
-    data["T"] = [] # T in K col 0
-    data["P"] = [] # P in kPa col 1
-    data["rho"] = [] # density kg/m3 col 2
-    data["U"] = [] # internal energy kJ/kg col 4
-    data["H"] = [] # enthalpy kJ/kg col 5
-    data["S"] = [] # entropy kJ/kg/K col 6
-    data["cv"] = []
-    data["cp"] = []
-    data["w"] = []
-    data["phase"] = [] # liquid, vapor, or supercritical col 13
+    data = {
+        "T": [], # T in K col 0
+        "P": [], # P in kPa col 1
+        "rho": [], # density kg/m3 col 2
+        "U": [], # internal energy kJ/kg col 4
+        "H": [], # enthalpy kJ/kg col 5
+        "S": [], # entropy kJ/kg/K col 6
+        "cv": [],
+        "cp": [],
+        "w": [],
+        "phase": [], # liquid, vapor, or supercritical col 13
+        "visc": [],
+        "tc": [],
+    }
 
     with open(dfile, 'r') as csvfile:
         dat = csv.reader(csvfile, delimiter='\t', quotechar='"')
@@ -57,6 +61,8 @@ def read_data(fname):
             data["cv"].append(float(row[7]))
             data["cp"].append(float(row[8]))
             data["w"].append(float(row[9]))
+            data["visc"].append(float(row[11]))
+            data["tc"].append(float(row[12]))
             data["phase"].append(row[13])
     return data
 
@@ -177,3 +183,68 @@ class TestSWCO2(object):
             check(T, data["rho"][i], func=model.func_cp, val=data["cp"][i], rel=1e-2)
             check(T, data["rho"][i], func=model.func_cv, val=data["cv"][i], rel=1e-2)
             check(T, data["rho"][i], func=model.func_w, val=data["w"][i], rel=1e-2)
+
+    @pytest.fixture(scope="class")
+    def model2(self):
+        model = ConcreteModel()
+        model.prop_param = swco2.SWCO2ParameterBlock()
+        model.prop_in = swco2.SWCO2StateBlock(
+            default={"parameters": model.prop_param}
+        )
+        return model
+
+    def test_transport(self, model2):
+        data = (
+           #(T,      P,        mu,          tc,         tol_mu,  tol_tc)
+            (200,    0.1e6,    10.06e-6,    9.63e-3,    0.02,    0.02),
+            (240,    0.1e6,    12.07e-6,    12.23e-3,   0.02,    0.02),
+            (240,    10.0e6,   188.91e-6,   159.24e-3,  0.02,    0.02),
+            (240,    25e6,     214.16e-6,   171.59e-3,  0.02,    0.02),
+            (280,    0.1e6,    14.05e-6,    15.19e-3,   0.02,    0.02),
+            (280,    2.5e6,    14.51e-6,    17.46e-3,   0.02,    0.04),
+            (280,    30.0e6,   134.98e-6,   136.63e-3,  0.02,    0.02),
+            (280,    50.0e6,   160.73e-6,   152.67e-3,  0.02,    0.02),
+            (306,    7.5e6,    24.06e-6,    23.93e-3,   0.02,    0.40), # supercritcal near critical
+            (400,    5.0e6,    20.37e-6,    27.46e-3,   0.02,    0.02),
+            (400,    50.0e6,   65.67e-6,    86.48e-3,   0.02,    0.02),
+            (400,    100e6,    101.41e-6,   121.41e-3,  0.02,    0.02),
+            (600,    50e6,     42.20e-6,    64.11e-3,   0.02,    0.02),
+            (1000,   5e6,      41.42e-6,    71.18e-3,   0.02,    0.02),
+            (1000,   50e6,     46.16e-6,    80.33e-3,   0.02,    0.02),
+            (1000,   100e6,    54.79e-6,    92.16e-3,   0.02,    0.02),
+        )
+
+        for d in data:
+            tol_mu = d[4]
+            tol_tc = d[5]
+            P = d[1]
+            T = d[0]
+            mu_data = d[2]
+            tc_data = d[3]
+            model2.prop_in.temperature.set_value(T)
+            model2.prop_in.pressure = P
+            Tsat = value(model2.prop_in.temperature_sat)
+            if P >= 7.377e6 and T >= 304.1282:
+                # super critical, which we clasify as liquid
+                ph = "Liq"
+            elif T > Tsat + 0.5:
+                ph = "Vap"
+            elif T < Tsat - 0.5:
+                ph = "Liq"
+            else:
+                # if too close to sat, don't want to get into a situation where
+                # I don't know if it's liquid or vapor, so using extreme caution
+                # If we're woried about it, we can add tests on the sat curve, and
+                # test both liquid and vapor
+                continue
+
+            rho = value(model2.prop_in.dens_mass_phase[ph])
+            mu = value(model2.prop_in.visc_d_phase[ph])
+            tc = value(model2.prop_in.therm_cond_phase[ph])
+            print("T = {}, P = {}, mu = {}, tc = {}, rho = {}, phase = {}".format(
+                    T, P, mu, tc, rho, ph
+                )
+            )
+            print("Data: mu = {}, tc = {}".format(mu_data, tc_data))
+            assert tc == pytest.approx(tc_data, rel=tol_tc)
+            assert mu == pytest.approx(mu_data, rel=tol_mu)
