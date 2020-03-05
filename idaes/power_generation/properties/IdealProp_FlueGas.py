a8943b42b5a5d911cab152afc1c32b6cebd8b0f2
John Eslick <john.eslick@netl.doe.gov>
2020-03-05 12:28:18 -0500

    Add file for compatablity.

---
+++ idaes/power_generation/properties/IdealProp_FlueGas.py
@@ -0,0 +1,3 @@
+# this is just for compatability with the old name.
+
+from .flue_gas_ideal import *
--- idaes/power_generation/properties/__init__.py
+++ idaes/power_generation/properties/__init__.py
@@ -1,6 +1,3 @@
-# This first one is for compatability with the old name eventually reomove
-from idaes.power_generation.properties import flue_gas_ideal as IdealProp_FlueGas
-
 from idaes.power_generation.properties.flue_gas_ideal import (
     FlueGasParameterBlock,
     FlueGasStateBlock,
