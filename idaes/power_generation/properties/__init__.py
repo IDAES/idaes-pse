5e1381e7773a6ea473e7467e5131531b803a0043
John Eslick <john.eslick@netl.doe.gov>
2020-03-05 12:16:05 -0500

    Update serializer to deal with scaling sufixes.

--- idaes/power_generation/properties/__init__.py
+++ idaes/power_generation/properties/__init__.py
@@ -1,5 +1,5 @@
 # This first one is for compatability with the old name eventually reomove
-import idaes.power_generation.properties.flue_gas_ideal as IdealProp_FlueGas
+from idaes.power_generation.properties import flue_gas_ideal as IdealProp_FlueGas
 
 from idaes.power_generation.properties.flue_gas_ideal import (
     FlueGasParameterBlock,
