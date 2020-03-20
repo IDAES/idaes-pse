01f2d469cd131bac4f8e2cf1bc3501fb375c0220
John Siirola <jsiirola@users.noreply.github.com>
2020-03-20 10:26:47 -0600

    Prevent normal import of beta module after using import_beta

--- idaes/beta.py
+++ idaes/beta.py
@@ -20,6 +20,7 @@ import idaes.logger as idaeslog
 
 _declared_beta_modules = set()
 _declared_beta_module_imports = set()
+_imported_beta_modules = {}
 
 def _caller_module_name():
     # Move up the stack twice: we want the module name of the
@@ -49,19 +50,33 @@ def import_beta(name, package=None):
         if len(package_info) == 2:
             package = package_info[0]
     absolute_name = importlib.util.resolve_name(name, package)
-    if absolute_name not in _declared_beta_module_imports:
+
+    if absolute_name in _imported_beta_modules:
+        # This is a beta module that we have already imported.  Do not
+        # re-import it and instead return the cached imported module
+        return _imported_beta_modules[absolute_name]
+    else:
         _declared_beta_module_imports.add(absolute_name)
         try:
-            module = importlib.import_module(absolute_name)
-            if absolute_name not in _declared_beta_modules:
+            _imported_beta_modules[absolute_name] \
+                = module = importlib.import_module(absolute_name)
+
+            if absolute_name in _declared_beta_modules:
+                # Remove this (BETA) module from sys.modules so that
+                # subsequent imports will re-trigger the
+                # declare_beta_module test in the module.
+                print("removing %s" % (absolute_name,))
+                del sys.modules[absolute_name]
+            else:
                 idaeslog.getLogger(absolute_name).info(
                     "Module '%s' imported module '%s' as a Beta module.  "
                     "This module is not declared beta and can be imported "
                     "using Python's normal import mechanisms."
                     % (_caller_module_name(), absolute_name,))
+                # Remove this (standard) module from the
+                # _imported_beta_modules cache
+                _imported_beta_modules.pop(absolute_name)
             return module
-        except:
+        finally:
             _declared_beta_module_imports.remove(absolute_name)
-            raise
-    return sys.modules[absolute_name]
             
--- idaes/tests/test_beta.py
+++ idaes/tests/test_beta.py
@@ -14,11 +14,11 @@
 
 import logging
 import pytest
+import sys
 
 from io import StringIO
 from pyomo.common.log import LoggingIntercept
-
-import idaes.beta
+from idaes.beta import import_beta
 
 def test_beta_module_exception():
     with pytest.raises(
@@ -29,20 +29,46 @@ def test_beta_module_exception():
 def test_beta_module_import():
     os = StringIO()
     with LoggingIntercept(os, 'idaes', logging.INFO):
-        mod = idaes.beta.import_beta('idaes.tests.beta_mod')
+        mod = import_beta('idaes.tests.beta_mod')
     assert mod.__name__ == 'idaes.tests.beta_mod'
     assert mod.reference_value == 42
     assert os.getvalue() == ""
 
-    mod = idaes.beta.import_beta('.beta_mod')
+    os = StringIO()
+    with LoggingIntercept(os, 'idaes', logging.INFO):
+        mod = import_beta('.beta_mod')
     assert mod.__name__ == 'idaes.tests.beta_mod'
     assert mod.reference_value == 42
+    assert os.getvalue() == ""
 
     os = StringIO()
     with LoggingIntercept(os, 'idaes.logger', logging.INFO):
-        mod = idaes.beta.import_beta('idaes.logger')
+        mod = import_beta('idaes.logger')
     assert mod.__name__ == 'idaes.logger'
     assert os.getvalue().strip() == "Module 'idaes.tests.test_beta' "\
         "imported module 'idaes.logger' as a Beta module.  "\
         "This module is not declared beta and can be "\
         "imported using Python's normal import mechanisms."
+
+def test_beta_reimport():
+    os = StringIO()
+    with LoggingIntercept(os, 'idaes', logging.INFO):
+        mod = import_beta('idaes.tests.beta_mod')
+    assert mod.__name__ == 'idaes.tests.beta_mod'
+    assert mod.reference_value == 42
+    assert os.getvalue() == ""
+
+    # Test that subsequent standard imports will still trigger an
+    # exception, even if the module was previously imported using
+    # import-beta()
+    with pytest.raises(
+            ImportError, match=r"Module 'idaes.tests.beta_mod' is in beta "
+            "and must be imported using idaes.beta.import_beta\(\)."):
+        import idaes.tests.beta_mod
+
+    # But subsequent beta_import() modules return the same (original)
+    # module object
+    os = StringIO()
+    with LoggingIntercept(os, 'idaes', logging.INFO):
+        mod2 = import_beta('idaes.tests.beta_mod')
+    assert mod2 is mod
