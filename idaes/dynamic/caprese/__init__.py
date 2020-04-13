2a9e1f4b171733f0fd02aec998b4c29e48bffc48
robbybp <robbybparker@gmail.com>
2020-04-13 09:49:22 -0600

    Remove dangling comma

--- idaes/dynamic/caprese/__init__.py
+++ idaes/dynamic/caprese/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 """ __init__.py for Caprese module
 """
-from .nmpc import NMPCSim, 
+from .nmpc import NMPCSim
 from util import (ControlInitOption, ElementInitializationInputOption,
         TimeResolutionOption, ControlPenaltyType, VariableCategory)
