4e46d0e64fa54291cb934f1d508cc34d4c8f5a24
John Eslick <john.eslick@netl.doe.gov>
2020-04-24 11:00:52 -0400

    Update release tag

--- idaes/config.py
+++ idaes/config.py
@@ -5,7 +5,7 @@ import os
 import importlib
 
 _log = logging.getLogger(__name__)
-default_binary_url = "https://github.com/IDAES/idaes-ext/releases/download/dev/"
+default_binary_url = "https://github.com/IDAES/idaes-ext/releases/download/2.1.0dev0/"
 binary_platform_map = {
     "rhel6": "centos6",
     "rhel7": "centos7",
