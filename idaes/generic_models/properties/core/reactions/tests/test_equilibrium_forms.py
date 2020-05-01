e4ee72a0b1cada6566b96d2b5411ba474fa13d6f
Andrew Lee <andrew.lee@netl.doe.gov>
2020-05-01 12:57:27 -0400

    Adding tests for equilirbium forms

--- idaes/generic_models/properties/core/reactions/tests/test_equilibrium_forms.py
+++ idaes/generic_models/properties/core/reactions/tests/test_equilibrium_forms.py
@@ -19,7 +19,7 @@ import pytest
 from pyomo.environ import Block, ConcreteModel, Var
 from pyomo.common.config import ConfigBlock
 
-from idaes.generic_models.properties.core.reactions.rate_forms import *
+from idaes.generic_models.properties.core.reactions.equilibrium_forms import *
 
 from idaes.core.util.testing import PhysicalParameterTestBlock
 from idaes.core.util.misc import add_object_reference
@@ -39,12 +39,12 @@ def model():
     m.rparams.config = ConfigBlock(implicit=True)
 
     m.rparams.config.property_package = m.pparams
-    m.rparams.config.rate_reactions = ConfigBlock(implicit=True)
-    m.rparams.config.rate_reactions.r1 = ConfigBlock(implicit=True)
-    m.rparams.config.rate_reactions.r1 = {
+    m.rparams.config.equilibrium_reactions = ConfigBlock(implicit=True)
+    m.rparams.config.equilibrium_reactions.r1 = ConfigBlock(implicit=True)
+    m.rparams.config.equilibrium_reactions.r1 = {
         "stoichiometry": {("p1", "c1"): -1,
                           ("p1", "c2"): 2},
-        "rate_form": mole_frac_power_law_rate,
+        "equilibrium_form": mole_frac_power_law_equil,
         "parameter_data": {}}
 
     m.rparams.reaction_r1 = Block()
@@ -54,48 +54,50 @@ def model():
     add_object_reference(m.rxn[1], "params", m.rparams)
     add_object_reference(m.rxn[1], "state_ref", m.thermo[1])
 
-    m.rxn[1].k_rxn = Var(["r1"], initialize=1)
+    m.rxn[1].k_eq = Var(["r1"], initialize=1)
 
     return m
 
 
-def test_mole_frac_power_law_rate_no_order(model):
-    mole_frac_power_law_rate.build_parameters(
-        model.rparams.reaction_r1, model.rparams.config.rate_reactions["r1"])
+def test_mole_frac_power_law_equil_no_order(model):
+    mole_frac_power_law_equil.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
 
     # Check parameter construction
     assert isinstance(model.rparams.reaction_r1.reaction_order, Var)
     assert len(model.rparams.reaction_r1.reaction_order) == 4
     for i, v in model.rparams.reaction_r1.reaction_order.items():
         try:
-            stoic = model.rparams.config.rate_reactions.r1.stoichiometry[i]
+            stoic = \
+                model.rparams.config.equilibrium_reactions.r1.stoichiometry[i]
         except KeyError:
             stoic = 0
 
-        if stoic < 1:
-            assert v.value == -stoic
-        else:
-            assert v.value == 0
+        assert v.value == stoic
 
     # Check reaction form
-    rform = mole_frac_power_law_rate.return_expression(
+    rform = mole_frac_power_law_equil.return_expression(
         model.rxn[1], model.rparams.reaction_r1, "r1", 300)
 
     assert str(rform) == str(
-        model.rxn[1].k_rxn["r1"] *
-        model.thermo[1].mole_frac_phase_comp["p1", "c1"] **
-        model.rparams.reaction_r1.reaction_order["p1", "c1"])
+        model.rxn[1].k_eq["r1"] == (
+            model.thermo[1].mole_frac_phase_comp["p1", "c1"] **
+            model.rparams.reaction_r1.reaction_order["p1", "c1"] *
+            model.thermo[1].mole_frac_phase_comp["p1", "c2"] **
+            model.rparams.reaction_r1.reaction_order["p1", "c2"]))
 
 
-def test_mole_frac_power_law_rate_with_order(model):
-    model.rparams.config.rate_reactions.r1.parameter_data = {
+def test_mole_frac_power_law_equil_with_order(model):
+    model.rparams.config.equilibrium_reactions.r1.parameter_data = {
         "reaction_order": {("p1", "c1"): 1,
-                           ("p1", "c2"): 2,
-                           ("p2", "c1"): 3,
-                           ("p2", "c2"): 4}}
+                            ("p1", "c2"): 2,
+                            ("p2", "c1"): 3,
+                            ("p2", "c2"): 4}}
 
-    mole_frac_power_law_rate.build_parameters(
-        model.rparams.reaction_r1, model.rparams.config.rate_reactions["r1"])
+    mole_frac_power_law_equil.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
 
     # Check parameter construction
     assert isinstance(model.rparams.reaction_r1.reaction_order, Var)
@@ -106,11 +108,11 @@ def test_mole_frac_power_law_rate_with_order(model):
     assert model.rparams.reaction_r1.reaction_order["p2", "c2"] == 4
 
     # Check reaction form
-    rform = mole_frac_power_law_rate.return_expression(
+    rform = mole_frac_power_law_equil.return_expression(
         model.rxn[1], model.rparams.reaction_r1, "r1", 300)
 
     assert str(rform) == str(
-        model.rxn[1].k_rxn["r1"] * (
+        model.rxn[1].k_eq["r1"] == (
             model.thermo[1].mole_frac_phase_comp["p1", "c1"] **
             model.rparams.reaction_r1.reaction_order["p1", "c1"] *
             model.thermo[1].mole_frac_phase_comp["p1", "c2"] **
