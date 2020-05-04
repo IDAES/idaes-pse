94f8f7e9105ee4847dc3374e23fe6e0da228f59f
Dan Gunter <dkgunter@lbl.gov>
2020-05-04 12:27:23 -0700

    mark tests with network activity as not for CI

--- idaes/commands/tests/test_commands.py
+++ idaes/commands/tests/test_commands.py
@@ -32,18 +32,21 @@ from idaes.util.system import TemporaryDirectory
 ################
 
 
+@pytest.mark.nocircleci()  # goes out to network
 def test_examples_n():
     target_dir = str(uuid.uuid4())  # pick something that won't exist
     retcode = subprocess.call(["idaes", "get-examples", "-N", "-d", target_dir])
     assert retcode == 255  # result of sys.exit(-1)
 
 
+@pytest.mark.nocircleci()  # goes out to network
 def test_examples_list_releases():
     releases = examples.get_releases(True)
     assert len(releases) > 0
     examples.print_releases(releases, True)
 
 
+@pytest.mark.nocircleci()  # goes out to network
 def test_examples_download_bad_version():
     releases = [examples.Release("baddate", "badtag", "info")]
     assert pytest.raises(examples.DownloadError, examples.download, Path("."), "1.2.3")
