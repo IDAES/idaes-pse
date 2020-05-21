67df8b26c1642451c39aecf293a68b96b8da8dc1
OOAmusat <47539353+OOAmusat@users.noreply.github.com>
2020-05-21 12:01:02 -0700

    Sphinx changes

--- idaes/surrogate/my_surrogate_base.py
+++ idaes/surrogate/my_surrogate_base.py
@@ -12,12 +12,17 @@ import os.path, pickle
 
 class Metrics:
     """
+
     Names for known types of metrics.
+
     Use these as keys in dictionaries, e.g.:
+
         m = {Metrics.RMSE: self.rmse}
+
     When adding attributes to this class, please include a comment with the
     prefix "#:" immediately above it, so Sphinx knows that this is documentation
     for the attribute.
+
     """
 
     #: Root mean-squared error
