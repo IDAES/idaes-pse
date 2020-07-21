7ecd139c8b41ed35f3354b049a503ccc6875460c
Andrew Lee <andrew.lee@netl.doe.gov>
2020-07-21 10:54:24 -0400

    Adding some tests on reaction parameter conversion

--- idaes/generic_models/properties/core/reactions/tests/test_equilibrium_constant.py
+++ idaes/generic_models/properties/core/reactions/tests/test_equilibrium_constant.py
@@ -84,12 +84,64 @@ def test_van_t_hoff_mole_frac(model):
 
 
 @pytest.mark.unit
+def test_van_t_hoff_mole_frac_convert(model):
+    model.rparams.config.equilibrium_reactions.r1.parameter_data = {
+        "k_eq_ref": (1, None),
+        "T_eq_ref": (900, pyunits.degR)}
+
+    van_t_hoff.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.k_eq_ref, Var)
+    assert model.rparams.reaction_r1.k_eq_ref.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.T_eq_ref, Var)
+    assert model.rparams.reaction_r1.T_eq_ref.value == 500
+
+    # Check expression
+    rform = van_t_hoff.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.99984, rel=1e-3)
+    assert_units_equivalent(rform, None)
+
+
+@pytest.mark.unit
 def test_van_t_hoff_molarity(model):
     model.rparams.config.equilibrium_reactions.r1.concentration_form = \
         ConcentrationForm.molarity
     model.rparams.config.equilibrium_reactions.r1.parameter_data = {
         "k_eq_ref": 1,
         "T_eq_ref": 500}
+
+    van_t_hoff.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.k_eq_ref, Var)
+    assert model.rparams.reaction_r1.k_eq_ref.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.T_eq_ref, Var)
+    assert model.rparams.reaction_r1.T_eq_ref.value == 500
+
+    # Check expression
+    rform = van_t_hoff.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.99984, rel=1e-3)
+    assert_units_equivalent(rform, pyunits.mol/pyunits.m**3)
+
+
+@pytest.mark.unit
+def test_van_t_hoff_molarity_convert(model):
+    model.rparams.config.equilibrium_reactions.r1.concentration_form = \
+        ConcentrationForm.molarity
+    model.rparams.config.equilibrium_reactions.r1.parameter_data = {
+        "k_eq_ref": (1e-3, pyunits.kmol/pyunits.m**3),
+        "T_eq_ref": (900, pyunits.degR)}
 
     van_t_hoff.build_parameters(
         model.rparams.reaction_r1,
@@ -138,12 +190,66 @@ def test_van_t_hoff_molality(model):
 
 
 @pytest.mark.unit
+def test_van_t_hoff_molality_convert(model):
+    model.rparams.config.equilibrium_reactions.r1.concentration_form = \
+        ConcentrationForm.molality
+    model.rparams.config.equilibrium_reactions.r1.parameter_data = {
+        "k_eq_ref": (1e-3, pyunits.kmol/pyunits.kg),
+        "T_eq_ref": (900, pyunits.degR)}
+
+    van_t_hoff.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.k_eq_ref, Var)
+    assert model.rparams.reaction_r1.k_eq_ref.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.T_eq_ref, Var)
+    assert model.rparams.reaction_r1.T_eq_ref.value == 500
+
+    # Check expression
+    rform = van_t_hoff.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.99984, rel=1e-3)
+    assert_units_equivalent(rform, pyunits.mol/pyunits.kg)
+
+
+@pytest.mark.unit
 def test_van_t_hoff_partial_pressure(model):
     model.rparams.config.equilibrium_reactions.r1.concentration_form = \
         ConcentrationForm.partialPressure
     model.rparams.config.equilibrium_reactions.r1.parameter_data = {
         "k_eq_ref": 1,
         "T_eq_ref": 500}
+
+    van_t_hoff.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.equilibrium_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.k_eq_ref, Var)
+    assert model.rparams.reaction_r1.k_eq_ref.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.T_eq_ref, Var)
+    assert model.rparams.reaction_r1.T_eq_ref.value == 500
+
+    # Check expression
+    rform = van_t_hoff.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.99984, rel=1e-3)
+    assert_units_equivalent(rform, pyunits.Pa)
+
+
+@pytest.mark.unit
+def test_van_t_hoff_partial_pressure_convert(model):
+    model.rparams.config.equilibrium_reactions.r1.concentration_form = \
+        ConcentrationForm.partialPressure
+    model.rparams.config.equilibrium_reactions.r1.parameter_data = {
+        "k_eq_ref": (1e-3, pyunits.kPa),
+        "T_eq_ref": (900, pyunits.degR)}
 
     van_t_hoff.build_parameters(
         model.rparams.reaction_r1,
--- idaes/generic_models/properties/core/reactions/tests/test_rate_constant.py
+++ idaes/generic_models/properties/core/reactions/tests/test_rate_constant.py
@@ -81,12 +81,64 @@ def test_arrhenius_mole_frac(model):
 
 
 @pytest.mark.unit
+def test_arrhenius_mole_frac_convert(model):
+    model.rparams.config.rate_reactions.r1.parameter_data = {
+        "arrhenius_const": (1e-3, pyunits.kmol/pyunits.m**3/pyunits.s),
+        "energy_activation": (0.5, pyunits.kJ/pyunits.mol)}
+
+    arrhenius.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.rate_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.arrhenius_const, Var)
+    assert model.rparams.reaction_r1.arrhenius_const.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.energy_activation, Var)
+    assert model.rparams.reaction_r1.energy_activation.value == 500
+
+    # Check expressions
+    rform = arrhenius.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.81836, rel=1e-3)
+    assert_units_equivalent(rform, pyunits.mol/pyunits.m**3/pyunits.s)
+
+
+@pytest.mark.unit
 def test_arrhenius_molarity(model):
     model.rparams.config.rate_reactions.r1.concentration_form = \
         ConcentrationForm.molarity
     model.rparams.config.rate_reactions.r1.parameter_data = {
         "arrhenius_const": 1,
         "energy_activation": 500}
+
+    arrhenius.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.rate_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.arrhenius_const, Var)
+    assert model.rparams.reaction_r1.arrhenius_const.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.energy_activation, Var)
+    assert model.rparams.reaction_r1.energy_activation.value == 500
+
+    # Check expressions
+    rform = arrhenius.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.81836, rel=1e-3)
+    assert_units_equivalent(rform, 1/pyunits.s)
+
+
+@pytest.mark.unit
+def test_arrhenius_molarity_convert(model):
+    model.rparams.config.rate_reactions.r1.concentration_form = \
+        ConcentrationForm.molarity
+    model.rparams.config.rate_reactions.r1.parameter_data = {
+        "arrhenius_const": (3600, 1/pyunits.hr),
+        "energy_activation": (0.5, pyunits.kJ/pyunits.mol)}
 
     arrhenius.build_parameters(
         model.rparams.reaction_r1,
@@ -135,12 +187,68 @@ def test_arrhenius_molality(model):
 
 
 @pytest.mark.unit
+def test_arrhenius_molality_convert(model):
+    model.rparams.config.rate_reactions.r1.concentration_form = \
+        ConcentrationForm.molality
+    model.rparams.config.rate_reactions.r1.parameter_data = {
+        "arrhenius_const": (1e3, pyunits.g/pyunits.m**3/pyunits.s),
+        "energy_activation": (0.5, pyunits.kJ/pyunits.mol)}
+
+    arrhenius.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.rate_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.arrhenius_const, Var)
+    assert model.rparams.reaction_r1.arrhenius_const.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.energy_activation, Var)
+    assert model.rparams.reaction_r1.energy_activation.value == 500
+
+    # Check expressions
+    rform = arrhenius.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.81836, rel=1e-3)
+    assert_units_equivalent(rform, pyunits.kg/pyunits.m**3/pyunits.s)
+
+
+@pytest.mark.unit
 def test_arrhenius_partial_pressure(model):
     model.rparams.config.rate_reactions.r1.concentration_form = \
         ConcentrationForm.partialPressure
     model.rparams.config.rate_reactions.r1.parameter_data = {
         "arrhenius_const": 1,
         "energy_activation": 500}
+
+    arrhenius.build_parameters(
+        model.rparams.reaction_r1,
+        model.rparams.config.rate_reactions["r1"])
+
+    # Check parameter construction
+    assert isinstance(model.rparams.reaction_r1.arrhenius_const, Var)
+    assert model.rparams.reaction_r1.arrhenius_const.value == 1
+
+    assert isinstance(model.rparams.reaction_r1.energy_activation, Var)
+    assert model.rparams.reaction_r1.energy_activation.value == 500
+
+    # Check expressions
+    rform = arrhenius.return_expression(
+        model.rxn[1], model.rparams.reaction_r1, "r1", 300*pyunits.K)
+
+    assert value(rform) == pytest.approx(0.81836, rel=1e-3)
+    assert_units_equivalent(rform,
+                            pyunits.mol/pyunits.m**3/pyunits.s/pyunits.Pa)
+
+
+@pytest.mark.unit
+def test_arrhenius_partial_pressure_convert(model):
+    model.rparams.config.rate_reactions.r1.concentration_form = \
+        ConcentrationForm.partialPressure
+    model.rparams.config.rate_reactions.r1.parameter_data = {
+        "arrhenius_const": (1e-3,
+                            pyunits.kmol/pyunits.m**3/pyunits.s/pyunits.Pa),
+        "energy_activation": (0.5, pyunits.kJ/pyunits.mol)}
 
     arrhenius.build_parameters(
         model.rparams.reaction_r1,
