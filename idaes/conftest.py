1376a0f51d384ffbb67b1b0439949674c4857e38
Dan Gunter <dkgunter@lbl.gov>
2020-07-21 10:10:58 -0700

    Code and documentation for platform/noplatform test tagging

--- idaes/conftest.py
+++ idaes/conftest.py
@@ -25,19 +25,29 @@ import sys
 # def test_something():
 #    print("this only runs on linux")
 #
+#
+# In addition, you can use "no<platform>" to exclude
+#
+# @pytest.mark.nowin32
+# def test_something():
+#    print("this will not run on windows")
+#
 # The names of the platforms should match what is returned by `sys.platform`, in particular:
 #    Linux = 'linux'
 #    Windows = 'win32'
 #    macOS = 'darwin'
 
 
-ALL = set("darwin linux win32".split())
+ALL = {"darwin", "linux", "win32"}
+ALL_NO = {"no" + tag for tag in ALL}
 
 
 def pytest_runtest_setup(item):
     supported_platforms = ALL.intersection(mark.name for mark in item.iter_markers())
+    excluded_platforms = ALL_NO.intersection(mark.name for mark in item.iter_markers())
     plat = sys.platform
-    if supported_platforms and plat not in supported_platforms:
+    if ((excluded_platforms and ("no" + plat) in excluded_platforms) or
+        (supported_platforms and plat not in supported_platforms)):
         pytest.skip("cannot run on platform {}".format(plat))
 
 ####
