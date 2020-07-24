d667f9c842c9e39813ab8be73928e42202b58873
Andrew Lee <andrew.lee@netl.doe.gov>
2020-07-24 10:14:29 -0400

    Trying to unpin version of coverage

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ kwargs = dict(
         # For developers. Only installed if [dev] is added to package name
         "dev": [
             "alabaster>=0.7.7",
-            "coverage==4.5.4",
+            "coverage",
             "flake8",
             "flask>=1.0",
             "flask-bower",
