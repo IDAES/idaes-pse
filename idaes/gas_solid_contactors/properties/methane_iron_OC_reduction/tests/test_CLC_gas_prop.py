2c1fbfb28f7c95b8049953cefa96e444fadc4487
aostace01 <angal@mix.wvu.edu>
2020-08-11 00:09:36 -0400

    Add files deleted by mistake

---
+++ idaes/gas_solid_contactors/properties/methane_iron_OC_reduction/tests/test_CLC_gas_prop.py
@@ -0,0 +1,80 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+Tests for CLC gas phase thermo state block; tests for construction and solve
+Author: Chinedu Okoli
+"""
+
+import pytest
+
+from pyomo.environ import (ConcreteModel, Var)
+
+from idaes.core import FlowsheetBlock
+
+from idaes.core.util.model_statistics import degrees_of_freedom
+
+from idaes.core.util.testing import (get_default_solver,
+                                     initialization_tester)
+
+from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+    gas_phase_thermo import GasPhaseThermoParameterBlock
+
+# Get default solver for testing
+solver = get_default_solver()
+
+# -----------------------------------------------------------------------------
+@pytest.fixture(scope="class")
+def gas_prop():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+
+    # gas properties and state inlet block
+    m.fs.properties = GasPhaseThermoParameterBlock()
+    m.fs.unit = m.fs.properties.build_state_block(
+        default={"parameters": m.fs.properties,
+                 "defined_state": True})
+
+    m.fs.unit.flow_mol.fix(1)
+    m.fs.unit.temperature.fix(450)
+    m.fs.unit.pressure.fix(1.60)
+    m.fs.unit.mole_frac_comp["CO2"].fix(0.4772)
+    m.fs.unit.mole_frac_comp["H2O"].fix(0.0646)
+    m.fs.unit.mole_frac_comp["CH4"].fix(0.4582)
+
+    return m
+
+
+@pytest.mark.unit
+def test_build_inlet_state_block(gas_prop):
+    assert isinstance(gas_prop.fs.unit.mw, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mol, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mol_comp, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mass, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mol_comp, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mol, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mass, Var)
+    assert isinstance(gas_prop.fs.unit.visc_d, Var)
+
+
+@pytest.mark.unit
+def test_setInputs_state_block(gas_prop):
+    assert degrees_of_freedom(gas_prop.fs.unit) == 0
+
+
+@pytest.mark.solver
+@pytest.mark.skipif(solver is None, reason="Solver not available")
+@pytest.mark.component
+def test_initialize(gas_prop):
+    initialization_tester(
+            gas_prop)
+
