46edb1fd0497ba2eb69533d1b6eceb5eced827e6
Robert Parker <robbybparker@gmail.com>
2020-08-24 15:46:18 -0400

    remove tests for deprecated class

--- idaes/apps/caprese/tests/test_rolling.py
+++ idaes/apps/caprese/tests/test_rolling.py
@@ -226,7 +226,7 @@ class TestVectorSeries(object):
 
     @pytest.mark.unit
     def test_dim(self):
-        # This test also loosely covers consistent_dimension and 
+        # This test also covers consistent_dimension and 
         # validate_dimension.
         m = make_model()
         data = OrderedDict([
@@ -382,68 +382,3 @@ class TestVectorSeries(object):
             # Assuming that history_2.dim() != 1 ...
             history_2.extend([],[[]])
             assert 'inconsistent dimension' in str(err)
-
-
-'''
-@pytest.mark.unit
-def test_history_init():
-    m = make_model()
-
-    # Cases to test:
-    #   1. synchronous with good data
-    #   2. synchronous with bad (asynchronous) data
-    #   3. asynchronous with synchronous data (should be fine)
-    #   4. asynchronous with asynchronous data
-    
-    # 1.
-    data = {cuid_from_timeslice(_slice, m.time): [float(i) for i in m.time] 
-        for _slice in m.v1_refs}
-
-    history = History(init=data, time=m.time, name='v1_history')
-
-    for cuid in data:
-        assert cuid in history
-        assert history[cuid] == [float(i) for i in m.time]
-        assert history[cuid].time == list(m.time)
-
-    # 4.
-    cuid_a = [cuid_from_timeslice(_slice, m.time) for _slice in m.v2a_refs]
-    cuid_b = [cuid_from_timeslice(_slice, m.time) for _slice in m.v2b_refs]
-    data = {cuid: [(i, float(i)) for i in m.time] for cuid in cuid_a}
-    data.update({cuid: [(i, 2*float(i)) for i in m.time if i % 2 == 0]
-        for cuid in cuid_b})
-
-    history = History(init=data, time=None, name='v2_history')
-
-    assert history.time is None
-    for cuid in cuid_a:
-        assert cuid in history
-        assert history[cuid] == [float(i) for i in m.time]
-        assert history[cuid].time == [i for i in m.time]
-    for cuid in cuid_b:
-        assert cuid in history
-        assert history[cuid] == [2*float(i) for i in m.time if i % 2 == 0]
-        assert history[cuid].time == [i for i in m.time if i % 2 == 0]
-
-    # 3.
-    data = {cuid_from_timeslice(_slice, m.time): [(i, 3*float(i)) for i in m.time]
-            for _slice in m.v3_refs}
-    history = History(init=data, time=None, name='v3_history')
-    
-    assert history.time is None
-    for cuid in data:
-        assert cuid in history
-        assert history[cuid] == [3*float(i) for i in m.time]
-        assert history[cuid].time == list(m.time)
-
-    # 2.
-    cuid_a = [cuid_from_timeslice(_slice, m.time) for _slice in m.v4a_refs]
-    cuid_b = [cuid_from_timeslice(_slice, m.time) for _slice in m.v4b_refs]
-    data = {}
-    data.update({cuid: [(i, 1.5*float(i)) for i in m.time if i % 3 == 0]})
-    data.update({cuid: [(i, 2.5*float(i)) for i in m.time if i % 5 == 0]})
-
-    with pytest.raises(ValueError, match=r'.*must have same length.*'):
-        history = History(init=data, time=m.time, name='v4_history')
-'''
-
