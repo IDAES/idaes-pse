eafe71060684362721466e06991293872b267b9e
Robert Parker <robbybparker@gmail.com>
2020-08-25 22:16:41 -0400

    change names and add k_aug suffixes

--- idaes/apps/caprese/advanced_step.py
+++ idaes/apps/caprese/advanced_step.py
@@ -1,54 +1,68 @@
-from pyomo.core.base.constraint import Var, Constraint, Param, value
+from pyomo.environ import Var, Constraint, Param, value, Suffix
+
+K_AUG_SUFFIXES = [
+        ("ipopt_zL_out", Suffix.IMPORT),
+        ("ipopt_zU_out", Suffix.IMPORT),
+        ("ipopt_zL_in", Suffix.EXPORT),
+        ("ipopt_zU_in", Suffix.EXPORT),
+        ("dual", Suffix.IMPORT_EXPORT),
+        ("npdp", Suffix.EXPORT),
+        ("dof_v", Suffix.EXPORT),
+        ]
 
 class AdvancedStepManager(object):
-    def __init__(self, block, dof_vars, index):
+    def __init__(self, block, wrt_vars, index):
         self.block = block
-        self.dof_vars = dof_vars
-        self.n_dof_vars = len(dof_vars)
+        self.wrt_vars = wrt_vars
+        self.n_wrt_vars = len(wrt_vars)
         self.index = index
 
         # Remove this line if I find some reason to not construct
         # Constraints right away. If this happens, I will need 
         # to have a "constructed" flag that __enter__ can check.
-        self.make_dof_constraints()
+        self.make_wrt_constraints()
 
-    def make_dof_constraints(self):
+    def make_param_constraints(self):
         block = self.block
-        dof_vars = self.dof_vars
-        n_dof_vars = self.n_dof_vars
+        param_vars = self.wrt_vars
+        n_wrt_vars = self.n_wrt_vars
         index = self.index
 
-        # TODO: There could be multiple "dof" components depending
+        # TODO: There could be multiple "wrt" components depending
         # on perspective. User should provide a name, and we should
         # use unique_component_name.
-        block.dof_set = Set(range(n_dof_vars))
+        block.wrt_set = Set(range(n_wrt_vars))
 
-        dof_param_dict = {
-                i: value(dof_vars[i][index])
-                for i in range(n_dof_vars)
+        wrt_param_dict = {
+                i: value(wrt_vars[i][index])
+                for i in range(n_wrt_vars)
                 }
-        block.dof_param = Param(dof_set, initialize=dof_param_dict)
-
-        def dof_constraint_rule(b, i):
-            return dof_vars[i][index] == b.dof_param[i]
-        block.dof_constraint = Constraint(
-                self.dof_set, 
-                rule=dof_constraint_rule,
+        block.wrt_param = Param(
+                wrt_set,
+                initialize=wrt_param_dict,
+                mutable=True,
                 )
-        block.dof_constraint.deactivate()
+
+        def wrt_constraint_rule(b, i):
+            return wrt_vars[i][index] == b.wrt_param[i]
+        block.wrt_constraint = Constraint(
+                self.wrt_set, 
+                rule=wrt_constraint_rule,
+                )
+        block.wrt_constraint.deactivate()
 
     def __enter__(self):
         index = self.index
-        dof_set = self.block.dof_set
-        self.block.dof_constraints.activate()
-        for i in dof_set:
-            # TODO: Should I check that dof_vars were previously fixed?
-            dof_vars[i][index].unfix()
+        wrt_set = self.block.wrt_set
+        self.block.wrt_constraints.activate()
+        for i in wrt_set:
+            # TODO: Should I check that wrt_vars were previously fixed?
+            wrt_vars[i][index].unfix()
         return self
 
     def __exit__(self, e_type, e_val, e_tb):
         index = self.index
-        dof_set = sself.block.dof_set
-        self.block.dof_constraints.deactivate()
-        for i in dof_set:
-            dof_vars[i][index].fix()
+        wrt_set = sself.block.wrt_set
+        self.block.wrt_constraints.deactivate()
+        for i in wrt_set:
+            wrt_vars[i][index].fix()
