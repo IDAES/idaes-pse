d66e23a2b98ab02ebc7c491d7a7040d628ae5b7c
jghouse88 <jaffer.ghouse@netl.doe.gov>
2020-09-13 01:39:56 -0400

    initial test case - PSE2018 ideal case study

--- idaes/generic_models/unit_models/distillation/tests/test_tray_column.py
+++ idaes/generic_models/unit_models/distillation/tests/test_tray_column.py
@@ -22,6 +22,8 @@ from pyomo.environ import (ConcreteModel, TerminationCondition,
 from idaes.core import (FlowsheetBlock, MaterialBalanceType, EnergyBalanceType,
                         MomentumBalanceType)
 from idaes.generic_models.unit_models.distillation import TrayColumn
+from idaes.generic_models.unit_models.distillation.condenser \
+    import CondenserType, TemperatureSpec
 from idaes.generic_models.properties.activity_coeff_models.\
     BTX_activity_coeff_VLE import BTXParameterBlock
 from idaes.core.util.model_statistics import degrees_of_freedom, \
@@ -47,35 +49,38 @@ def test_config():
                                                  "Ideal"})
 
     m.fs.unit = TrayColumn(default={
-                           "number_of_trays": 3,
-                           "feed_tray_location": 2,
+                           "number_of_trays": 10,
+                           "feed_tray_location": 5,
+                           "condenser_type": CondenserType.totalCondenser,
+                           "condenser_temperature_spec":
+                               TemperatureSpec.atBubblePoint,
                            "property_package": m.fs.properties,
                            "has_heat_transfer": False,
                            "has_pressure_change": False})
 
-    assert len(m.fs.unit.config) == 9
+    assert len(m.fs.unit.config) == 12
 
-    assert m.fs.unit.tray[2].config.is_feed_tray
+    assert m.fs.unit.tray[5].config.is_feed_tray
 
     assert hasattr(m.fs.unit, "condenser")
     assert hasattr(m.fs.unit, "reboiler")
 
-    # assert hasattr(m.fs.unit, "liq_stream")
-    # assert hasattr(m.fs.unit, "vap_stream")
+    assert hasattr(m.fs.unit, "liq_stream")
+    assert hasattr(m.fs.unit, "vap_stream")
 
     # Inlet feed conditions
-    m.fs.unit.tray[2].feed.flow_mol.fix(100)
-    m.fs.unit.tray[2].feed.temperature.fix(368)
-    m.fs.unit.tray[2].feed.pressure.fix(101325)
-    m.fs.unit.tray[2].feed.mole_frac_comp[0, "benzene"].fix(0.5)
-    m.fs.unit.tray[2].feed.mole_frac_comp[0, "toluene"].fix(0.5)
+    m.fs.unit.tray[5].feed.flow_mol.fix(100)
+    m.fs.unit.tray[5].feed.temperature.fix(368)
+    m.fs.unit.tray[5].feed.pressure.fix(101325)
+    m.fs.unit.tray[5].feed.mole_frac_comp[0, "benzene"].fix(0.5)
+    m.fs.unit.tray[5].feed.mole_frac_comp[0, "toluene"].fix(0.5)
 
     # unit level inputs
     m.fs.unit.condenser.reflux_ratio.fix(1.4)
+    m.fs.unit.condenser.condenser_pressure.fix(101325)
+
     m.fs.unit.reboiler.boilup_ratio.fix(1.3)
 
-    m.fs.unit.tray[:].properties_out[:].pressure.fix(101325)
-
-    # assert degrees_of_freedom(m) == 0
+    assert degrees_of_freedom(m) == 0
 
     m.fs.unit.initialize()
