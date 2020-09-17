823a5ae5bb9fac25c728f33392de1c26e0d461f2
Tim Bartholomew <timothy.bartholomew@netl.doe.gov>
2020-09-17 12:44:07 -0400

    added scaling methods to reaction_base

--- idaes/core/reaction_base.py
+++ idaes/core/reaction_base.py
@@ -16,6 +16,7 @@ This module contains classes for reaction blocks and reaction parameter blocks.
 
 # Import Pyomo libraries
 from pyomo.common.config import ConfigBlock, ConfigValue, In
+from pyomo.environ import Var, Constraint, Expression
 
 # Import IDAES cores
 from idaes.core.process_block import ProcessBlock
@@ -29,6 +30,7 @@ from idaes.core.util.config import (is_physical_parameter_block,
                                     is_reaction_parameter_block,
                                     is_state_block)
 from idaes.core.util.misc import add_object_reference
+import idaes.core.util.scaling as iscale
 import idaes.logger as idaeslog
 
 _log = idaeslog.getLogger(__name__)
@@ -91,6 +93,66 @@ class ReactionParameterBlock(ProcessBlockData,
         # TODO: Need way to tie reaction package to a specfic property package
         self._validate_property_parameter_units()
         self._validate_property_parameter_properties()
+
+    # This is a dict to store default property scaling factors. They are
+        # defined in the parameter block to provide a universal default for
+        # quantities in a particular kind of state block.  For example, you can
+        # set flow scaling once instead of for every state block. Some of these
+        # may be left for the user to set and some may be defined in a property
+        # module where reasonable defaults can be defined a priori. See
+        # set_default_scaling, get_default_scaling, and unset_default_scaling
+        self.default_scaling_factor = {}
+
+    def set_default_scaling(self, attrbute, value, index=None):
+        """Set a default scaling factor for a property.
+
+        Args:
+            attribute: property attribute name
+            value: default scaling factor
+            index: for indexed properties, if this is not provied the scaling
+                factor default applies to all indexed elements where specific
+                indexes are no specifcally specified.
+
+        Returns:
+            None
+        """
+        self.default_scaling_factor[(attrbute, index)] = value
+
+    def unset_default_scaling(self, attrbute, index=None):
+        """Remove a previously set default value
+
+        Args:
+            attribute: property attribute name
+            index: optional index for indexed properties
+
+        Returns:
+            None
+        """
+        try:
+            del self.default_scaling_factor[(attrbute, index)]
+        except KeyError:
+            pass
+
+    def get_default_scaling(self, attrbute, index=None):
+        """ Returns a default scale factor for a property
+
+        Args:
+            attribute: property attribute name
+            index: optional index for indexed properties
+
+        Returns:
+            None
+        """
+        try:
+            # If a specific component data index exists
+            return self.default_scaling_factor[(attrbute, index)]
+        except KeyError:
+            try:
+                # indexed, but no specifc index?
+                return self.default_scaling_factor[(attrbute, None)]
+            except KeyError:
+                # Can't find a default scale factor for what you asked for
+                return None
 
     @property
     def reaction_block_class(self):
@@ -527,3 +589,15 @@ should be constructed in this reaction block,
         comp = getattr(self, attr)
         clear_call_list(self, attr)
         return comp
+
+    def calculate_scaling_factors(self):
+        super().calculate_scaling_factors()
+        # Get scaling factor defaults, if no scaling factor set
+        for v in self.component_data_objects(
+            (Constraint, Var, Expression), descend_into=False):
+            if iscale.get_scaling_factor(v) is None:  # don't replace if set
+                name = v.getname().split("[")[0]
+                index = v.index()
+                sf = self.config.parameters.get_default_scaling(name, index)
+                if sf is not None:
+                    iscale.set_scaling_factor(v, sf)
