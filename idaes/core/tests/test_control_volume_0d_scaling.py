32365ca8cc89d7507b58032baa542e9b185cde91
Tim Bartholomew <timothy.bartholomew@netl.doe.gov>
2020-09-18 16:18:55 -0400

    test componentPhase mass balance type, forgot to add file

--- idaes/core/tests/test_control_volume_0d_scaling.py
+++ idaes/core/tests/test_control_volume_0d_scaling.py
@@ -35,16 +35,52 @@ import idaes.logger as idaeslog
 
 
 # -----------------------------------------------------------------------------
-# Basic tests
 @pytest.mark.unit
-def test_base_build():
+def test_basic_scaling():
     m = pyo.ConcreteModel()
     m.fs = FlowsheetBlock(default={"dynamic": False})
     m.fs.pp = PhysicalParameterTestBlock()
-    m.fs.cv = ControlVolume0DBlock(default={
-            "property_package": m.fs.pp
-        }
-    )
+    m.fs.cv = ControlVolume0DBlock(default={"property_package": m.fs.pp})
+
+    m.fs.cv.add_geometry()
+    m.fs.cv.add_state_blocks(has_phase_equilibrium=False)
+
+    m.fs.cv.add_material_balances(
+        balance_type=MaterialBalanceType.componentTotal,
+        has_phase_equilibrium=False)
+
+    m.fs.cv.add_energy_balances(
+        balance_type=EnergyBalanceType.enthalpyTotal)
+
+    m.fs.cv.add_momentum_balances(
+        balance_type=MomentumBalanceType.pressureTotal,
+        has_pressure_change=True)
+
+    iscale.calculate_scaling_factors(m)
+
+    # check scaling on select variables
+    assert iscale.get_scaling_factor(m.fs.cv.volume[0]) == 1e-3
+    assert iscale.get_scaling_factor(m.fs.cv.deltaP[0]) == 1040  # 10x the properties pressure scaling factor
+    assert iscale.get_scaling_factor(m.fs.cv.properties_in[0].flow_vol) == 100
+
+    # check scaling on mass, energy, and pressure balances.
+    for c in m.fs.cv.material_balances.values():
+        # this uses the minimum material flow term scale
+        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 112
+    for c in m.fs.cv.enthalpy_balances.values():
+        # this uses the minimum enthalpy flow term scale
+        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 110
+    for c in m.fs.cv.pressure_balance.values():
+        # This uses the inlet pressure scale
+        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 104
+
+
+@pytest.mark.unit
+def test_user_set_scaling():
+    m = pyo.ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+    m.fs.pp = PhysicalParameterTestBlock()
+    m.fs.cv = ControlVolume0DBlock(default={"property_package": m.fs.pp})
     m.fs.cv.add_geometry()
     m.fs.cv.add_state_blocks(has_phase_equilibrium=False)
     m.fs.cv.add_material_balances(
@@ -69,25 +105,9 @@ def test_base_build():
     assert iscale.get_scaling_factor(m.fs.cv.heat) == 11
     assert iscale.get_scaling_factor(m.fs.cv.work) == 12
 
-    # Didn't specify a deltaP scaling factor, so by default pressure in scaling
-    # factor * 10 is used.
-    for v in m.fs.cv.deltaP.values(): #deltaP is time indexed
-        assert iscale.get_scaling_factor(v) == 1040
-
-    # check scaling on mass, energy, and pressure balances.
-    for c in m.fs.cv.material_balances.values():
-        # this uses the minimum material flow term scale
-        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 112
-    for c in m.fs.cv.enthalpy_balances.values():
-        # this uses the minimum enthalpy flow term scale
-        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 110
-    for c in m.fs.cv.pressure_balance.values():
-        # This uses the inlet pressure scale
-        assert iscale.get_constraint_transform_applied_scaling_factor(c) == 104
-
 
 @pytest.mark.unit
-def test_all_vars_and_cons_for_static_control_volume():
+def test_full_auto_scaling():
     m = pyo.ConcreteModel()
     m.fs = FlowsheetBlock(default={"dynamic": False})
     m.fs.pp = PhysicalParameterTestBlock()
@@ -97,18 +117,18 @@ def test_all_vars_and_cons_for_static_control_volume():
         "reaction_package": m.fs.rp})
     m.fs.cv.add_geometry()
     m.fs.cv.add_state_blocks(has_phase_equilibrium=True)
-    # m.fs.cv.add_reaction_blocks(has_equilibrium=True)
+    m.fs.cv.add_reaction_blocks(has_equilibrium=True)
 
     m.fs.cv.add_material_balances(
         balance_type=MaterialBalanceType.componentTotal,
-        has_rate_reactions=False,
-        has_equilibrium_reactions=False,
+        has_rate_reactions=True,
+        has_equilibrium_reactions=True,
         has_phase_equilibrium=True,
         has_mass_transfer=True)
 
     m.fs.cv.add_energy_balances(
         balance_type=EnergyBalanceType.enthalpyTotal,
-        has_heat_of_reaction=False,
+        has_heat_of_reaction=True,
         has_heat_transfer=True,
         has_work_transfer=True,
         has_enthalpy_transfer=True)
@@ -126,8 +146,9 @@ def test_all_vars_and_cons_for_static_control_volume():
     unscaled_constraint_list = list(iscale.unscaled_constraints_generator(m))
     assert len(unscaled_constraint_list) == 0
 
+
 @pytest.mark.unit
-def test_all_vars_and_cons_for_dynamic_control_volume():
+def test_full_auto_scaling_dynamic():
     m = pyo.ConcreteModel()
     m.fs = FlowsheetBlock(default={"dynamic": True, "time_units": pyo.units.s})
     m.fs.pp = PhysicalParameterTestBlock()
@@ -160,6 +181,47 @@ def test_all_vars_and_cons_for_dynamic_control_volume():
 
     m.discretizer = pyo.TransformationFactory('dae.finite_difference')
     m.discretizer.apply_to(m, nfe=3, wrt=m.fs.time, scheme="BACKWARD")
+
+    iscale.calculate_scaling_factors(m)
+
+    # check that all variables have scaling factors
+    unscaled_var_list = list(iscale.unscaled_variables_generator(m))
+    assert len(unscaled_var_list) == 0
+    # check that all constraints have been scaled
+    unscaled_constraint_list = list(iscale.unscaled_constraints_generator(m))
+    assert len(unscaled_constraint_list) == 0
+
+
+@pytest.mark.unit
+def test_full_auto_scaling_mbtype_phase():
+    m = pyo.ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+    m.fs.pp = PhysicalParameterTestBlock()
+    m.fs.rp = ReactionParameterTestBlock(default={"property_package": m.fs.pp})
+    m.fs.cv = ControlVolume0DBlock(default={
+        "property_package": m.fs.pp,
+        "reaction_package": m.fs.rp})
+    m.fs.cv.add_geometry()
+    m.fs.cv.add_state_blocks(has_phase_equilibrium=True)
+    m.fs.cv.add_reaction_blocks(has_equilibrium=True)
+
+    m.fs.cv.add_material_balances(
+        balance_type=MaterialBalanceType.componentPhase,
+        has_rate_reactions=True,
+        has_equilibrium_reactions=True,
+        has_phase_equilibrium=True,
+        has_mass_transfer=True)
+
+    m.fs.cv.add_energy_balances(
+        balance_type=EnergyBalanceType.enthalpyTotal,
+        has_heat_of_reaction=True,
+        has_heat_transfer=True,
+        has_work_transfer=True,
+        has_enthalpy_transfer=True)
+
+    m.fs.cv.add_momentum_balances(
+        balance_type=MomentumBalanceType.pressureTotal,
+        has_pressure_change=True)
 
     iscale.calculate_scaling_factors(m)
 
