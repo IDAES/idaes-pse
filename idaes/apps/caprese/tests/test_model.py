036a0266aee179754b47cdeeb530a4e529903eea
Robert Parker <robbybparker@gmail.com>
2020-10-08 12:58:15 -0400

    file for testing model wrappers

---
+++ idaes/apps/caprese/tests/test_model.py
@@ -0,0 +1,60 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+"""
+
+import pyomo.environ as aml
+import pyomo.dae as dae
+import pyomo.network as pyn
+from pyomo.common.collections import ComponentSet
+from pyomo.core.expr.visitor import identify_variables
+from pyomo.util.calc_var_value import calculate_variable_from_constraint
+
+from idaes.core import (FlowsheetBlock, MaterialBalanceType, EnergyBalanceType,
+        MomentumBalanceType)
+from idaes.core.util.model_statistics import (degrees_of_freedom,
+        activated_equalities_generator, unfixed_variables_generator)
+from idaes.core.util.initialization import initialize_by_time_element
+from idaes.core.util.exceptions import ConfigurationError
+#from idaes.apps.caprese import nmpc
+#from idaes.apps.caprese.nmpc import *
+#from idaes.apps.caprese.util import *
+#from idaes.apps.caprese.common.config import (
+#        VariableCategory,
+#        ElementInitializationInputOption,
+#        ControlInitOption,
+#        )
+from idaes.apps.caprese.tests.test_simple_model import (
+        make_model, 
+        make_small_model,
+        initialize_t0,
+        copy_values_forward,
+        )
+from idaes.apps.caprese.model import DynamicBlock
+import idaes.logger as idaeslog
+import random
+import pytest
+
+__author__ = "Robert Parker"
+
+class TestDynamicBlock(object):
+
+    def make_block(self, sample_time=0.5, horizon=1, nfe=2):
+        model = make_model(horizon=horizon, nfe=nfe)
+        time = model.time
+        inputs = [model.flow_in[time.first()]]
+        block = DynamicBlock(model=model, time=time, inputs=inputs)
+        return block
+
+    def test_constructor(self):
+        block = self.make_block()
