0484bbca3d55cd0435dac2ef3e4b8473eff52c0c
Robert Parker <robbybparker@gmail.com>
2020-10-26 23:25:09 -0400

    add constructor test with component_objects

--- idaes/apps/caprese/tests/test_model.py
+++ idaes/apps/caprese/tests/test_model.py
@@ -39,6 +39,16 @@ from idaes.apps.caprese.model import (
         IndexedDynamicBlock,
         _DynamicBlockData,
         )
+from idaes.apps.caprese.common.config import VariableCategory
+from idaes.apps.caprese.nmpc_var import (
+        NmpcVar,
+        DiffVar,
+        DerivVar,
+        AlgVar,
+        InputVar,
+        FixedVar,
+        MeasuredVar,
+        )
 import idaes.logger as idaeslog
 import random
 import pytest
@@ -77,11 +87,7 @@ class TestDynamicBlock(object):
         assert hasattr(block, 'vardata_map')
 
         subblocks = [
-                #model,
-                # FIXME: I am temporarily adding model via 
-                # super(_BlockData).__setattr__ so I can add the same
-                # model multiple times without creating a reference.
-                # This needs to change.
+                block.mod,
                 block.vectors,
                 block.DIFFERENTIAL_BLOCK,
                 block.ALGEBRAIC_BLOCK,
@@ -102,18 +108,24 @@ class TestDynamicBlock(object):
         assert block.v in ComponentSet(identify_variables(block.c.expr))
 
     def test_construct_indexed(self):
-        model = make_model(horizon=1, nfe=2)
-        time = model.time
-        t0 = time.first()
-        inputs = [model.flow_in[t0]]
-        measurements = [model.conc[0,'A'], model.conc[0,'B']]
-        model.block_set = aml.Set(initialize=[0,1,2])
+        block_set = aml.Set(initialize=[0,1,2])
+        block_set.construct()
+        horizon_map = {0: 1., 1: 3., 2: 5.}
+        nfe_map = {0: 2, 1: 6, 2: 10}
+        model_map = {i: make_model(horizon_map[i], nfe_map[i])
+                for i in block_set}
+        time_map = {i: model_map[i].time for i in block_set}
+        inputs_map = {i: [model_map[i].flow_in[0]] for i in block_set}
+        measurements_map = {
+                i: [model_map[i].conc[0,'A'], model_map[i].conc[0,'B']]
+                for i in block_set
+                }
         block = DynamicBlock(
-                model.block_set,
-                model=model,
-                time=time,
-                inputs=inputs,
-                measurements=measurements,
+                block_set,
+                model=model_map,
+                time=time_map,
+                inputs=inputs_map,
+                measurements=measurements_map,
                 )
         assert type(block) is IndexedDynamicBlock
         assert isinstance(block, DynamicBlock)
@@ -123,40 +135,22 @@ class TestDynamicBlock(object):
         # Are blocks constructed sparse by default?
         assert all(b.parent_component() is block for b in block.values())
 
-        for i in model.block_set:
+        for i in block_set:
             assert i in block
 
-        for b in block.values():
-            assert b.mod is model
-            # Bug here:
-            # I am attaching model to block multiple times.
-            # I need to either:
-            # 1. allow a user to initialize an IndexedDynamicBlock with a
-            #    dict of models, or a rule (rule should already be possible)
-            # 2a. attach model as a reference
-            #    ^ This is not great as creating a reference to a scalar
-            #      component leads to some unintuitive behavior.
-            #      (Need to use the `None` index)
-            # 2b. Use __setattr__ to set the model attribute.
-            #     ^ Currently using this while I decide on the right
-            #       alternative. Note that this precludes some behavior
-            #       I want, such as being able to call solve on these
-            #       blocks.
-            # 3. clone the user's model
-            assert b.time is time
-            assert all(i1 is i2 for i1, i2 in zip(b._inputs, inputs))
-            assert all(i1 is i2 for i1, i2 in zip(b._measurements, measurements))
+        for i, b in block.items():
+            assert b.mod is model_map[i]
+            assert b.time is time_map[i]
+            assert all(i1 is i2 for i1, i2 in zip(b._inputs, inputs_map[i]))
+            assert all(i1 is i2 for i1, i2 in 
+                    zip(b._measurements, measurements_map[i]))
 
             assert hasattr(b, 'category_dict')
             assert hasattr(b, 'measured_vars')
             assert hasattr(b, 'vardata_map')
 
             subblocks = [
-                    #model,
-                    # FIXME: I am temporarily adding model via 
-                    # super(_BlockData).__setattr__ so I can add the same
-                    # model multiple times without creating a reference.
-                    # This needs to change.
+                    b.mod,
                     b.vectors,
                     b.DIFFERENTIAL_BLOCK,
                     b.ALGEBRAIC_BLOCK,
@@ -215,8 +209,11 @@ class TestDynamicBlock(object):
         for i, b in block.items():
             assert b.mod is model_map[i]
             assert b.time is model_map[i].time
-#            assert all(i1 is i2 for i1, i2 in zip(b._inputs, inputs))
-#            assert all(i1 is i2 for i1, i2 in zip(b._measurements, measurements))
+            t0 = b.time.first()
+            assert all(i1 is i2 for i1, i2 in zip(b._inputs, 
+                [model_map[i].flow_in[t0]]))
+            assert all(i1 is i2 for i1, i2 in zip(b._measurements, 
+                [model_map[i].conc[t0,'A'], model_map[i].conc[t0,'B']]))
 
             assert hasattr(b, 'category_dict')
             assert hasattr(b, 'measured_vars')
@@ -224,10 +221,6 @@ class TestDynamicBlock(object):
 
             subblocks = [
                     b.mod,
-                    # FIXME: I am temporarily adding model via 
-                    # super(_BlockData).__setattr__ so I can add the same
-                    # model multiple times without creating a reference.
-                    # This needs to change.
                     b.vectors,
                     b.DIFFERENTIAL_BLOCK,
                     b.ALGEBRAIC_BLOCK,
@@ -247,8 +240,69 @@ class TestDynamicBlock(object):
             assert b.v.value == 3
             assert b.v in ComponentSet(identify_variables(b.c.expr))
 
-    # Make sure we can construct as a simple or indexed block
-    # - has all the attributes we expect in these cases.
-    # Make sure we can construct with a rule
-    # Make sure we can attach as a subblock
-    # Make sure we can add components
+    def test_init(self):
+        m = make_small_model()
+        time = m.time
+        t0 = time.first()
+        helper = DynamicBlock(
+                model=m,
+                time=time,
+                inputs=[m.flow_in[0]],
+                measurements=[m.conc[0,'A'], m.conc[0,'B']])
+        helper.construct()
+        assert hasattr(helper, 'category_dict')
+        assert hasattr(helper, 'measured_vars')
+        assert hasattr(helper, 'vardata_map')
+
+        # Make sure category dict contains variables we expect
+        assert VariableCategory.DIFFERENTIAL in helper.category_dict
+        assert VariableCategory.ALGEBRAIC in helper.category_dict
+        assert VariableCategory.DERIVATIVE in helper.category_dict
+        assert VariableCategory.INPUT in helper.category_dict
+        assert VariableCategory.FIXED in helper.category_dict
+    
+        pred_diff_vars = ComponentSet((
+                m.conc[t0,'A'],
+                m.conc[t0,'B'],
+                ))
+        diff_vars = list(helper.component_objects(DiffVar))
+        assert len(pred_diff_vars) == len(diff_vars)
+        for var in diff_vars:
+            assert var[t0] in pred_diff_vars
+
+        pred_alg_vars = ComponentSet((
+                m.flow_out[t0],
+                m.rate[t0,'A'],
+                m.rate[t0,'B'],
+                ))
+        alg_vars = list(helper.component_objects(AlgVar))
+        assert len(pred_alg_vars) == len(alg_vars)
+        for var in alg_vars:
+            assert var[t0] in pred_alg_vars
+
+        pred_input_vars = ComponentSet((
+                m.flow_in[t0],
+                ))
+        input_vars = list(helper.component_objects(InputVar))
+        assert len(pred_input_vars) == len(input_vars)
+        for var in input_vars:
+            assert var[t0] in pred_input_vars
+
+        pred_fixed_vars = ComponentSet((
+                m.conc_in[t0,'A'],
+                m.conc_in[t0,'B'],
+                ))
+        fixed_vars = list(helper.component_objects(FixedVar))
+        assert len(pred_fixed_vars) == len(fixed_vars)
+        for var in fixed_vars:
+            assert var[t0] in pred_fixed_vars
+
+        pred_deriv_vars = ComponentSet((
+                m.dcdt[t0,'A'],
+                m.dcdt[t0,'B'],
+                ))
+        deriv_vars = list(helper.component_objects(DerivVar))
+        assert len(pred_deriv_vars) == len(deriv_vars)
+        for var in deriv_vars:
+            assert var[t0] in pred_deriv_vars
+
