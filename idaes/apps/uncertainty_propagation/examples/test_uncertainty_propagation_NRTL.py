cdad3ece21645ec961924928654cb6a176a55215
Jangho Park <janghopark@lbl.gov>
2020-12-17 10:26:17 -0800

    add examples
    add two examples

--- idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_NRTL.py
+++ idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_NRTL.py
@@ -11,21 +11,21 @@
 # at the URL "https://github.com/IDAES/idaes-pse".
 ##############################################################################
 
-# from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
-# import pandas as pd
-# from NRTL_model_scripts import NRTL_model, NRTL_model_opt
+from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
+import pandas as pd
+from NRTL_model_scripts import NRTL_model, NRTL_model_opt
 
 
-# variable_name = ["fs.properties.tau[benzene,toluene]", "fs.properties.tau[toluene,benzene]"]
+variable_name = ["fs.properties.tau[benzene,toluene]", "fs.properties.tau[toluene,benzene]"]
 
-# #  https://github.com/IDAES/examples-pse/blob/main/src/Tutorials/Advanced/ParamEst/BT_NRTL_dataset.csv
-# data = pd.read_csv('BT_NRTL_dataset.csv')
+#  https://github.com/IDAES/examples-pse/blob/main/src/Tutorials/Advanced/ParamEst/BT_NRTL_dataset.csv
+data = pd.read_csv('BT_NRTL_dataset.csv')
 
-# def SSE(model, data):
-#     expr = ((float(data["vap_benzene"]) -
-#              model.fs.flash.vap_outlet.mole_frac_comp[0, "benzene"])**2 +
-#             (float(data["liq_benzene"]) -
-#              model.fs.flash.liq_outlet.mole_frac_comp[0, "benzene"])**2)
-#     return expr*1E4
+def SSE(model, data):
+    expr = ((float(data["vap_benzene"]) -
+             model.fs.flash.vap_outlet.mole_frac_comp[0, "benzene"])**2 +
+            (float(data["liq_benzene"]) -
+             model.fs.flash.liq_outlet.mole_frac_comp[0, "benzene"])**2)
+    return expr*1E4
 
-# obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(NRTL_model,NRTL_model_opt, data, variable_name, SSE)
+obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(NRTL_model,NRTL_model_opt, data, variable_name, SSE)
--- idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_rooney.py
+++ idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_rooney.py
@@ -11,23 +11,23 @@
 # at the URL "https://github.com/IDAES/idaes-pse".
 ##############################################################################
 
-# from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
-# import pandas as pd
-# from rooney_biegler import rooney_biegler_model,rooney_biegler_model_opt
+from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
+import pandas as pd
+from rooney_biegler import rooney_biegler_model,rooney_biegler_model_opt
 
 
-# variable_name = ['asymptote', 'rate_constant']
-# data = pd.DataFrame(data=[[1,8.3],
-#                           [2,10.3],
-#                           [3,19.0],
-#                           [4,16.0],
-#                           [5,15.6],
-#                           [7,19.8]],
-#                     columns=['hour', 'y'])
+variable_name = ['asymptote', 'rate_constant']
+data = pd.DataFrame(data=[[1,8.3],
+                          [2,10.3],
+                          [3,19.0],
+                          [4,16.0],
+                          [5,15.6],
+                          [7,19.8]],
+                    columns=['hour', 'y'])
 
-# def SSE(model, data):
-#     expr = sum((data.y[i] - model.response_function[data.hour[i]])**2 for i in data.index)
-#     return expr
+def SSE(model, data):
+    expr = sum((data.y[i] - model.response_function[data.hour[i]])**2 for i in data.index)
+    return expr
 
 
-# obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(rooney_biegler_model,rooney_biegler_model_opt, data, variable_name, SSE)
+obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(rooney_biegler_model,rooney_biegler_model_opt, data, variable_name, SSE)
