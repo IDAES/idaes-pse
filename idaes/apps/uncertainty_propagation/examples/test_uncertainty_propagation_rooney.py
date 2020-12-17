0450a65f571701ea9ff8ba711226c713099c5a38
Jangho Park <janghopark@lbl.gov>
2020-12-17 10:11:16 -0800

    minor update
    remove examples

--- idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_rooney.py
+++ idaes/apps/uncertainty_propagation/examples/test_uncertainty_propagation_rooney.py
@@ -10,34 +10,24 @@
 # license information, respectively. Both files are also available online
 # at the URL "https://github.com/IDAES/idaes-pse".
 ##############################################################################
-from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
-import pandas as pd
-from rooney_biegler import rooney_biegler_model,rooney_biegler_model_opt
+
+# from idaes.apps.uncertainty_propagation.uncertainties import quantify_propagate_unucertainty
+# import pandas as pd
+# from rooney_biegler import rooney_biegler_model,rooney_biegler_model_opt
 
 
-variable_name = ['asymptote', 'rate_constant']
-data = pd.DataFrame(data=[[1,8.3],
-                          [2,10.3],
-                          [3,19.0],
-                          [4,16.0],
-                          [5,15.6],
-                          [7,19.8]],
-                    columns=['hour', 'y'])
+# variable_name = ['asymptote', 'rate_constant']
+# data = pd.DataFrame(data=[[1,8.3],
+#                           [2,10.3],
+#                           [3,19.0],
+#                           [4,16.0],
+#                           [5,15.6],
+#                           [7,19.8]],
+#                     columns=['hour', 'y'])
 
-def SSE(model, data):
-    expr = sum((data.y[i] - model.response_function[data.hour[i]])**2 for i in data.index)
-    return expr
-
-
-obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(rooney_biegler_model,rooney_biegler_model_opt, data, variable_name, SSE)
-
-print("\nEstiamted theta")
-print(theta)
-print("\nCovarinace of theta")
-print(cov)
-print("\nError propagation in the objective function, var(f(theta))")
-print(propagation_f)
-print("\nError propagation in the constraints, var(c(theta))")
-print(propagation_c)
+# def SSE(model, data):
+#     expr = sum((data.y[i] - model.response_function[data.hour[i]])**2 for i in data.index)
+#     return expr
 
 
+# obj, theta, cov, propagation_f, propagation_c =  quantify_propagate_unucertainty(rooney_biegler_model,rooney_biegler_model_opt, data, variable_name, SSE)
