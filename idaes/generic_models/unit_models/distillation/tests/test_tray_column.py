db2b2d2c2ca0733032ca5d2d1a76616f0b091a03
jghouse88 <jaffer.ghouse@netl.doe.gov>
2020-12-18 14:25:50 -0500

    updating values for generic test

--- idaes/generic_models/unit_models/distillation/tests/test_tray_column.py
+++ idaes/generic_models/unit_models/distillation/tests/test_tray_column.py
@@ -109,8 +109,6 @@ class TestBTXIdeal():
 
         assert degrees_of_freedom(m.fs.unit) == 0
 
-        raise Exception()
-
         return m
 
     @pytest.fixture(scope="class")
@@ -153,27 +151,27 @@ class TestBTXIdeal():
     def test_build(self, btx_ftpz, btx_fctp):
         assert len(btx_ftpz.fs.unit.config) == 12
 
-        assert btx_ftpz.fs.unit.tray[5].config.is_feed_tray
+        assert btx_ftpz.fs.unit.feed_tray.config.is_feed_tray
 
         assert hasattr(btx_ftpz.fs.unit, "condenser")
         assert hasattr(btx_ftpz.fs.unit, "reboiler")
 
-        assert hasattr(btx_ftpz.fs.unit, "liq_stream")
-        assert hasattr(btx_ftpz.fs.unit, "vap_stream")
+        assert hasattr(btx_ftpz.fs.unit, "rectification_section")
+        assert hasattr(btx_ftpz.fs.unit, "stripping_section")
 
         assert len(btx_fctp.fs.unit.config) == 12
 
-        assert btx_fctp.fs.unit.tray[5].config.is_feed_tray
+        assert btx_fctp.fs.unit.feed_tray.config.is_feed_tray
 
         assert hasattr(btx_fctp.fs.unit, "condenser")
         assert hasattr(btx_fctp.fs.unit, "reboiler")
 
-        assert hasattr(btx_fctp.fs.unit, "liq_stream")
-        assert hasattr(btx_fctp.fs.unit, "vap_stream")
+        assert hasattr(btx_fctp.fs.unit, "rectification_section")
+        assert hasattr(btx_fctp.fs.unit, "stripping_section")
 
     @pytest.mark.solver
     @pytest.mark.component
-    def test_initialize(self, btx_ftpz, btx_fctp):
+    def test_initialize(self, btx_ftpz):
         initialization_tester(btx_ftpz)
 
     @pytest.mark.solver
@@ -224,106 +222,106 @@ class TestBTXIdeal():
                       bottoms.pressure[0]))
 
 
-# class TestBTXIdealGeneric():
-#     @pytest.fixture(scope="class")
-#     def btx_ftpz_generic(self):
-#         m = ConcreteModel()
-#         m.fs = FlowsheetBlock(default={"dynamic": False})
-#         m.fs.properties = GenericParameterBlock(default=configuration)
-#
-#         m.fs.unit = TrayColumn(default={
-#                                "number_of_trays": 10,
-#                                "feed_tray_location": 5,
-#                                "condenser_type": CondenserType.totalCondenser,
-#                                "condenser_temperature_spec":
-#                                    TemperatureSpec.atBubblePoint,
-#                                "property_package": m.fs.properties,
-#                                "has_heat_transfer": False,
-#                                "has_pressure_change": False})
-#
-#         # Inlet feed conditions
-#         m.fs.unit.feed.flow_mol.fix(100)
-#         m.fs.unit.feed.temperature.fix(368)
-#         m.fs.unit.feed.pressure.fix(101325)
-#         m.fs.unit.feed.mole_frac_comp[0, "benzene"].fix(0.5)
-#         m.fs.unit.feed.mole_frac_comp[0, "toluene"].fix(0.5)
-#
-#         # unit level inputs
-#         m.fs.unit.condenser.reflux_ratio.fix(1.4)
-#         m.fs.unit.condenser.condenser_pressure.fix(101325)
-#
-#         m.fs.unit.reboiler.boilup_ratio.fix(1.3)
-#
-#         assert degrees_of_freedom(m) == 0
-#
-#         return m
-#
-#     @pytest.mark.unit
-#     @pytest.mark.generic
-#     def test_build(self, btx_ftpz_generic):
-#         assert len(btx_ftpz_generic.fs.unit.config) == 12
-#
-#         assert btx_ftpz_generic.fs.unit.tray[5].config.is_feed_tray
-#
-#         assert hasattr(btx_ftpz_generic.fs.unit, "condenser")
-#         assert hasattr(btx_ftpz_generic.fs.unit, "reboiler")
-#
-#         assert hasattr(btx_ftpz_generic.fs.unit, "liq_stream")
-#         assert hasattr(btx_ftpz_generic.fs.unit, "vap_stream")
-#
-#         assert len(btx_ftpz_generic.fs.unit.config) == 12
-#
-#     @pytest.mark.solver
-#     @pytest.mark.generic
-#     @pytest.mark.component
-#     def test_initialize(self, btx_ftpz_generic):
-#         initialization_tester(btx_ftpz_generic)
-#
-#     @pytest.mark.solver
-#     @pytest.mark.generic
-#     @pytest.mark.skipif(solver is None, reason="Solver not available")
-#     @pytest.mark.component
-#     def test_solve(self, btx_ftpz_generic):
-#         results = solver.solve(btx_ftpz_generic)
-#
-#         assert results.solver.termination_condition == \
-#             TerminationCondition.optimal
-#         assert results.solver.status == SolverStatus.ok
-#
-#     @pytest.mark.solver
-#     @pytest.mark.generic
-#     @pytest.mark.skipif(solver is None, reason="Solver not available")
-#     @pytest.mark.component
-#     def test_solution(self, btx_ftpz_generic):
-#
-#         # Distillate port - btx_ftpz
-#         assert (pytest.approx(47.45, rel=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.condenser.distillate.flow_mol[0]))
-#         assert (pytest.approx(0.8882, rel=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.condenser.
-#                       distillate.mole_frac_comp[0, "benzene"]))
-#         assert (pytest.approx(0.1118, rel=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.condenser.
-#                       distillate.mole_frac_comp[0, "toluene"]))
-#         assert (pytest.approx(355.642, abs=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.condenser.
-#                       distillate.temperature[0]))
-#         assert (pytest.approx(101325, abs=1e-3) ==
-#                 value(btx_ftpz_generic.fs.unit.condenser.
-#                       distillate.pressure[0]))
-#
-#         # Bottoms port - btx_ftpz
-#         assert (pytest.approx(52.553, rel=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.reboiler.bottoms.flow_mol[0]))
-#         assert (pytest.approx(0.1495, rel=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.reboiler.
-#                       bottoms.mole_frac_comp[0, "benzene"]))
-#         assert (pytest.approx(0.8504, rel=1e-3) ==
-#                 value(btx_ftpz_generic.fs.unit.reboiler.
-#                       bottoms.mole_frac_comp[0, "toluene"]))
-#         assert (pytest.approx(377.337, abs=1e-2) ==
-#                 value(btx_ftpz_generic.fs.unit.reboiler.
-#                       bottoms.temperature[0]))
-#         assert (pytest.approx(101325, abs=1e-3) ==
-#                 value(btx_ftpz_generic.fs.unit.reboiler.
-#                       bottoms.pressure[0]))
+class TestBTXIdealGeneric():
+    @pytest.fixture(scope="class")
+    def btx_ftpz_generic(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(default={"dynamic": False})
+        m.fs.properties = GenericParameterBlock(default=configuration)
+
+        m.fs.unit = TrayColumn(default={
+                               "number_of_trays": 10,
+                               "feed_tray_location": 5,
+                               "condenser_type": CondenserType.totalCondenser,
+                               "condenser_temperature_spec":
+                                   TemperatureSpec.atBubblePoint,
+                               "property_package": m.fs.properties,
+                               "has_heat_transfer": False,
+                               "has_pressure_change": False})
+
+        # Inlet feed conditions
+        m.fs.unit.feed.flow_mol.fix(100)
+        m.fs.unit.feed.temperature.fix(368)
+        m.fs.unit.feed.pressure.fix(101325)
+        m.fs.unit.feed.mole_frac_comp[0, "benzene"].fix(0.5)
+        m.fs.unit.feed.mole_frac_comp[0, "toluene"].fix(0.5)
+
+        # unit level inputs
+        m.fs.unit.condenser.reflux_ratio.fix(1.4)
+        m.fs.unit.condenser.condenser_pressure.fix(101325)
+
+        m.fs.unit.reboiler.boilup_ratio.fix(1.3)
+
+        assert degrees_of_freedom(m) == 0
+
+        return m
+
+    @pytest.mark.unit
+    @pytest.mark.generic
+    def test_build(self, btx_ftpz_generic):
+        assert len(btx_ftpz_generic.fs.unit.config) == 12
+
+        assert btx_ftpz_generic.fs.unit.feed_tray.config.is_feed_tray
+
+        assert hasattr(btx_ftpz_generic.fs.unit, "condenser")
+        assert hasattr(btx_ftpz_generic.fs.unit, "reboiler")
+
+        assert hasattr(btx_ftpz_generic.fs.unit, "rectification_section")
+        assert hasattr(btx_ftpz_generic.fs.unit, "stripping_section")
+
+        assert len(btx_ftpz_generic.fs.unit.config) == 12
+
+    @pytest.mark.solver
+    @pytest.mark.generic
+    @pytest.mark.component
+    def test_initialize(self, btx_ftpz_generic):
+        initialization_tester(btx_ftpz_generic)
+
+    @pytest.mark.solver
+    @pytest.mark.generic
+    @pytest.mark.skipif(solver is None, reason="Solver not available")
+    @pytest.mark.component
+    def test_solve(self, btx_ftpz_generic):
+        results = solver.solve(btx_ftpz_generic)
+
+        assert results.solver.termination_condition == \
+            TerminationCondition.optimal
+        assert results.solver.status == SolverStatus.ok
+
+    @pytest.mark.solver
+    @pytest.mark.generic
+    @pytest.mark.skipif(solver is None, reason="Solver not available")
+    @pytest.mark.component
+    def test_solution(self, btx_ftpz_generic):
+
+        # Distillate port - btx_ftpz
+        assert (pytest.approx(49.28, rel=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.condenser.distillate.flow_mol[0]))
+        assert (pytest.approx(0.8784, rel=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.condenser.
+                      distillate.mole_frac_comp[0, "benzene"]))
+        assert (pytest.approx(0.1215, rel=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.condenser.
+                      distillate.mole_frac_comp[0, "toluene"]))
+        assert (pytest.approx(355.853, abs=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.condenser.
+                      distillate.temperature[0]))
+        assert (pytest.approx(101325, abs=1e-3) ==
+                value(btx_ftpz_generic.fs.unit.condenser.
+                      distillate.pressure[0]))
+
+        # Bottoms port - btx_ftpz
+        assert (pytest.approx(50.711, rel=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.reboiler.bottoms.flow_mol[0]))
+        assert (pytest.approx(0.1322, rel=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.reboiler.
+                      bottoms.mole_frac_comp[0, "benzene"]))
+        assert (pytest.approx(0.8677, rel=1e-3) ==
+                value(btx_ftpz_generic.fs.unit.reboiler.
+                      bottoms.mole_frac_comp[0, "toluene"]))
+        assert (pytest.approx(378.0433, abs=1e-2) ==
+                value(btx_ftpz_generic.fs.unit.reboiler.
+                      bottoms.temperature[0]))
+        assert (pytest.approx(101325, abs=1e-3) ==
+                value(btx_ftpz_generic.fs.unit.reboiler.
+                      bottoms.pressure[0]))
