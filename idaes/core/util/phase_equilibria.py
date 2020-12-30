6a0bc0eb6a2c12f1371ca234eaa52137842a1d09
Alejandro Garciadiego <agarciad@nd.edu>
2020-12-30 16:27:48 -0500

    adressing comments adding CO2-IL

--- idaes/core/util/phase_equilibria.py
+++ idaes/core/util/phase_equilibria.py
@@ -19,11 +19,14 @@ plots.
 from pyomo.environ import (ConcreteModel,
                            SolverFactory,
                            value,
+                           Var,
                            Constraint,
                            Expression,
                            units as pyunits)
 import idaes.logger as idaeslog
 from pyomo.opt import TerminationCondition, SolverStatus
+
+import idaes.logger as idaeslog
 
 # Import plotting functions
 import matplotlib.pyplot as plt
@@ -31,7 +34,7 @@ import numpy as np
 
 # Author: Alejandro Garciadiego
 def Txy_diagram(
-    comp_1, comp_2, Press, Num_points, model, figure_name = None,
+    model, component_1, component_2, pressure, num_points = 20, temperature = 298.15,  figure_name = None,
     print_legend = True, include_pressure = False, print_level=idaeslog.NOTSET,
     solver_op={'tol': 1e-6}):
 
@@ -41,10 +44,11 @@ def Txy_diagram(
     been generated calls build_txy_diagrams() to create a plot.
 
     Args:
-        comp_1: Component which composition will be plotted in x axis
-        comp_2: Component which composition will decrease in x axis
-        Press: Pressure at which the bubble and drew temperatures will be calculated
-        Num_points: Number of data point to be calculated
+        component_1: Component which composition will be plotted in x axis
+        component_2: Component which composition will decrease in x axis
+        pressure: Pressure at which the bubble and drew temperatures will be calculated
+        temperature: Temperature at which to initialize state block
+        num_points: Number of data point to be calculated
         properties: property package which contains parameters to calculate bubble
         and dew temperatures for the mixture of the compnents specified.
         figure_name: if a figure name is included the plot will save with the name
@@ -60,15 +64,15 @@ def Txy_diagram(
         Plot
     """
     # Run txy_ data funtion to obtain bubble and dew twmperatures
-    Txy_data_to_plot = Txy_data(comp_1, comp_2, Press, Num_points, model,
+    Txy_data_to_plot = Txy_data(model, component_1, component_2, pressure, num_points, temperature,
                                 print_level, solver_op)
 
     # Run diagrams function to convert t-x-y data into a plot
     build_txy_diagrams(Txy_data_to_plot, figure_name, print_legend, include_pressure)
 
 # Author: Alejandro Garciadiego
-def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTSET,
-                solver_op={'tol': 1e-6}):
+def Txy_data(model, component_1, component_2, pressure, num_points = 20, temperature = 298.15,
+            print_level=idaeslog.NOTSET, solver_op={'tol': 1e-6}):
     """
     Function to generate T-x-y data. The function builds a state block and extracts
     bubble and dew temperatures at P pressure for N number of compositions.
@@ -76,10 +80,11 @@ def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTS
     create a smoother looking plot.
 
     Args:
-        comp_1: Component 1
-        comp_2: Component 2
-        Press: Pressure at which the bubble and drew temperatures will be calculates
-        Num_points: Number of data point to be calculated
+        component_1: Component 1
+        component_2: Component 2
+        pressure: Pressure at which the bubble and drew temperatures will be calculates
+        temperature: Temperature at which to initialize state block
+        num_points: Number of data point to be calculated
         model: Model wit intialized Property package which contains data to calculate
         bubble and dew temperatures for  component 1 and component 2
         print_level: printing level from initialization
@@ -91,7 +96,7 @@ def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTS
     """
 
     components = list(model.params.component_list)
-    components_used = [comp_1, comp_2]
+    components_used = [component_1, component_2]
     components_not_used = list(set(components) - set(components_used))
 
     # Add properties parameter blocks to the flowsheet with specifications
@@ -100,21 +105,21 @@ def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTS
         [1], default = {"defined_state": True})
 
     # Set intial concentration of component 1 close to 1
-    x = 0.995
+    x = 0.99
 
     # Set conditions for flash unit model
-    model.props[1].mole_frac_comp[comp_1].fix(x)
+    model.props[1].mole_frac_comp[component_1].fix(x)
 
     for i in components_not_used:
         model.props[1].mole_frac_comp[i].fix(1e-5)
 
     xs = sum(value(model.props[1].mole_frac_comp[i]) for i in components_not_used)
 
-    model.props[1].mole_frac_comp[comp_2].fix(1 - x - xs)
+    model.props[1].mole_frac_comp[component_2].fix(1 - x - xs)
 
     model.props[1].flow_mol.fix(1)
-    model.props[1].temperature.fix(298.15)
-    model.props[1].pressure.fix(Press)
+    model.props[1].temperature.fix(temperature)
+    model.props[1].pressure.fix(pressure)
 
     # Initialize flash unit model
     model.props.initialize(optarg=solver_op, outlvl=print_level)
@@ -122,7 +127,7 @@ def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTS
     solver = SolverFactory('ipopt')
 
     # Create an array of compositions with N number of points
-    x_d = np.linspace(x, 1 - x - xs, Num_points)
+    x_d = np.linspace(x, 1 - x - xs, num_points)
 
     # Create emprty arrays for concentration, bubble temperature and dew temperature
     X = []
@@ -136,25 +141,38 @@ def Txy_data(comp_1, comp_2, Press, Num_points, model, print_level=idaeslog.NOTS
     count = 1
     # Create and run loop to calculate temperatures at every composition
     for i in range(len(x_d)):
-        model.props[1].mole_frac_comp[comp_1].fix(x_d[i])
-        model.props[1].mole_frac_comp[comp_2].fix(1 - x_d[i] - xs)
-
+        model.props[1].mole_frac_comp[component_1].fix(x_d[i])
+        model.props[1].mole_frac_comp[component_2].fix(1 - x_d[i] - xs)
+        # model.display()
         # solve the model
         status = solver.solve(model, tee = False)
-
+        # model.display()
         # If solution is optimal store the concentration, and calculated temperatures in the created arrays
         if (status.solver.status == SolverStatus.ok) and (status.solver.termination_condition == TerminationCondition.optimal):
-            Tbubb.append(value(model.props[1].temperature_bubble['Vap', 'Liq']))
-            Tdew.append(value(model.props[1].temperature_dew['Vap', 'Liq']))
+
+            print('Case: ',count,' Optimal. ', component_1, 'x = {:.2f}'.format(x_d[i]))
+
+            if hasattr(model.props[1], "_mole_frac_tdew") and hasattr(model.props[1], "_mole_frac_tbub"):
+                Tbubb.append(value(model.props[1].temperature_bubble['Vap', 'Liq']))
+                Tdew.append(value(model.props[1].temperature_dew['Vap', 'Liq']))
+
+            elif hasattr(model.props[1], "_mole_frac_tdew"):
+                print('One of the components only exists in vapor phase.')
+                Tdew.append(value(model.props[1].temperature_dew['Vap', 'Liq']))
+
+            elif hasattr(model.props[1], "_mole_frac_tbub"):
+                print('One of the components only exists in liquid phase.')
+                Tbubb.append(value(model.props[1].temperature_bubble['Vap', 'Liq']))
+
             X.append(x_d[i])
-            print('Case: ',count,' Optimal. ', comp_1, 'x = {:.2f}'.format(x_d[i]))
+
         # If the solver did not solve to an optimal solution, do not store the data point
         else:
-            print('Case: ',count,' No Result', comp_1, 'x = {:.2f}'.format(x_d[i]))
+            print('Case: ',count,' No Result', component_1, 'x = {:.2f}'.format(x_d[i]))
         count += 1
 
     # Call TXYData function and store the data in TD class
-    TD = TXYDataClass(comp_1, comp_2, Punit, Tunit, Press)
+    TD = TXYDataClass(component_1, component_2, Punit, Tunit, pressure)
     TD.TBubb = Tbubb
     TD.TDew = Tdew
     TD.x = X
@@ -168,29 +186,29 @@ class TXYDataClass:
     Write data needed for build_txy_diagrams() into a class. The class can be
     obtained by running Txy_data() or by assigining values to the class.
     """
-    def __init__(self, Comp_1, Comp_2, Punits, Tunits, Press):
+    def __init__(self, component_1, component_2, Punits, Tunits, pressure):
         """
         Args:
-            Comp_1: Component 1
-            Comp_2: Component 2
+            component_1: Component 1
+            component_2: Component 2
             Punits: Initial value of heat of hot utility
             Tunits: Initial value of heat to be removed by cold utility
-            Press: Pressure at which the T-x-y data was evaluated
+            pressure: Pressure at which the T-x-y data was evaluated
 
         Returns:
             (Class): A class containing the T-x-y data
         """
 
         # Build
-        self.Component_1 = Comp_1
-        self.Component_2 = Comp_2
+        self.Component_1 = component_1
+        self.Component_2 = component_2
 
         # Assign units of pressure and temperature
         self.Punits = Punits
         self.Tunits = Tunits
 
         # Assign pressure at which the data has been calculated
-        self.P = Press
+        self.P = pressure
 
         # Create arrays for data
         self.TBubb = []
@@ -246,21 +264,51 @@ def build_txy_diagrams(txy_data, figure_name=None, print_legend=True, include_pr
     # Declare a plot and it's size
     ig, ax = plt.subplots(figsize=(12,8))
 
-    if include_pressure == True:
-        # Plot results for bubble temperature
-        ax.plot(txy_data.x, txy_data.TBubb,"r", label="Bubble Temp P = "+
-        str(txy_data.Press) + " " +
-                str(txy_data.Punits), linewidth=1.5)
+    if len(txy_data.TBubb) and len(txy_data.TDew) > 0:
+        if include_pressure == True:
+            # Plot results for bubble temperature
 
-        # Plot results for dew temperature
-        ax.plot(txy_data.x, txy_data.TDew, "b", label="Dew Temp P = " + str(txy_data.Press) + " " +
-                str(txy_data.Punits), linewidth=1.5)
-    else:
-        # Plot results for bubble temperature
-        ax.plot(txy_data.x, txy_data.TBubb, "r", label="Bubble Temp ", linewidth=1.5)
 
-        # Plot results for dew temperature
-        ax.plot(txy_data.x, txy_data.TDew, "b", label="Dew Temp", linewidth=1.5)
+                ax.plot(txy_data.x, txy_data.TBubb,"r", label="Bubble Temp P = "+
+                str(txy_data.Press) + " " +
+                        str(txy_data.Punits), linewidth=1.5)
+
+                # Plot results for dew temperature
+                ax.plot(txy_data.x, txy_data.TDew, "b", label="Dew Temp P = " + str(txy_data.Press) + " " +
+                        str(txy_data.Punits), linewidth=1.5)
+
+        else:
+            # Plot results for bubble temperature
+            ax.plot(txy_data.x, txy_data.TBubb, "r", label="Bubble Temp ", linewidth=1.5)
+
+            # Plot results for dew temperature
+            ax.plot(txy_data.x, txy_data.TDew, "b", label="Dew Temp", linewidth=1.5)
+
+    elif len(txy_data.TDew) == 0:
+
+        if include_pressure == True:
+            # Plot results for bubble temperature
+
+                # Plot results for dew temperature
+                ax.plot(txy_data.x, txy_data.TBubb, "b", label="Dew Temp P = " + str(txy_data.Press) + " " +
+                        str(txy_data.Punits), linewidth=1.5)
+
+        else:
+            # Plot results for dew temperature
+            ax.plot(txy_data.x, txy_data.TBubb, "b", label="Dew Temp", linewidth=1.5)
+
+    elif len(txy_data.TBubb) == 0:
+
+        if include_pressure == True:
+            # Plot results for bubble temperature
+
+                # Plot results for dew temperature
+                ax.plot(txy_data.x, txy_data.TDew, "b", label="Bubble Temp P = " + str(txy_data.Press) + " " +
+                        str(txy_data.Punits), linewidth=1.5)
+
+        else:
+            # Plot results for dew temperature
+            ax.plot(txy_data.x, txy_data.TDew, "b", label="Dew Temp", linewidth=1.5)
 
     # Include grid
     ax.grid()
--- idaes/core/util/tests/test_phase_equilibria.py
+++ idaes/core/util/tests/test_phase_equilibria.py
@@ -16,19 +16,29 @@ This module contains phase equilibria utility functions for use in IDAES models.
 
 import pytest
 
+import idaes.logger as idaeslog
+
 from pyomo.environ import ConcreteModel, Expression, Set, Block, Var
 # Import Pyomo units
 from pyomo.environ import units as pyunits
+
 from idaes.core import LiquidPhase, VaporPhase, Component
+from idaes.core.phases import PhaseType as PT
 
 from idaes.generic_models.properties.core.state_definitions import FTPx
 from idaes.generic_models.properties.core.eos.ideal import Ideal
+from idaes.generic_models.properties.core.eos.ceos import Cubic, CubicType
 from idaes.generic_models.properties.core.phase_equil import smooth_VLE
 from idaes.generic_models.properties.core.phase_equil.bubble_dew import \
-        IdealBubbleDew
-from idaes.generic_models.properties.core.phase_equil.forms import fugacity
+        IdealBubbleDew, LogBubbleDew
+from idaes.generic_models.properties.core.phase_equil.forms import fugacity, log_fugacity
+
 import idaes.generic_models.properties.core.pure.NIST as NIST
+import idaes.generic_models.properties.core.pure.RPP4 as RPP4
+import idaes.generic_models.properties.core.pure.RPP5 as RPP5
+
 from idaes.core import FlowsheetBlock
+
 from idaes.generic_models.properties.core.generic.generic_property import (
         GenericParameterBlock)
 
@@ -36,9 +46,9 @@ from idaes.core.util.phase_equilibria import (TXYDataClass,Txy_data)
 
 # Author: Alejandro Garciadiego
 @pytest.mark.unit
-def test_Txy_data():
+def test_Txy_dataclass():
 
-    TD = TXYDataClass('benzene','toluene',kg / m / s ** 2,K,101325)
+    TD = TXYDataClass('benzene', 'toluene', pyunits.kg / pyunits.m / pyunits.s ** 2, pyunits.K, 101325)
     TD.TBubb = [353.3205, 365.3478, 383.8817]
     TD.TDew = [353.3237, 372.0203, 383.8845]
     TD.x = [0.9999, 0.5, 9.9999]
@@ -109,13 +119,167 @@ def test_Txy_data():
 
     model.params = GenericParameterBlock(default=configuration)
 
-    TD = Txy_data('benzene','toluene', 101325, 3, model)
+    TD = Txy_data(model, 'benzene', 'toluene', 101325, num_points = 3, temperature = 298.15,
+    print_level=idaeslog.CRITICAL, solver_op={'tol': 1e-6})
 
     assert TD.Component_1 == 'benzene'
     assert TD.Component_2 == 'toluene'
     assert TD.Punits == pyunits.kg / pyunits.m / pyunits.s ** 2
     assert TD.Tunits == pyunits.K
     assert TD.P == 101325
-    assert TD.TBubb == [pytest.approx(353.3853, abs=1e-4), pytest.approx(365.2127, abs=1e-4), pytest.approx(383.5312, abs=1e-4)]
-    assert TD.TDew == [pytest.approx(353.5429, abs=1e-2), pytest.approx(371.8702, abs=1e-4), pytest.approx(383.6709, abs=1e-4)]
-    assert TD.x == [pytest.approx(0.995, abs=1e-4), pytest.approx(0.5, abs=1e-4), pytest.approx(0.005, abs=1e-4)]
+    assert TD.TBubb == [pytest.approx(353.4853, abs=1e-4), pytest.approx(365.2127, abs=1e-4), pytest.approx(383.2909, abs=1e-4)]
+    assert TD.TDew == [pytest.approx(353.7978, abs=1e-2), pytest.approx(371.8702, abs=1e-4), pytest.approx(383.5685, abs=1e-4)]
+    assert TD.x == [pytest.approx(0.99, abs=1e-4), pytest.approx(0.5, abs=1e-4), pytest.approx(0.01, abs=1e-4)]
+
+# Author: Alejandro Garciadiego
+@pytest.mark.unit
+def test_Txy_data_no_dew():
+    configuration = {
+        # Specifying components
+    "components": {
+        "bmimPF6": {"type": Component,
+                    "valid_phase_types": PT.liquidPhase,
+                    "phase_equilibrium_form": {("Vap", "Liq"): log_fugacity},
+                    "parameter_data": {
+                        "mw": (284.18E-3, pyunits.kg/pyunits.mol),  # [1]
+                        "pressure_crit": (24e5, pyunits.Pa),  # [1]
+                        "temperature_crit": (860, pyunits.K),  # [1]
+                        "omega": 0.7917}},
+
+        "carbon_dioxide": {"type": Component,
+                  "pressure_sat_comp": NIST,
+                  "phase_equilibrium_form": {("Vap", "Liq"): log_fugacity},
+                  "parameter_data": {
+                      "mw": (44.010E-3, pyunits.kg/pyunits.mol),
+                      "pressure_crit": (71.8e5, pyunits.Pa),
+                      "temperature_crit": (304.1, pyunits.K),
+                      "omega": 0.239,
+                      "pressure_sat_comp_coeff": {"A": (6.81228, None),
+                                                  "B": (1301.679, pyunits.K),
+                                                  "C": (-3.494, pyunits.K)}}}},
+
+        # Specifying phases
+        "phases":  {'Liq': {"type": LiquidPhase,
+                            "equation_of_state": Cubic,
+                            "equation_of_state_options": {
+                                "type": CubicType.PR}},
+                    'Vap': {"type": VaporPhase,
+                            "equation_of_state": Cubic,
+                            "equation_of_state_options": {
+                                "type": CubicType.PR}}},
+
+        # Set base units of measurement
+        "base_units": {"time": pyunits.s,
+                       "length": pyunits.m,
+                       "mass": pyunits.kg,
+                       "amount": pyunits.mol,
+                       "temperature": pyunits.K},
+
+        # Specifying state definition
+        "state_definition": FTPx,
+        "state_bounds": {"flow_mol": (0, 100, 1000, pyunits.mol/pyunits.s),
+                         "temperature": (10, 300, 500, pyunits.K),
+                         "pressure": (5e-4, 1e5, 1e10, pyunits.Pa)},
+        "pressure_ref": (101325, pyunits.Pa),
+        "temperature_ref": (298.15, pyunits.K),
+
+        # Defining phase equilibria
+        "phases_in_equilibrium": [("Vap", "Liq")],
+        "phase_equilibrium_state": {("Vap", "Liq"): smooth_VLE},
+        "bubble_dew_method": LogBubbleDew,
+        "parameter_data": {"PR_kappa": {("bmimPF6", "bmimPF6"): 0.000,
+                                        ("bmimPF6", "carbon_dioxide"): -0.05718,
+                                        ("carbon_dioxide", "carbon_dioxide"): 0.000,
+                                        ("carbon_dioxide", "bmimPF6"): 0.0293}}}
+
+    model = ConcreteModel()
+
+    model.params = GenericParameterBlock(default=configuration)
+
+    TD = Txy_data(model, "carbon_dioxide", "bmimPF6", 101325, num_points = 3, temperature = 150.15,
+    print_level=idaeslog.CRITICAL, solver_op={'tol': 1e-6})
+
+    assert TD.Component_1 == 'carbon_dioxide'
+    assert TD.Component_2 == 'bmimPF6'
+    assert TD.Punits == pyunits.kg / pyunits.m / pyunits.s ** 2
+    assert TD.Tunits == pyunits.K
+    assert TD.P == 101325
+    assert TD.TBubb == [pytest.approx(185.6468, abs=1e-4), pytest.approx(191.4697, abs=1e-4), pytest.approx(331.1625, abs=1e-4)]
+    assert TD.TDew == []
+    assert TD.x == [pytest.approx(0.99, abs=1e-4), pytest.approx(0.5, abs=1e-4), pytest.approx(0.01, abs=1e-4)]
+
+# Author: Alejandro Garciadiego
+@pytest.mark.unit
+def test_Txy_data_no_liq():
+    configuration = {
+        # Specifying components
+        "components": {
+        "methane": {"type": Component,
+                    "valid_phase_types": PT.vaporPhase,
+                    "parameter_data": {
+                        "mw": (16.043E-3, pyunits.kg/pyunits.mol),  # [1]
+                        "pressure_crit": (46e5, pyunits.Pa),  # [1]
+                        "temperature_crit": (190.4, pyunits.K),  # [1]
+                        "omega": 0.011}},
+
+        "ethane": {"type": Component,
+                    "pressure_sat_comp": RPP5,
+                    "phase_equilibrium_form": {("Vap", "Liq"): log_fugacity},
+                    "parameter_data": {
+                        "mw": (30.070E-3, pyunits.kg/pyunits.mol),  # [1]
+                        "pressure_crit": (48.8e5, pyunits.Pa),  # [1]
+                        "temperature_crit": (305.4, pyunits.K),  # [1]
+                        "omega": 0.099,
+                        "pressure_sat_comp_coeff": {"A": (3.95405, None),  # [4]
+                                                    "B": (663.720, pyunits.K),
+                                                    "C": (256.681, pyunits.K)}}}},
+
+    # Specifying phases
+    "phases":  {"Liq": {"type": LiquidPhase,
+                        "equation_of_state": Cubic,
+                        "equation_of_state_options": {
+                            "type": CubicType.PR}},
+                "Vap": {"type": VaporPhase,
+                        "equation_of_state": Cubic,
+                        "equation_of_state_options": {
+                            "type": CubicType.PR}}},
+
+    # Set base units of measurement
+    "base_units": {"time": pyunits.s,
+                   "length": pyunits.m,
+                   "mass": pyunits.kg,
+                   "amount": pyunits.mol,
+                   "temperature": pyunits.K},
+
+    # Specifying state definition
+    "state_definition": FTPx,
+    "state_bounds": {"flow_mol": (0, 100, 1000, pyunits.mol/pyunits.s),
+                     "temperature": (10, 300, 1500, pyunits.K),
+                     "pressure": (5e-4, 1e5, 1e10, pyunits.Pa)},
+    "pressure_ref": (101325, pyunits.Pa),
+    "temperature_ref": (298.15, pyunits.K),
+
+    # Defining phase equilibria
+    "phases_in_equilibrium": [("Vap", "Liq")],
+    "phase_equilibrium_state": {("Vap", "Liq"): smooth_VLE},
+    "bubble_dew_method": LogBubbleDew,
+    "parameter_data": {"PR_kappa": {("methane", "methane"): 0.000,
+                                    ("methane", "ethane"): 0.000,
+                                    ("ethane", "ethane"): 0.000,
+                                    ("ethane", "methane"): 0.000}}}
+
+    model = ConcreteModel()
+
+    model.params = GenericParameterBlock(default=configuration)
+
+    TD = Txy_data(model, 'methane', 'ethane', 101325, num_points = 3, temperature = 350,
+    print_level=idaeslog.CRITICAL, solver_op={'tol': 1e-6})
+
+    assert TD.Component_1 == 'methane'
+    assert TD.Component_2 == 'ethane'
+    assert TD.Punits == pyunits.kg / pyunits.m / pyunits.s ** 2
+    assert TD.Tunits == pyunits.K
+    assert TD.P == 101325
+    assert TD.TBubb == []
+    assert TD.TDew == [pytest.approx(126.9025, abs=1e-2), pytest.approx(172.1489, abs=1e-4), pytest.approx(184.2534, abs=1e-4)]
+    assert TD.x == [pytest.approx(0.99, abs=1e-4), pytest.approx(0.5, abs=1e-4), pytest.approx(0.01, abs=1e-4)]
