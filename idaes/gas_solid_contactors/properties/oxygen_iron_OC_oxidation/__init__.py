74fb26eeaba437c83601f0630c6285d75784113c
Chinedu Okoli <cokoli@ad.scilan.pgh>
2021-01-22 14:57:36 -0500

    Added oxidation property package, example BFB flowsheet and tests for a CLC oxygen carrier (Abad)

---
+++ idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/__init__.py
@@ -0,0 +1,9 @@
+from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+      gas_phase_thermo import (GasPhaseThermoParameterBlock,
+                               GasPhaseThermoStateBlock)
+from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+    solid_phase_thermo import (SolidPhaseThermoParameterBlock,
+                               SolidPhaseThermoStateBlock)
+from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+    hetero_reactions import (HeteroReactionParameterBlock,
+                             ReactionBlock)
