6c1b57b06f298243dc373fccc149fe48584d1c1e
Miguel Zamarripa <miguel.zamarripa-perez@netl.doe.gov>
2021-01-27 11:38:07 -0500

    adding more tests

--- idaes/power_generation/flowsheets/subcritical_power_plant/tests/test_subcritical_flowsheets.py
+++ idaes/power_generation/flowsheets/subcritical_power_plant/tests/test_subcritical_flowsheets.py
@@ -3,11 +3,15 @@ import pyomo.environ as pyo
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.generic_models.properties import iapws95
 
+
+import idaes.power_generation.flowsheets.subcritical_power_plant.\
+    subcritical_power_plant as subcrit_plant
 import idaes.power_generation.flowsheets.subcritical_power_plant.\
     steam_cycle_flowsheet as steam_cycle
 import idaes.power_generation.flowsheets.subcritical_power_plant.\
     subcritical_boiler_flowsheet as blr
-
+import idaes.power_generation.flowsheets.subcritical_power_plant.\
+    subcritical_boiler as recyrc
 import pytest
 
 __author__ = "Boiler Subsystem Team (J. Ma, M. Zamarripa)"
@@ -84,3 +88,58 @@ def test_steam_cycle():
                       - m.fs_main.fs_stc.fwh6.desuperheat.outlet_2.flow_mol[0]  # out to economizer
                       + m.fs_main.fs_stc.makeup_valve.inlet.flow_mol[0]  # in from makeup
                       ))
+
+
+@pytest.mark.integration
+@pytest.mark.solver
+@pytest.mark.skipif(not prop_available, reason="IAPWS not available")
+@pytest.mark.skipif(not solver_available, reason="Solver not available")
+def test_subc_power_plant():
+    m = subcrit_plant.main_steady_state()
+    assert degrees_of_freedom(m) == 0
+    assert (pytest.approx(61634.3740, abs=1e-1) ==
+            pyo.value(m.fs_main.fs_stc.turb.inlet_split.inlet.enth_mol[0]))
+    assert (pytest.approx(14908.39189, abs=1e-2) ==
+            pyo.value(m.fs_main.fs_stc.turb.inlet_split.inlet.flow_mol[0]))
+    assert (pytest.approx(320, abs=1e-2) ==
+            pyo.value(m.fs_main.fs_stc.power_output[0]))
+    assert (pytest.approx(0, abs=1e-2) ==
+            pyo.value(m.fs_main.fs_stc.turb.inlet_split.inlet.flow_mol[0]  # turbine inlet
+                      - m.fs_main.fs_stc.turb.hp_split[14].outlet_1.flow_mol[0]  # out to reheat
+                      + m.fs_main.fs_stc.turb.ip_stages[1].inlet.flow_mol[0]  # in from reheat
+                      - m.fs_main.fs_stc.spray_valve.outlet.flow_mol[0]  # out to attemperator
+                      - m.fs_main.fs_stc.fwh6.desuperheat.outlet_2.flow_mol[0]  # out to economizer
+                      + m.fs_main.fs_stc.makeup_valve.inlet.flow_mol[0]  # in from makeup
+                      ))
+
+
+@pytest.mark.integration
+@pytest.mark.solver
+@pytest.mark.skipif(not prop_available, reason="IAPWS not available")
+@pytest.mark.skipif(not solver_available, reason="Solver not available")
+def test_dynamic_power_plant():
+    # constructing and initializing dynamic power plant
+    # not solving due to simulation time >20 min
+    m = subcrit_plant.get_model(dynamic=True)
+    assert m.dynamic == True
+    assert degrees_of_freedom(m) == 0
+
+
+@pytest.mark.component
+@pytest.mark.solver
+@pytest.mark.skipif(not prop_available, reason="IAPWS not available")
+@pytest.mark.skipif(not solver_available, reason="Solver not available")
+def test_dynamic_steam_cycle():
+    # constructing and initializing dynamic steam cycle flowsheet
+    m = steam_cycle.get_model(dynamic=True)
+    assert m.dynamic == True
+    assert degrees_of_freedom(m) == 7
+
+
+@pytest.mark.unit
+@pytest.mark.solver
+@pytest.mark.skipif(not prop_available, reason="IAPWS not available")
+@pytest.mark.skipif(not solver_available, reason="Solver not available")
+def test_subcritical_recyrculation_system():
+    m = recyrc.main()
+    assert degrees_of_freedom(m) == 0
