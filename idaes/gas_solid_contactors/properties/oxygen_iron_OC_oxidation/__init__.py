a15ca13fa146c95d995d12654bbb2f0c07ae26c5
Chinedu Okoli <cokoli@ad.scilan.pgh>
2021-02-03 23:26:03 -0500

    updated init file of oxidation property package

--- idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/__init__.py
+++ idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/__init__.py
@@ -1,9 +1,9 @@
-from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+from idaes.gas_solid_contactors.properties.oxygen_iron_OC_oxidation. \
       gas_phase_thermo import (GasPhaseThermoParameterBlock,
                                GasPhaseThermoStateBlock)
-from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+from idaes.gas_solid_contactors.properties.oxygen_iron_OC_oxidation. \
     solid_phase_thermo import (SolidPhaseThermoParameterBlock,
                                SolidPhaseThermoStateBlock)
-from idaes.gas_solid_contactors.properties.methane_iron_OC_reduction. \
+from idaes.gas_solid_contactors.properties.oxygen_iron_OC_oxidation. \
     hetero_reactions import (HeteroReactionParameterBlock,
                              ReactionBlock)
