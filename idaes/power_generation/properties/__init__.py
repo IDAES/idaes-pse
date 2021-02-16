acf65d64d96e57a949e057b202a806615891a8b6
Alex Noring <anoring@andrew.cmu.edu>
2021-02-16 14:28:23 -0500

    added natural gas property package and tests

--- idaes/power_generation/properties/__init__.py
+++ idaes/power_generation/properties/__init__.py
@@ -2,3 +2,7 @@ from idaes.power_generation.properties.flue_gas_ideal import (
     FlueGasParameterBlock,
     FlueGasStateBlock,
 )
+from idaes.power_generation.properties.natural_gas_PR import (
+    get_NG_properties,
+    rxn_configuration,
+)
