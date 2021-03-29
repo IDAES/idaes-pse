66fad6d6f8951d94526dd2464061eea1e467cfd3
Andrew Lee <andrew.lee@netl.doe.gov>
2021-03-29 11:55:39 -0400

    Adding Gibbs energy form for calculating equilibrium constants

--- idaes/generic_models/properties/core/reactions/equilibrium_constant.py
+++ idaes/generic_models/properties/core/reactions/equilibrium_constant.py
@@ -52,7 +52,7 @@ class van_t_hoff():
                     # In most cases ,should have _phase_component_set
                     pc_set = parent._phase_component_set
                 else:
-                    # However, for electrolytes need ture species set
+                    # However, for electrolytes need true species set
                     pc_set = parent.true_phase_component_set
 
             for p, j in pc_set:
@@ -97,3 +97,92 @@ class van_t_hoff():
     @staticmethod
     def calculate_scaling_factors(b, rblock):
         return 1/value(rblock.k_eq_ref)
+
+
+# -----------------------------------------------------------------------------
+# Constant dh_rxn and ds_rxn
+class gibbs_energy():
+
+    @staticmethod
+    def build_parameters(rblock, config):
+        parent = rblock.parent_block()
+        units = parent.get_metadata().derived_units
+
+        c_form = config.concentration_form
+        if c_form is None:
+            raise ConfigurationError(
+                "{} concentration_form configuration argument was not set. "
+                "Please ensure that this argument is included in your "
+                "configuration dict.".format(rblock.name))
+        elif (c_form == ConcentrationForm.moleFraction or
+              c_form == ConcentrationForm.massFraction):
+            e_units = None
+        else:
+            order = 0
+
+            try:
+                # This will work for Reaction Packages
+                pc_set = parent.config.property_package._phase_component_set
+            except AttributeError:
+                # Need to allow for inherent reactions in Property Packages
+                if not parent._electrolyte:
+                    # In most cases ,should have _phase_component_set
+                    pc_set = parent._phase_component_set
+                else:
+                    # However, for electrolytes need true species set
+                    pc_set = parent.true_phase_component_set
+
+            for p, j in pc_set:
+                order += rblock.reaction_order[p, j].value
+
+            if (c_form == ConcentrationForm.molarity or
+                    c_form == ConcentrationForm.activity):
+                c_units = units["density_mole"]
+            elif c_form == ConcentrationForm.molality:
+                c_units = units["amount"]*units["mass"]**-1
+            elif c_form == ConcentrationForm.partialPressure:
+                c_units = units["pressure"]
+            else:
+                raise BurntToast(
+                    "{} get_concentration_term received unrecognised "
+                    "ConcentrationForm ({}). This should not happen - please "
+                    "contact the IDAES developers with this bug."
+                    .format(rblock.name, c_form))
+
+            e_units = c_units**order
+
+        rblock._keq_units = e_units
+
+        rblock.dh_rxn_ref = Var(
+                doc="Specific molar heat of reaction at reference state",
+                units=units["energy_mole"])
+        set_param_from_config(rblock, param="dh_rxn_ref", config=config)
+
+        rblock.ds_rxn_ref = Var(
+                doc="Specific molar entropy of reaction at reference state",
+                units=units["entropy_mole"])
+        set_param_from_config(rblock, param="ds_rxn_ref", config=config)
+
+        rblock.T_eq_ref = Var(
+                doc="Reference temperature for equilibrium constant",
+                units=units["temperature"])
+        set_param_from_config(rblock, param="T_eq_ref", config=config)
+
+    @staticmethod
+    def return_expression(b, rblock, r_idx, T):
+        units = rblock.parent_block().get_metadata().derived_units
+
+        R = pyunits.convert(c.gas_constant, to_units=units["gas_constant"])
+
+        return (exp(
+            (-rblock.dh_rxn_ref / (R*T)) +
+            (rblock.ds_rxn_ref / R)) * rblock._keq_units)
+
+    @staticmethod
+    def calculate_scaling_factors(b, rblock):
+        units = rblock.parent_block().get_metadata().derived_units
+        R = pyunits.convert(c.gas_constant, to_units=units["gas_constant"])
+
+        keq_val = value(exp(-rblock.dh_rxn_ref/(R*rblock.T_eq_ref) +
+                            rblock.ds_rxn_ref/R))
+        return 1/keq_val
