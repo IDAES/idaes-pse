2d15d490bb071a8b7010d8ed4aae73a20ade140f
Andrew Lee <andrew.lee@netl.doe.gov>
2021-04-02 17:14:14 -0400

    Adding first parameters to eNRTL module

--- idaes/generic_models/properties/core/eos/enrtl.py
+++ idaes/generic_models/properties/core/eos/enrtl.py
@@ -15,10 +15,11 @@ Methods for eNRTL activity coefficient method.
 
 Only applicable to liquid/electrolyte phases
 """
-import types
+
+from pyomo.environ import Reals, units as pyunits, Var
 
 from .eos_base import EoSBase
-from idaes.core.util.exceptions import BurntToast
+from idaes.core.util.exceptions import ConfigurationError
 import idaes.logger as idaeslog
 
 
@@ -50,7 +51,56 @@ class ENRTL(EoSBase):
         pass
 
     def build_parameters(b):
-        pass
+        param_block = b.parent_block()
+
+        # Build symmetric indexing set
+        sym_set = []
+        for i in param_block.apparent_species_set:
+            for j in param_block.apparent_species_set:
+                if (j, i) not in sym_set:
+                    sym_set.append((i, j))
+
+        def alpha_init(b, i, j):
+            try:
+                alpha_data = param_block.config.parameter_data[
+                    b.local_name+"_alpha"]
+            except KeyError:
+                alpha_data = {}
+
+            if (i, j) in alpha_data.keys():
+                v = alpha_data[(i, j)]
+                # Check for non-symmetric value assignment
+                if (j, i) in alpha_data.keys():
+                    if alpha_data[(j, i)] != v:
+                        raise ConfigurationError(
+                            "{} eNRTL alpha parameter assigned non-symmetric "
+                            "value for pair {}. Please assign only one value "
+                            "for component pair.".format(b.name, (i, j)))
+                    else:
+                        _log.info("eNRTL alpha value provided for both {} and "
+                                  "{}. It is only necessary to provide a "
+                                  "value for one of these due to symmetry."
+                                  .format((i, j), (j, i)))
+            elif(j, i) in alpha_data.keys():
+                v = alpha_data[(j, i)]
+            elif ((i in param_block.solvent_set or
+                   i in param_block.solute_set) and
+                  (j in param_block.solvent_set or
+                   j in param_block.solute_set)):
+                # Molecular-molecular interaction, default value is 0.3
+                v = 0.3
+            else:
+                # All other intereactions have default value 0.2
+                v = 0.2
+            return v
+
+        b.add_component(
+            'alpha',
+            Var(sym_set,
+                within=Reals,
+                initialize=alpha_init,
+                doc='Symmetric non-randomness parameters',
+                units=pyunits.dimensionless))
 
     @staticmethod
     def dens_mol_phase(b, p):
---
+++ idaes/generic_models/properties/core/eos/tests/test_enrtl.py
@@ -0,0 +1,158 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2020, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+Tests for eNRTL methods
+
+Author: Andrew Lee
+"""
+import pytest
+
+from pyomo.environ import (ConcreteModel,
+                           value,
+                           Var,
+                           units as pyunits)
+
+from idaes.core import (AqueousPhase,
+                        Solvent,
+                        Solute,
+                        Apparent,
+                        Anion,
+                        Cation)
+from idaes.generic_models.properties.core.eos.enrtl import ENRTL
+from idaes.generic_models.properties.core.generic.generic_property import (
+        GenericParameterBlock)
+from idaes.generic_models.properties.core.state_definitions import FTPx
+from idaes.core.util.exceptions import ConfigurationError
+
+
+configuration = {
+    "components": {
+        "H2O": {"type": Solvent},
+        "C6H12": {"type": Solute},
+        "NaCl": {"type": Apparent},
+        "HCl": {"type": Apparent},
+        "NaOH": {"type": Apparent},
+        "Na+": {"type": Cation,
+                "charge": +1},
+        "H+": {"type": Cation,
+               "charge": +1},
+        "Cl-": {"type": Anion,
+                "charge": -1},
+        "OH-": {"type": Anion,
+                "charge": -1}},
+    "phases": {
+        "Liq": {"type": AqueousPhase,
+                "equation_of_state": ENRTL}},
+    "base_units": {"time": pyunits.s,
+                   "length": pyunits.m,
+                   "mass": pyunits.kg,
+                   "amount": pyunits.mol,
+                   "temperature": pyunits.K},
+    "state_definition": FTPx,
+    "pressure_ref": 1e5,
+    "temperature_ref": 300}
+
+
+class TestParameters(object):
+    @pytest.mark.unit
+    def test_parameters_no_assignment(self):
+        m = ConcreteModel()
+
+        m.params = GenericParameterBlock(default=configuration)
+
+        assert isinstance(m.params.Liq.alpha, Var)
+        assert len(m.params.Liq.alpha) == 15
+        for (i, j) in m.params.Liq.alpha:
+            if i != j:
+                assert (j, i) not in m.params.Liq.alpha
+            if (i, j) in [
+                    ("C6H12", "C6H12"), ("H2O", "H2O"), ("H2O", "C6H12")]:
+                assert m.params.Liq.alpha[(i, j)].value == 0.3
+                assert m.params.Liq.alpha[(i, j)].fixed
+            else:
+                assert m.params.Liq.alpha[(i, j)].value == 0.2
+                assert m.params.Liq.alpha[(i, j)].fixed
+
+    @pytest.mark.unit
+    def test_parameters_assignment(self):
+        test_config = dict(configuration)
+        test_config["parameter_data"] = {}
+        test_config["parameter_data"]["Liq_alpha"] = {}
+        test_config["parameter_data"]["Liq_alpha"][("H2O", "NaCl")] = 0.6
+
+        m = ConcreteModel()
+
+        m.params = GenericParameterBlock(default=test_config)
+
+        assert isinstance(m.params.Liq.alpha, Var)
+        assert len(m.params.Liq.alpha) == 15
+        for (i, j) in m.params.Liq.alpha:
+            if i != j:
+                assert (j, i) not in m.params.Liq.alpha
+            if (i, j) == ("H2O", "NaCl"):
+                assert m.params.Liq.alpha[(i, j)].value == 0.6
+                assert m.params.Liq.alpha[(i, j)].fixed
+            elif (i, j) in [
+                    ("C6H12", "C6H12"), ("H2O", "H2O"), ("H2O", "C6H12")]:
+                assert m.params.Liq.alpha[(i, j)].value == 0.3
+                assert m.params.Liq.alpha[(i, j)].fixed
+            else:
+                assert m.params.Liq.alpha[(i, j)].value == 0.2
+                assert m.params.Liq.alpha[(i, j)].fixed
+
+    @pytest.mark.unit
+    def test_parameters_unsymmetric_alpha(self):
+        test_config = dict(configuration)
+        test_config["parameter_data"] = {}
+        test_config["parameter_data"]["Liq_alpha"] = {}
+        test_config["parameter_data"]["Liq_alpha"][("H2O", "NaCl")] = 0.6
+        test_config["parameter_data"]["Liq_alpha"][("NaCl", "H2O")] = 0.8
+
+        m = ConcreteModel()
+
+        with pytest.raises(ConfigurationError,
+                           match="params.Liq eNRTL alpha parameter assigned "
+                           "non-symmetric value for pair \('H2O', 'NaCl'\). "
+                           "Please assign only one value for component pair."):
+            m.params = GenericParameterBlock(default=test_config)
+
+    @pytest.mark.unit
+    def test_parameters_symmetry_duplicate(self):
+        test_config = dict(configuration)
+        test_config["parameter_data"] = {}
+        test_config["parameter_data"]["Liq_alpha"] = {}
+        test_config["parameter_data"]["Liq_alpha"][("H2O", "NaCl")] = 0.6
+        test_config["parameter_data"]["Liq_alpha"][("NaCl", "H2O")] = 0.6
+
+        m = ConcreteModel()
+
+        with pytest.raises(ConfigurationError,
+                           match="params.Liq eNRTL alpha parameter assigned "
+                           "non-symmetric value for pair \('H2O', 'NaCl'\). "
+                           "Please assign only one value for component pair."):
+            m.params = GenericParameterBlock(default=test_config)
+
+    @pytest.mark.unit
+    def test_parameters_unused_parameter(self):
+        test_config = dict(configuration)
+        test_config["parameter_data"] = {}
+        test_config["parameter_data"]["Liq_alpha"] = {}
+        test_config["parameter_data"]["Liq_alpha"][("H2O", "Na+")] = 0.6
+
+        m = ConcreteModel()
+
+        with pytest.raises(ConfigurationError,
+                           match="params.Liq eNRTL alpha parameter assigned "
+                           "non-symmetric value for pair \('H2O', 'NaCl'\). "
+                           "Please assign only one value for component pair."):
+            m.params = GenericParameterBlock(default=test_config)
