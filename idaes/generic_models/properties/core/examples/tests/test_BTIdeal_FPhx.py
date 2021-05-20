13731ce7b7a61d0985b96269389a22d34b817f1d
John Siirola <jsiirola@users.noreply.github.com>
2021-05-19 23:57:52 -0600

    Leverage assertStructuredAlmostEqual to test state_bounds

--- idaes/generic_models/properties/core/examples/tests/test_BTIdeal_FPhx.py
+++ idaes/generic_models/properties/core/examples/tests/test_BTIdeal_FPhx.py
@@ -22,6 +22,7 @@ from pyomo.environ import (ConcreteModel,
                            Var,
                            units as pyunits)
 from pyomo.util.check_units import assert_units_consistent
+from pyomo.common.unittest import assertStructuredAlmostEqual
 
 from idaes.core import Component
 from idaes.core.util.model_statistics import (degrees_of_freedom,
@@ -160,11 +161,15 @@ class TestParamBlock(object):
 
         assert model.params.config.state_definition == FPhx
 
-        assert model.params.config.state_bounds == {
-            "flow_mol": (0, 100, 1000, pyunits.mol/pyunits.s),
-            "enth_mol": (1e4, 5e4, 2e5, pyunits.J/pyunits.mol),
-            "temperature": (273.15, 300, 450, pyunits.K),
-            "pressure": (5e4, 1e5, 1e6, pyunits.Pa)}
+        assertStructuredAlmostEqual(
+            model.params.config.state_bounds,
+            { "flow_mol": (0, 100, 1000, pyunits.mol/pyunits.s),
+              "enth_mol": (1e4, 5e4, 2e5, pyunits.J/pyunits.mol),
+              "temperature": (273.15, 300, 450, pyunits.K),
+              "pressure": (5e4, 1e5, 1e6, pyunits.Pa) },
+            item_callback=lambda x: value(x) * (
+                pyunits.get_units(x) or pyunits.dimensionless)._get_pint_unit()
+        )
 
         assert model.params.config.phase_equilibrium_state == {
             ("Vap", "Liq"): SmoothVLE}
