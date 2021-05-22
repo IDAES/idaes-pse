6589175a8d403ae171c298d2f73c232f89df8991
Robert Parker <robbybparker@gmail.com>
2021-05-22 00:52:57 -0400

    implement property testing methods for MW

--- idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/tests/test_CLC_gas_prop.py
+++ idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/tests/test_CLC_gas_prop.py
@@ -23,8 +23,14 @@ from pyomo.environ import (
         TerminationCondition,
         SolverStatus,
         Var,
+        Reference,
+        value,
         units as pyunits,
         )
+from pyomo.contrib.incidence_analysis import IncidenceGraphInterface
+from pyomo.common.collections import ComponentSet, ComponentMap
+from pyomo.util.calc_var_value import calculate_variable_from_constraint
+from pyomo.common.unittest import TestCase
 
 from idaes.core import FlowsheetBlock
 
@@ -186,17 +192,291 @@ def test_indexed_state_block():
             assert m.fs.properties._metadata._properties[name]["method"] is None
 
 
+def test_property_construction_ordered():
+    """
+    The purpose of this test is to make sure each property can be
+    constructed properly. Further, because we know that these
+    variables form a DAG, we check that the addition of each new
+    property adds one variable and constraint, i.e. that the
+    matching defined in the parameter block is in topological order.
+    """
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+    m.fs.properties = GasPhaseParameterBlock()
+    m.fs.state = m.fs.properties.build_state_block()
+
+    nvar = 7
+    ncon = 1
+    assert len(list(m.component_data_objects(Var))) == nvar
+    assert len(list(m.component_data_objects(Constraint))) == ncon
+
+    # Make sure the matching captures all the non-state vars.
+    state_vars = m.fs.state.define_state_vars()
+    n_state_vars = len(state_vars)
+    n_vars = len(m.fs.properties._metadata._properties)
+    assert len(m.fs.properties.matching) == n_vars - n_state_vars
+
+    # Make sure we can add constraints one at a time, and only
+    # add one additional variable. This is specific to this
+    # property package.
+    for varname, conname in m.fs.properties.matching:
+        assert varname not in state_vars
+        var = getattr(m.fs.state, varname)
+        con = getattr(m.fs.state, conname)
+        dim = len(var)
+        nvar += dim
+        ncon += dim
+        assert dim == len(con)
+        assert isinstance(var, Var)
+        assert isinstance(con, Constraint)
+        assert len(list(m.component_data_objects(Var))) == nvar
+        assert len(list(m.component_data_objects(Constraint))) == ncon
+
+
+def test_property_construction_unordered():
+    """
+    The purpose of this test is to make sure "chained construction"
+    of properties happens properly. I.e. if we add a property that
+    requires several other properties, these are added as well.
+    """
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+    m.fs.properties = GasPhaseParameterBlock()
+    m.fs.state = m.fs.properties.build_state_block()
+
+    nvar = 7
+    ncon = 1
+    assert len(list(m.component_data_objects(Var))) == nvar
+    assert len(list(m.component_data_objects(Constraint))) == ncon
+
+    # Access this property to force the construction of prerequisite
+    # properties.
+    m.fs.state.cp_mass
+
+    # Make sure dependent properties have been properly added.
+    assert hasattr(m.fs.state, "cp_mol")
+    assert hasattr(m.fs.state, "cp_mol_comp")
+    assert hasattr(m.fs.state, "mw")
+    nvar += len(m.fs.state.cp_mol_comp)
+    nvar += len(m.fs.state.cp_mol)
+    nvar += len(m.fs.state.cp_mass)
+    nvar += len(m.fs.state.mw)
+    ncon += len(m.fs.state.cp_mol_comp)
+    ncon += len(m.fs.state.cp_mol)
+    ncon += len(m.fs.state.cp_mass)
+    ncon += len(m.fs.state.mw)
+    assert len(list(m.component_data_objects(Var))) == nvar
+    assert len(list(m.component_data_objects(Constraint))) == ncon
+
+
+def _create_subsystem_block(variables, constraints):
+    block = ConcreteModel()
+    block.vars = Reference(variables)
+    block.cons = Reference(constraints)
+    var_set = ComponentSet(variables)
+    other_vars = []
+    for con in constraints:
+        for var in identify_variables(con.body, include_fixed=False):
+            # Fixed vars will not be picked up by writer, so no point
+            # including them. If we use this block for other purposes
+            # than solving, this may change.
+            if var not in var_set:
+                other_vars.append(var)
+                var_set.add(var)
+    block.other_vars = Reference(other_vars)
+    return block
+
+
+def _solve_strongly_connected_components(block):
+    variables = [var for var in block.component_data_objects(Var)
+            if not var.fixed]
+    constraints = [con for con in block.component_data_objects(Constraint)
+            if con.active]
+    assert len(variables) == len(constraints)
+    igraph = IncidenceGraphInterface()
+    var_block_map, con_block_map = igraph.block_triangularize(
+            variables=variables,
+            constraints=constraints,
+            )
+    blocks = set(var_block_map.values())
+    n_blocks = len(blocks)
+    var_blocks = [[] for b in range(n_blocks)]
+    con_blocks = [[] for b in range(n_blocks)]
+    for var, b in var_block_map.items():
+        var_blocks[b].append(var)
+    for con, b in con_block_map.items():
+        con_blocks[b].append(con)
+    for b_vars, b_cons in zip(var_blocks, con_blocks):
+        assert len(b_vars) == len(b_cons)
+        if len(b_vars) == 1:
+            var = b_vars[0]
+            con = b_cons[0]
+            calculate_variable_from_constraint(var, con)
+        else:
+            _temp = _create_subsystem_block(b_vars, b_cons)
+            _temp.other_vars[:].fix()
+            solver.solve(_temp)
+            _temp.other_vars[:].unfix()
+
+
+class ParameterSweeper(object):
+
+    def __init__(self, state_block, state_values, target_values, n_scenario):
+        self.state_block = state_block
+
+        # dict: state name -> state var
+        self.states = self.state_block.define_state_vars()
+
+        # dict: state_name -> list of values
+        self.state_values = state_values
+
+        # dict: property name -> list of values
+        self.target_values = target_values
+
+        self.n_scenario = n_scenario
+
+        self.initial_state_values = None
+
+        self._ip = -1 # Index pointer for iteration
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._ip += 1
+
+        i = self._ip
+        n_scenario = self.n_scenario
+        states = self.states
+        state_var_values = self.state_values
+        target_values = self.target_values
+        block = self.state_block
+
+        if i >= n_scenario:
+            self._ip = -1
+            raise StopIteration
+
+        else:
+            for name, var in states.items():
+                if var.is_indexed():
+                    for idx, obj in var.items():
+                        val = state_var_values[name][idx][i]
+                        obj.set_value(val)
+                else:
+                    val = state_var_values[name][i]
+                    var.set_value(val)
+
+            target = ComponentMap()
+            for name, values in target_values.items():
+                var = getattr(block, name)
+                if var.is_indexed():
+                    target[var] = {
+                            idx: target_values[name][idx][i] for idx in var
+                            }
+                else:
+                    target[var] = target_values[name][i]
+
+            return block, target
+
+    def __enter__(self):
+        # Store initial values of state vars
+        initial_state_values = {}
+        for name, var in self.states.items():
+            if var.is_indexed():
+                initial_state_values[name] = {
+                        idx: data.value for idx, data in var.items()
+                        }
+            else:
+                initial_state_values[name] = var.value
+        self.initial_state_values = initial_state_values
+
+        return self
+
+    def __exit__(self, ex_type, ex_val, ex_bt):
+        # Re-load initial values into state vars
+        states = self.states
+        values = self.initial_state_values
+        for name, var in states.items():
+            if var.is_indexed():
+                for idx, data in var.items():
+                    data.set_value(values[name][idx])
+            else:
+                var.set_value(values[name])
+
+
+class TestProperties(TestCase):
+    """
+    The purpose of these tests is to ensure that the property package's
+    state block can be used to solve for the specified properties, and
+    that the values produced are what we expect.
+    """
+    def _make_model(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(default={"dynamic": False})
+        m.fs.properties = GasPhaseParameterBlock()
+        m.fs.state = m.fs.properties.build_state_block(default={
+            # defined_state True because we want to ensure the state
+            # block is square.
+            "defined_state": True,
+            })
+        for var in m.fs.state.define_state_vars().values():
+            var.fix()
+        return m
+
+    def test_mw(self):
+        m = self._make_model()
+
+        # Define a somewhat arbitrary set of values we'd like to use to
+        # test molecular weight.
+        n_scenario = 7
+        state_var_values = {
+                "flow_mol": [1.0*pyunits.mol/pyunits.s]*n_scenario,
+                "temperature": [300.0*pyunits.K]*n_scenario,
+                "pressure": [1.0*pyunits.bar]*n_scenario,
+                "mole_frac_comp": {
+                    "O2":  [1.0, 0.5, 0.25, 0.0, 0.0, 0.0, 0.0],
+                    "N2":  [0.0, 0.5, 0.25, 1.0, 0.0, 0.0, 0.0],
+                    "H2O": [0.0, 0.0, 0.25, 0.0, 1.0, 0.0, 0.5],
+                    "CO2": [0.0, 0.0, 0.25, 0.0, 0.0, 1.0, 0.5],
+                    },
+                }
+        target_values = {
+                "mw": [
+                    32.0*pyunits.g/pyunits.mol,
+                    30.0*pyunits.g/pyunits.mol,
+                    30.5*pyunits.g/pyunits.mol,
+                    28.0*pyunits.g/pyunits.mol,
+                    18.0*pyunits.g/pyunits.mol,
+                    44.0*pyunits.g/pyunits.mol,
+                    31.0*pyunits.g/pyunits.mol,
+                    ]
+                }
+
+        # Construct mw and all prerequisites
+        m.fs.state.mw
+        sweeper = ParameterSweeper(
+                m.fs.state,
+                state_var_values,
+                target_values,
+                n_scenario,
+                )
+        with sweeper as scenarios:
+            for block, target in scenarios:
+                _solve_strongly_connected_components(block)
+                for var, val in target.items():
+                    if var.is_indexed():
+                        for idx, data in var.items():
+                            val = value(pyunits.convert(val, var.get_units()))
+                            assert data.value == pytest.approx(val, abs=1e-8)
+                    else:
+                        val = value(pyunits.convert(val, var.get_units()))
+                        assert var.value == pytest.approx(val, abs=1e-8)
+
+
 if __name__ == "__main__":
     test_state_vars()
-    m = make_model()
-    print(degrees_of_freedom(m))
-    for var in m.component_data_objects(Var):
-        print(var.name, var.fixed)
-
-    for con in m.component_data_objects(Constraint, active=True):
-        print(con.name)
-
-    # At this point, the model is very uninteresting. We have only queried
-    # variables that have "method": None. This means that no constraints
-    # have been added to the state block.
-    import pdb; pdb.set_trace()
+    test_indexed_state_block()
+    test_property_construction_ordered()
+    test_property_construction_unordered()
+    test = TestProperties()
+    test.test_mw()
