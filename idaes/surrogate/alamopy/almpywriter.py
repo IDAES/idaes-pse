cd61ab184b30dcbb4124e03b8c7a4afb185c9b21
Dan Gunter <dkgunter@lbl.gov>
2021-06-01 10:57:21 -0700

    Updated header

--- idaes/surrogate/alamopy/almpywriter.py
+++ idaes/surrogate/alamopy/almpywriter.py
@@ -1,9 +1,6 @@
 ###############################################################################
-# Copyright
-# =========
-#
-# Institute for the Design of Advanced Energy Systems Process Systems Engineering
-# Framework (IDAES PSE Framework) was produced under the DOE Institute for the
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
 # Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021 by the
 # software owners: The Regents of the University of California, through Lawrence
 # Berkeley National Laboratory,  National Technology & Engineering Solutions of
@@ -15,8 +12,7 @@
 # U.S. Government has been granted for itself and others acting on its behalf a
 # paid-up, nonexclusive, irrevocable, worldwide license in the Software to
 # reproduce, distribute copies to the public, prepare derivative works, and
-# perform publicly and display publicly, and to permit other to do so. Copyright
-# (C) 2018-2019 IDAES - All Rights Reserved
+# perform publicly and display publicly, and to permit other to do so.
 #
 ###############################################################################
 import re
--- idaes/tests/test_beta.py
+++ idaes/tests/test_beta.py
@@ -1,10 +1,7 @@
 # coding: utf-8
 ###############################################################################
-# Copyright
-# =========
-#
-# Institute for the Design of Advanced Energy Systems Process Systems Engineering
-# Framework (IDAES PSE Framework) was produced under the DOE Institute for the
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
 # Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021 by the
 # software owners: The Regents of the University of California, through Lawrence
 # Berkeley National Laboratory,  National Technology & Engineering Solutions of
@@ -16,8 +13,7 @@
 # U.S. Government has been granted for itself and others acting on its behalf a
 # paid-up, nonexclusive, irrevocable, worldwide license in the Software to
 # reproduce, distribute copies to the public, prepare derivative works, and
-# perform publicly and display publicly, and to permit other to do so. Copyright
-# (C) 2018-2019 IDAES - All Rights Reserved
+# perform publicly and display publicly, and to permit other to do so.
 #
 ###############################################################################
 
