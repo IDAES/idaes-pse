06d1dba4b079fc9c5ee25689d280a891652d1445
Dan Gunter <dkgunter@lbl.gov>
2021-06-01 12:08:43 -0700

    headers changed

--- idaes/generic_models/properties/core/phase_equil/__init__.py
+++ idaes/generic_models/properties/core/phase_equil/__init__.py
@@ -1,18 +1,13 @@
-###############################################################################
-# ** Copyright Notice **
+#################################################################################
 # The Institute for the Design of Advanced Energy Systems Integrated Platform
 # Framework (IDAES IP) was produced under the DOE Institute for the
-# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021 by the
-# software owners: The Regents of the University of California, through Lawrence
-# Berkeley National Laboratory,  National Technology & Engineering Solutions of
-# Sandia, LLC, Carnegie Mellon University, West Virginia University Research
-# Corporation, et al.  All rights reserved.
+# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021
+# by the software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia University
+# Research Corporation, et al.  All rights reserved.
 #
-# NOTICE.  This Software was developed under funding from the U.S. Department of
-# Energy and the U.S. Government consequently retains certain rights. As such, the
-# U.S. Government has been granted for itself and others acting on its behalf a
-# paid-up, nonexclusive, irrevocable, worldwide license in the Software to
-# reproduce, distribute copies to the public, prepare derivative works, and
-# perform publicly and display publicly, and to permit other to do so.
-###############################################################################
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
+# license information.
+#################################################################################
 from .smooth_VLE import SmoothVLE
