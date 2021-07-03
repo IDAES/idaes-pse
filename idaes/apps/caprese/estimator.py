678c9dde9584837da88839f3529a5972822ce968
kuanhanl_DH4200 <kuanhanl@andrew.cmu.edu>
2021-07-02 23:30:55 -0400

    start creating mhe(mhe.py & estimator.py)

---
+++ idaes/apps/caprese/estimator.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021
+# by the software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia University
+# Research Corporation, et al.  All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
+# license information.
+#################################################################################
+""" Block-like object meant for Moving Horizon Estimator (MHE) models.
+"""
+
+__author__ = "Kuan-Han Lin"
+
+# import idaes.logger as idaeslog
+# from idaes.apps.caprese.util import initialize_by_element_in_range
+# from idaes.apps.caprese.common.config import (
+#         ControlPenaltyType,
+#         )
+# from idaes.apps.caprese.common.config import VariableCategory as VC
+# from idaes.apps.caprese.categorize import (
+#         categorize_dae_variables,
+#         CATEGORY_TYPE_MAP,
+#         )
+# from idaes.apps.caprese.nmpc_var import (
+#         NmpcVar,
+#         DiffVar,
+#         AlgVar,
+#         InputVar,
+#         DerivVar,
+#         FixedVar,
+#         MeasuredVar,
+#         )
+from idaes.apps.caprese.dynamic_block import (
+        _DynamicBlockData,
+        IndexedDynamicBlock,
+        DynamicBlock,
+        )
+# from idaes.core.util.model_statistics import degrees_of_freedom
+
+from pyomo.environ import (
+        Var,
+#         Objective,
+#         TerminationCondition,
+        Constraint,
+        Block,
+        )
+from pyomo.core.base.block import _BlockData
+# from pyomo.common.collections import ComponentMap
+# from pyomo.core.base.range import remainder
+# from pyomo.dae.set_utils import deactivate_model_at
+# from pyomo.dae.flatten import flatten_dae_components
+from pyomo.core.base.indexed_component import UnindexedComponent_set
+
+
+class _EstimatorBlockData(_DynamicBlockData):
+    """ This class adds methods useful for working with dynamic
+    models to be used by a estimator. These include methods for
+    """
+    
+    def empty_now(self):
+        print("dummy fun = ", 123)
+        
+    def _add_actual_measurement_param(self):
+        """This function creates a block and "fixed" variables to allocate 
+        actual measurement measured from the plant. 
+
+        """
+        block_name = "ACTUAL_MEASUREMENT_BLOCK" #should be change after define MEA_ERR category
+        mea_set = self.MEASUREMENT_SET #better to use get_category_set_name
+        actmea_block = Block(mea_set)
+        self.add_component(block_name, actmea_block)
+        # meaerr_block.deactivate() #keep it activate? Yes!
+        sample_points = self.sample_points
+        for i in mea_set:
+            var_name = "actual_mea"
+            init_mea_block = self.MEASUREMENT_BLOCK[i]
+            init_val = {idx: init_mea_block.var[idx].value for idx in sample_points}
+            actmea_block[i].add_component(var_name, Var(sample_points, initialize = init_val))
+            actmea_block[i].find_component(var_name).fix()
+    
+    def _add_measurement_error(self):
+        block_name = "MEASUREMENT_ERROR_BLOCK" #should be change after define MEA_ERR category
+        mea_set = self.MEASUREMENT_SET #better to use get_category_set_name
+        meaerr_block = Block(mea_set)
+        self.add_component(block_name, meaerr_block)
+        # meaerr_block.deactivate() #keep it activate? Yes!
+        sample_points = self.sample_points
+        for i in mea_set:
+            var_name = "mea_err"
+            meaerr_block[i].add_component(var_name, Var(sample_points, initialize = 0.0))
+            
+            con_name = "con_mea_err" #actual_mea = measured_state + mea_err
+            def _con_mea_err(m, s):
+                ind = m.index()
+                parent = m.parent_block()
+                actual_mea = parent.ACTUAL_MEASUREMENT_BLOCK[ind].actual_mea
+                measured_stat = parent.MEASUREMENT_BLOCK[ind].var
+                mea_err = parent.MEASUREMENT_ERROR_BLOCK[ind].mea_err
+                return actual_mea[s] == measured_stat[s] + mea_err[s]
+            meaerr_block[i].add_component(con_name, Constraint(sample_points, rule = _con_mea_err))
+            
+    # def _add_measurement_constraint(self):
+    #     block_name = "MEASUREMENT_CONSTRAINT_BLOCK" #should be change after define MEA_ERR category
+    #     mea_set = self.MEASUREMENT_SET #better to use get_category_set_name
+    #     meacon_block = Block(mea_set)
+    #     self.add_component(block_name, meacon_block)
+        
+    #     con_name = "con_mea_err" #actual_mea = measured_state + mea_err
+    #     sample_points = self.sample_points
+    #     for i in mea_set:
+    #         def _con_mea_err(m, s):
+    #             ind = m.index()
+    #             parent = m.parent_block()
+    #             actual_mea = parent.ACTUAL_MEASUREMENT_BLOCK[ind].actual_mea
+    #             measured_stat = parent.MEASUREMENT_BLOCK[ind].var
+    #             mea_err = parent.MEASUREMENT_ERROR_BLOCK[ind].mea_err
+    #             return actual_mea[s] == measured_stat[s] + mea_err[s]
+    #         meacon_block[i].add_component(con_name, Constraint(sample_points, rule = _con_mea_err))
+        
+        
+
+class EstimatorBlock(DynamicBlock):
+    """ This is a user-facing class to be instantiated when one
+    wants to work with a block capable of the methods of
+    `_EstimatorBlockData`.
+    """
+    _ComponentDataClass = _EstimatorBlockData
+
+    def __new__(cls, *args, **kwds):
+        # Decide what class to allocate
+        if cls != EstimatorBlock:
+            target_cls = cls
+        elif not args or (args[0] is UnindexedComponent_set and len(args) == 1):
+            target_cls = SimpleEstimatorBlock
+        else:
+            target_cls = IndexedEstimatorBlock
+        return super(EstimatorBlock, cls).__new__(target_cls)
+
+
+class SimpleEstimatorBlock(_EstimatorBlockData, EstimatorBlock):
+    def __init__(self, *args, **kwds):
+        _EstimatorBlockData.__init__(self, component=self)
+        EstimatorBlock.__init__(self, *args, **kwds)
+
+    # Pick up the display() from Block and not BlockData
+    display = EstimatorBlock.display
+
+
+class IndexedEstimatorBlock(EstimatorBlock):
+
+    def __init__(self, *args, **kwargs):
+        EstimatorBlock.__init__(self, *args, **kwargs)
---
+++ idaes/apps/caprese/examples/cstr_rodrigo/mhe_main.py
@@ -0,0 +1,116 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+"""
+Example for Caprese's module for MHE.
+"""
+import random
+from idaes.apps.caprese.mhe import MHESim
+# from idaes.apps.caprese.util import apply_noise_with_bounds
+from pyomo.environ import SolverFactory
+# from pyomo.dae.initialization import solve_consistent_initial_conditions
+# import idaes.logger as idaeslog
+from cstr_rodrigo_model2 import make_model
+import pandas as pd
+import matplotlib.pyplot as plt
+
+__author__ = "Kuan-Han Lin"
+
+
+
+# See if ipopt is available and set up solver
+if SolverFactory('ipopt').available():
+    solver = SolverFactory('ipopt')
+    solver.options = {
+            'tol': 1e-6,
+            'bound_push': 1e-8,
+            'halt_on_ampl_error': 'yes',
+            'linear_solver': 'ma57',
+            }
+else:
+    solver = None
+
+class PlotData(object):
+    def __init__(self, group, location, name=None, t_switch=None):
+        # Would really like a PlotData class that is constructed based on an
+        # NMPCVar object that contains necessary setpoint/reference
+        # information, instead of having to access that in the NMPCVarGroup
+        time = group.index_set
+        if t_switch == None:
+            t_switch = group.t0
+
+        self.name = name
+
+        var = group.varlist[location]
+        initial = group.reference[location]
+        setpoint = group.setpoint[location]
+        self.data_series = pd.Series(
+                [var[t].value for t in time],
+                index=[t for t in time])
+        self.setpoint_series = pd.Series(
+                [initial if t < t_switch else setpoint for t in time])
+
+    def plot(self):
+        # fig, ax can be formatted to the user's liking
+        fig, ax = plt.subplots()
+        if self.name is not None:
+            self.data_series.plot(label=self.name)
+        else:
+            self.data_series.plot()
+        return fig, ax
+
+def main(plot_switch=False):
+    m_estimator = make_model(horizon=10, ntfe=10, ntcp=2, bounds=True)
+    sample_time = 2.
+    m_plant = make_model(horizon=sample_time, ntfe=2, ntcp=2, bounds = True)
+    time_plant = m_plant.t
+
+    simulation_horizon = 20
+    n_samples_to_simulate = round(simulation_horizon/sample_time)
+
+    samples_to_simulate = [time_plant.first() + i*sample_time
+                           for i in range(1, n_samples_to_simulate)]
+
+    # We must identify for the estimator which variables are our
+    # inputs and measurements.
+    inputs = [
+            m_plant.Tjinb[0],
+            ]
+    measurements = [
+            m_estimator.Tall[0, "T"],
+            # m_controller.Tall[0, "Tj"],
+            m_estimator.Ca[0],
+            ]
+    
+    # Construct the "MHE simulator" object
+    mhe = MHESim(
+            plant_model=m_plant,
+            plant_time_set=m_plant.t,
+            estimator_model=m_estimator, 
+            estimator_time_set=m_estimator.t,
+            inputs_at_t0=inputs,
+            measurements=measurements,
+            sample_time=sample_time,
+            )
+    
+    return mhe
+
+    plant = nmpc.plant
+    estimator = nmpc.estimator
+    
+    p_t0 = nmpc.plant.time.first()
+    c_t0 = nmpc.estimator.time.first()
+    p_ts = nmpc.plant.sample_points[1]
+    c_ts = nmpc.estimator.sample_points[1]
+
+if __name__ == '__main__':
+    mhe = main()
