729cad5775f0f9a1aebd8f0af8b00bbdad0616aa
Robert Parker <robbybparker@gmail.com>
2021-07-07 23:27:58 -0400

    test density properties

--- idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/tests/test_CLC_solid_prop.py
+++ idaes/gas_solid_contactors/properties/oxygen_iron_OC_oxidation/tests/test_CLC_solid_prop.py
@@ -23,11 +23,22 @@ from pyomo.environ import (
         SolverStatus,
         Var,
         Constraint,
+        value,
+        units as pyunits,
+        )
+import pyomo.common.unittest as unittest
+from pyomo.common.collections import ComponentMap
+from pyomo.util.subsystems import ParamSweeper
+from pyomo.contrib.incidence_analysis import (
+        solve_strongly_connected_components,
         )
 
 from idaes.core import FlowsheetBlock
 
-from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.core.util.model_statistics import (
+        degrees_of_freedom,
+        number_large_residuals,
+        )
 
 from idaes.core.util.testing import (get_default_solver,
                                      initialization_tester)
@@ -189,3 +200,121 @@ def test_property_construction_ordered():
         assert isinstance(con, Constraint)
         assert len(list(m.fs.state.component_data_objects(Var))) == nvar
         assert len(list(m.component_data_objects(Constraint))) == ncon
+
+
+class TestProperties(unittest.TestCase):
+
+    def _make_model(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(default={"dynamic": False})
+        m.fs.properties = SolidPhaseParameterBlock()
+        m.fs.state = m.fs.properties.build_state_block(default={
+            "defined_state": True,
+            })
+        for var in m.fs.state.define_state_vars().values():
+            var.fix()
+        return m
+
+    def test_dens_mass_skeletal(self):
+        m = self._make_model()
+        state = m.fs.state
+
+        n_scen = 4
+        state_values = {
+                "flow_mass": [1.0*pyunits.kg/pyunits.s]*n_scen,
+                "temperature": [1200.0*pyunits.K]*n_scen,
+                "particle_porosity": [0.27]*n_scen,
+                "mass_frac_comp[Fe2O3]": [1.0, 0.0, 0.0, 1.0/3.0],
+                "mass_frac_comp[Fe3O4]": [0.0, 1.0, 0.0, 1.0/3.0],
+                "mass_frac_comp[Al2O3]": [0.0, 0.0, 1.0, 1.0/3.0],
+                }
+        state_values = ComponentMap((state.find_component(name), values)
+                for name, values in state_values.items())
+        kgm3 = pyunits.kg/pyunits.m**3
+        target_values = {
+                "dens_mass_skeletal": [
+                    5250.000*kgm3,
+                    5000.000*kgm3,
+                    3987.000*kgm3,
+                    4678.061*kgm3,
+                    ],
+                }
+        target_values = ComponentMap((state.find_component(name), values)
+                for name, values in target_values.items())
+
+        # Construct dens_mass_particle and all its prerequisites
+        state.dens_mass_particle
+
+        param_sweeper = ParamSweeper(
+                n_scen,
+                state_values,
+                output_values=target_values,
+                )
+        with param_sweeper:
+            for inputs, outputs in param_sweeper:
+                solve_strongly_connected_components(state)
+
+                assert number_large_residuals(state, tol=1e-8) == 0
+
+                for var, val in inputs.items():
+                    val = value(pyunits.convert(val, var.get_units()))
+                    assert var.value == pytest.approx(value(val), abs=1e-3)
+
+                for var, val in outputs.items():
+                    val = value(pyunits.convert(val, var.get_units()))
+                    assert var.value == pytest.approx(value(val), abs=1e-3)
+
+    def test_dens_mass_particle(self):
+        m = self._make_model()
+        state = m.fs.state
+
+        n_scen = 3
+        state_values = {
+                "flow_mass": [1.0*pyunits.kg/pyunits.s]*n_scen,
+                "temperature": [1200.0*pyunits.K]*n_scen,
+                "particle_porosity": [0.22, 0.27, 0.32],
+                "mass_frac_comp[Fe2O3]": [1.0/3.0]*n_scen,
+                "mass_frac_comp[Fe3O4]": [1.0/3.0]*n_scen,
+                "mass_frac_comp[Al2O3]": [1.0/3.0]*n_scen,
+                }
+        state_values = ComponentMap((state.find_component(name), values)
+                for name, values in state_values.items())
+        kgm3 = pyunits.kg/pyunits.m**3
+        target_values = {
+                "dens_mass_particle": [
+                    3648.888*kgm3,
+                    3414.985*kgm3,
+                    3181.081*kgm3,
+                    ],
+                }
+        target_values = ComponentMap((state.find_component(name), values)
+                for name, values in target_values.items())
+
+        # Construct dens_mass_particle and all its prerequisites
+        state.dens_mass_particle
+
+        param_sweeper = ParamSweeper(
+                n_scen,
+                state_values,
+                output_values=target_values,
+                )
+        with param_sweeper:
+            for inputs, outputs in param_sweeper:
+                solve_strongly_connected_components(state)
+
+                # Check that we have eliminated infeasibility
+                assert number_large_residuals(state, tol=1e-8) == 0
+
+                # Sanity check that inputs have been set properly
+                for var, val in inputs.items():
+                    val = value(pyunits.convert(val, var.get_units()))
+                    assert var.value == pytest.approx(value(val), abs=1e-3)
+
+                for var, val in outputs.items():
+                    val = value(pyunits.convert(val, var.get_units()))
+                    assert var.value == pytest.approx(value(val), abs=1e-3)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    TestProperties().test_dens_mass_skeletal()
