319e9f5afc6bb6566f56f87f0fe1bf35bbdad1f7
Andrew Lee <andrew.lee@netl.doe.gov>
2021-07-09 14:30:00 -0400

    Adding placeholder for osmotic pressure in eos_base

--- idaes/generic_models/properties/core/eos/eos_base.py
+++ idaes/generic_models/properties/core/eos/eos_base.py
@@ -282,6 +282,10 @@ class EoSBase():
     def gibbs_mol_phase_comp(b, p, j):
         raise NotImplementedError(_msg(b, "gibbs_mol_phase_comp"))
 
+    @staticmethod
+    def pressure_osmotic_phase(b, p):
+        raise NotImplementedError(_msg(b, "pressure_osmotic_phase"))
+
 
 def _msg(b, attr):
     return ("{} Equation of State module has not implemented a method for {}. "
