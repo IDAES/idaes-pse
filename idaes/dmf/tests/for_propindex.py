26602dc08dc7a7679423a0d0911ea8cf6839597c
Dan Gunter <dkgunter@lbl.gov>
2021-09-10 16:44:34 -0700

    tweaks

--- idaes/dmf/tests/for_propindex.py
+++ idaes/dmf/tests/for_propindex.py
@@ -17,6 +17,6 @@ class IndexMePlease1(PhysicalParameterBlock):
 
     @classmethod
     def define_metadata(cls, m):
-        m.add_default_units({'temperature': 'K'})
+        m.add_default_units({'temperature': 'K', 'time': 's'})
         m.add_properties({'pressure': {'units': 'Pa', 'method': 'foo'},
                           'temperature': {'method': 'bar'}})
