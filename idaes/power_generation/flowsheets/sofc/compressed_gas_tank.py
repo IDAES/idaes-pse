766dd42f0b3a624b4693b727cb8dedcdb56b3d92
Naresh Susarla <susarlan@ad.science>
2021-10-04 14:33:34 -0400

    adjusted disclaimer to address failing tests

--- idaes/power_generation/flowsheets/sofc/compressed_gas_tank.py
+++ idaes/power_generation/flowsheets/sofc/compressed_gas_tank.py
@@ -10,19 +10,17 @@
 # license information, respectively. Both files are also available online
 # at the URL "https://github.com/IDAES/idaes-pse".
 ##############################################################################
-
 ##############################################################################
 # DISPATCHES was produced under the DOE Design Integration and Synthesis
-# Platform to Advance Tightly Coupled Hybrid Energy Systems program
-# (DISPATCHES), and is copyright (c) 2021 by the software owners: The Regents
-# of the University of California, through Lawrence Berkeley National
-# Laboratory, National Technology & Engineering Solutions of Sandia, LLC,
-# Alliance for Sustainable Energy, LLC, Battelle Energy Alliance, LLC,
-# University of Notre Dame du Lac, etal. All rights reserved.
+# Platform to Advance Tightly Coupled Hybrid Energy Systems program (DISPATCHES),
+# and is copyright (c) 2021 by the software owners: The Regents of the University
+# of California, through Lawrence Berkeley National Laboratory, National
+# Technology & Engineering Solutions of Sandia, LLC, Alliance for Sustainable
+# Energy, LLC, Battelle Energy Alliance, LLC, University of Notre Dame du Lac, et
+# al. All rights reserved.
 #
-# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
-# license information, respectively.
-# Both files are also available online at the URL:
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. Both files are also available online at the URL:
 # "https://github.com/gmlc-dispatches/dispatches".
 #
 ##############################################################################
