ef86282612267ba4b5b5d466a6e8c95fc1eff72e
Andrew Lee <andrew.lee@netl.doe.gov>
2021-10-18 17:12:12 -0400

    Adding tests for surrogate_base

--- idaes/surrogate/surrogate_base.py
+++ idaes/surrogate/surrogate_base.py
@@ -13,10 +13,7 @@
 """
 Common Surrogate interface for IDAES.
 """
-from pyomo.environ import Var
-from pyomo.common.config import ConfigBlock, ConfigValue, ConfigList
-from pyomo.core.base.global_set import UnindexedComponent_set
-import os.path, pickle
+from pyomo.common.config import ConfigBlock
 
 from idaes.surrogate.metrics import compute_fit_metrics
 
@@ -45,15 +42,17 @@ class SurrogateTrainer(object):
            input_bounds: None, or dict of tuples
               if None, these are set later from the provided data
               if provided, it should be a dictionary where the keys correspond
-              to the input label, and the values are tuples of bounds (lower,upper)
+              to the input label, and the values are tuples of bounds
+              (lower, upper)
            training_dataframe: pandas DataFrame
-              Pandas DataFrame corresponding to the training data. Columns must include
-              all the labels in input_labels and output_labels
+              Pandas DataFrame corresponding to the training data. Columns must
+              include all the labels in input_labels and output_labels
            validation_dataframe: pandas DataFrame or None
-             Pandas DateFrame corresponding to the validation data. Columns must include
-             all the labels in input_labels and output_labels. If None is passed, then
-             no validation data will be used. Some derived surrogate trainers may require
-             validation data, while others may not.
+             Pandas DateFrame corresponding to the validation data. Columns
+             must include all the labels in input_labels and output_labels. If
+             None is passed, then no validation data will be used. Some
+             derived surrogate trainers may require validation data, while
+             others may not.
         """
         # Set the config block from passed settings
         self.config = self.CONFIG(settings)
@@ -61,8 +60,10 @@ class SurrogateTrainer(object):
         # We must have at least one input label and one output label
         if input_labels is None or len(input_labels) < 1 \
            or output_labels is None or len(output_labels) < 1:
-            raise ValueError('SurrogateTrainer requires a list of input_labels and a list of output_labels'
-                             ' which must both have a length of at least one')
+            raise ValueError(
+                'SurrogateTrainer requires a list of input_labels and a list '
+                'of output_labels which must both have a length of at '
+                'least one')
 
         self._input_labels = list(input_labels)
         self._output_labels = list(output_labels)
@@ -70,8 +71,10 @@ class SurrogateTrainer(object):
         # check that the input and output labels do not overlap
         all_labels = set(self._input_labels)
         all_labels.update(self._output_labels)
-        if len(all_labels) != len(self._input_labels) + len(self._output_labels):
-            raise ValueError('Duplicate label found in input_labels and/or output_labels.')
+        if len(all_labels) != (
+                len(self._input_labels) + len(self._output_labels)):
+            raise ValueError(
+                'Duplicate label found in input_labels and/or output_labels.')
 
         # create the data members for training and validation data
         self._training_dataframe = training_dataframe
@@ -79,15 +82,21 @@ class SurrogateTrainer(object):
 
         # check that all input labels and output labels are in the dataframes
         if set(self._input_labels) - set(self._training_dataframe.columns):
-            raise ValueError('An input label was specified that was not found in the training data.')
-        if  self._validation_dataframe is not None and \
-            set(self._input_labels) - set(self._validation_dataframe.columns):
-            raise ValueError('An input label was specified that was not found in the validation data.')
+            raise ValueError('An input label was specified that was not '
+                             'found in the training data.')
+        if (self._validation_dataframe is not None and
+                set(self._input_labels) -
+                set(self._validation_dataframe.columns)):
+            raise ValueError('An input label was specified that was not '
+                             'found in the validation data.')
         if set(self._output_labels) - set(self._training_dataframe.columns):
-            raise ValueError('An input label was specified that was not found in the training data.')
-        if  self._validation_dataframe is not None and \
-            set(self._output_labels) - set(self._validation_dataframe.columns):
-            raise ValueError('An input label was specified that was not found in the validation data.')
+            raise ValueError('An output label was specified that was not '
+                             'found in the training data.')
+        if (self._validation_dataframe is not None and
+                set(self._output_labels) -
+                set(self._validation_dataframe.columns)):
+            raise ValueError('An output label was specified that was not '
+                             'found in the validation data.')
 
         if input_bounds is not None:
             self._input_bounds = dict(input_bounds)
@@ -95,7 +104,8 @@ class SurrogateTrainer(object):
             # get the bounds from the data
             mx = self._training_dataframe.max().to_dict()
             mn = self._training_dataframe.min().to_dict()
-            self._input_bounds = {k: (mn[k], mx[k]) for k in self._input_labels}
+            self._input_bounds = {
+                k: (mn[k], mx[k]) for k in self._input_labels}
 
     def n_inputs(self):
         return len(self._input_labels)
@@ -116,23 +126,26 @@ class SurrogateTrainer(object):
         """
         This method should be overridden by the derived classes.
 
-        The ``train_surrogate`` method is used to train a surrogate model using data provided in set_training_data.
-        This method should return an instance of a derived surrogate object (from SurrogateBase)
+        The ``train_surrogate`` method is used to train a surrogate model
+        using data provided in set_training_data. This method should return an
+        instance of a derived surrogate object (from SurrogateBase)
 
         Returns:
            tuple : (TrainingStatus, surrogate object)
 
         """
-        raise NotImplementedError('train_surrogate called, but not implemented on the derived class')
+        raise NotImplementedError(
+            'train_surrogate called, but not implemented on the derived class')
 
 
 class SurrogateBase():
-    def __init__(self, input_labels=None, output_labels=None, input_bounds=None):
+    def __init__(
+            self, input_labels=None, output_labels=None, input_bounds=None):
         """
-        Base class for standard IDAES Surrogate object. This class is responsible for
-        being able to load/save a surrogate model, evaluate the model given an input
-        dataframe, and populating a block to provide an EO representation of the
-        surrogate for solving in IDAES.
+        Base class for standard IDAES Surrogate object. This class is
+        responsible for being able to load/save a surrogate model, evaluate
+        the model given an input dataframe, and populating a block to provide
+        an EO representation of the surrogate for solving in IDAES.
 
         Args:
            input_labels: list
@@ -216,14 +229,14 @@ class SurrogateBase():
 
     def compute_fit_metrics(self, data):
         """
-        This method computes a variety of metrics regarding the fit of the 
+        This method computes a variety of metrics regarding the fit of the
         surrogate to the data provided.
 
         Args:
            data : pandas DataFrame
-              pandas DataFrame that includes columns for the inputs and the outputs.
-              Metrics for the quality of fit will be computed across all the rows in
-              the provided data.
+              pandas DataFrame that includes columns for the inputs and the
+              outputs. Metrics for the quality of fit will be computed across
+              all the rows in the provided data.
 
         Returns:
            TrainingMetrics object
