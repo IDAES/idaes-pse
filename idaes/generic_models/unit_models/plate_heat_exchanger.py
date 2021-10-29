71abf49c4d38396d2385b8b68e3f61a0d39ba7e5
Paul Akula <paulakula11@gmail.com>
2021-10-29 13:34:31 -0400

    minor updates-removes debugging code

--- idaes/generic_models/unit_models/plate_heat_exchanger.py
+++ idaes/generic_models/unit_models/plate_heat_exchanger.py
@@ -253,7 +253,7 @@ class PlateHeatExchangerData(UnitModelBlockData):
 
     def _make_params(self):
         self.P = Param(initialize=self.config.passes,
-                       units=None,
+                       units=pyunits.dimensionless,
                        doc="Total number of passes for hot or cold fluid")
         self.PH = RangeSet(self.P,
                            doc="Set of hot fluid passes")
@@ -275,7 +275,7 @@ class PlateHeatExchangerData(UnitModelBlockData):
                               units=pyunits.m,
                               doc=" Port diameter of plate ")
         self.Np = Param(self.PH,
-                        units=None,
+                        units=pyunits.dimensionless,
                         doc="Number of channels in each pass", mutable=True)
         # Number of channels in each pass
         for i in self.PH:
@@ -543,7 +543,7 @@ class PlateHeatExchangerData(UnitModelBlockData):
             return (2 * blk.fric_c[t] * (blk.plate_length + blk.port_dia) *
                     blk.P * blk.Gpc[t]**2) /\
                 (blk.cold_fluid.properties_in[t].dens_mass * blk.channel_dia) +\
-                1.4 * (blk.P * blk.Gpc[t]**2 * 0.5 /
+                0.7 * (blk.P * blk.Gpc[t]**2  /
                        blk.cold_fluid.properties_in[t].dens_mass) + \
                 blk.cold_fluid.properties_in[t].dens_mass * \
                 CONST.acceleration_gravity * (blk.plate_length + blk.port_dia)
@@ -825,161 +825,3 @@ class PlateHeatExchangerData(UnitModelBlockData):
             },
             time_point=time_point,
         )
-
-
-if __name__ == '__main__':
-
-    from pyomo.environ import ConcreteModel, value
-    from idaes.core import FlowsheetBlock
-    import matplotlib.pyplot as plt
-
-    from idaes.generic_models.properties.core.generic.generic_property import (
-        GenericParameterBlock)
-    from idaes.power_generation.carbon_capture.mea_solvent_system.properties.MEA_solvent \
-        import configuration as aqueous_mea
-
-    from idaes.core.util.model_statistics import (degrees_of_freedom,
-                                                  number_variables,
-                                                  number_total_constraints,
-                                                  number_unused_variables)
-
-    solver = get_solver()
-    m = ConcreteModel()
-    m.fs = FlowsheetBlock(default={"dynamic": False})
-    # Set up property package
-    m.fs.hotside_properties = GenericParameterBlock(default=aqueous_mea)
-    m.fs.coldside_properties = GenericParameterBlock(default=aqueous_mea)
-
-    # create instance of plate heat exchanger  on flowsheet
-    m.fs.unit = PlateHeatExchanger(default={'passes': 4,
-                                            'channel_list': [12, 12, 12, 12],
-                                            'divider_plate_number': 2,
-                                            "hot_side": {
-                                                "property_package": m.fs.hotside_properties
-                                            },
-                                            "cold_side": {
-                                                "property_package": m.fs.coldside_properties
-                                            }})
-    # hot fluid
-    m.fs.unit.hot_inlet.flow_mol[0].fix(60.54879)
-    m.fs.unit.hot_inlet.temperature[0].fix(392.23)
-    m.fs.unit.hot_inlet.pressure[0].fix(202650)
-    m.fs.unit.hot_inlet.mole_frac_comp[0, "CO2"].fix(0.0158)
-    m.fs.unit.hot_inlet.mole_frac_comp[0, "H2O"].fix(0.8747)
-    m.fs.unit.hot_inlet.mole_frac_comp[0, "MEA"].fix(0.1095)
-
-    # cold fluid
-    m.fs.unit.cold_inlet.flow_mol[0].fix(63.01910)
-    m.fs.unit.cold_inlet.temperature[0].fix(326.36)
-    m.fs.unit.cold_inlet.pressure[0].fix(202650)
-    m.fs.unit.cold_inlet.mole_frac_comp[0, "CO2"].fix(0.0414)
-    m.fs.unit.cold_inlet.mole_frac_comp[0, "H2O"].fix(0.8509)
-    m.fs.unit.cold_inlet.mole_frac_comp[0, "MEA"].fix(0.1077)
-
-    print('dof = {}'.format(degrees_of_freedom(m.fs.unit)))
-    m.fs.unit.initialize()
-    solver.solve(m.fs.unit)
-    print('PC_out = {}'.format(value(m.fs.unit.cold_outlet.pressure[0])))
-    print('PH_out = {}'.format(value(m.fs.unit.hot_outlet.pressure[0])))
-    m.fs.unit.report()
-
-    '''
-    #molar flowrate
-    hot_molar_flowrate =  [60.54879,102.07830,60.05750,58.86128,104.30124,105.49475,27.53904,
-                           60.88287,60.88904,60.04379,77.69829,76.66811,59.68288]
-    cold_molar_flowrate = [63.01910,104.99350,62.53341,60.40000,106.18207,107.38606, 28.19399,
-                           63.60044,63.16421,61.14541,81.36657,79.89472,60.39896]
-
-    # plant inlet temperature
-    hot_Temp_IN  = [392.23,389.57,393.78,382.42,376.32,392.69,389.69,392.10,392.36,392.30,391.19,390.95,392.26]
-    cold_Temp_IN = [326.36,332.26,329.12,318.82,319.58,330.54,321.42,327.72,327.47,326.72,328.59,325.44,326.11]
-
-    # plant exit temperature
-    NCCC_hot_Temp_OUT=[330.42,336.70,331.44,323.41,324.57,334.63,324.83,331.25,331.08,330.43,332.96,329.96,329.75]
-    NCCC_cold_Temp_OUT= [384.9111111,383.2111111,383.6944444,376.1722222,370.5,384.9555556,382.0388889,
-                        384.6388889,384.8055556,384.5888889,384.3277778,383.5333333,384.5277778]
-
-    # hot-side (lean solvent) inlet mole fraction
-    xh_H2O=[0.8747,0.8569,0.8739,0.8505,0.8573,0.8808,0.8582,0.8757,0.8758,0.8702,0.8646,0.8590,0.8676]
-    xh_MEA=[0.1095,0.1148,0.1138,0.1110,0.1020,0.1033,0.1144,0.1070,0.1071,0.1115,0.1106,0.1152,0.1134]
-    xh_CO2=[0.0158,0.0284,0.0123,0.0385,0.0407,0.0160,0.0274,0.0172,0.0171,0.0183,0.0248,0.0258,0.0190]
-
-    # cold-side(rich solvent) inlet mole fraction
-    xc_H2O=[0.8509,0.8426,0.8546,0.8378,0.8501,0.8676,0.8315,0.8554,0.8586,0.8490,0.8468,0.8408,0.8455]
-    xc_MEA=[0.1077,0.1137,0.1123,0.1104,0.1019,0.1038,0.1143,0.1050,0.1055,0.1110,0.1079,0.1127,0.1141]
-    xc_CO2=[0.0414,0.0438,.0331,0.0518,0.0480,0.0286,0.0541,0.0397,0.0360,0.0400,0.0453,0.0465,0.0404]
-
-    # create list to save results
-    PHE_THOUT=[]
-    PHE_TCOUT=[]
-
-    # fix the inputs, solve and save results
-    for i in range(1):
-        # hot fluid
-        m.fs.unit.hot_inlet.flow_mol[0].fix(hot_molar_flowrate[i])
-        m.fs.unit.hot_inlet.temperature[0].fix(hot_Temp_IN[i])
-        m.fs.unit.hot_inlet.mole_frac_comp[0,"CO2"].fix(xh_CO2[i])
-        m.fs.unit.hot_inlet.mole_frac_comp[0,"H2O"].fix(xh_H2O[i])
-        m.fs.unit.hot_inlet.mole_frac_comp[0,"MEA"].fix(xh_MEA[i])
-
-        #cold fluid
-        m.fs.unit.cold_inlet.flow_mol[0].fix(cold_molar_flowrate[i])
-        m.fs.unit.cold_inlet.temperature[0].fix(cold_Temp_IN[i])
-        m.fs.unit.cold_inlet.mole_frac_comp[0,"CO2"].fix(xc_CO2[i])
-        m.fs.unit.cold_inlet.mole_frac_comp[0,"H2O"].fix(xc_H2O[i])
-        m.fs.unit.cold_inlet.mole_frac_comp[0,"MEA"].fix(xc_MEA[i])
-
-        solver.solve(m.fs.unit,tee=False)
-        PHE_THOUT.append(value(m.fs.unit.hot_outlet.temperature[0]))
-        PHE_TCOUT.append(value(m.fs.unit.cold_outlet.temperature[0]))
-
-    m.fs.unit.report()
-
-
-
-    fontsize = 16
-    labelsize = 16
-    markersize=12
-
-    x= [i for i in range(1,len(NCCC_cold_Temp_OUT)+1)]
-
-    plt.figure(figsize=(8,6))
-
-    plt.plot(x,NCCC_cold_Temp_OUT,
-            color='g',
-            linestyle='',
-            label='Data: Rich solvent',
-            mfc="None",
-            marker='s',
-            markersize=markersize)
-    plt.plot(x,PHE_TCOUT,
-            color='g',
-            linestyle='',
-            label='Model: Rich solvent',
-            mfc="None",
-            marker='x',
-            markersize=markersize)
-    plt.plot(x,NCCC_hot_Temp_OUT,
-            color='r',
-            linestyle='',
-            label='Data: Lean solvent',
-            mfc="None",
-            marker='o',
-            markersize=markersize)
-    plt.plot(x,PHE_THOUT,
-            color='r',
-            linestyle='',
-            label='Model: Lean solvent',
-            mfc="None",
-            marker='+',
-            markersize=markersize)
-
-    plt.ylim(278,400)
-    plt.ylabel('Temperature (K)',fontsize=fontsize,fontweight='bold')
-    plt.xlabel('NCCC Case No.',fontsize=fontsize,fontweight='bold')
-    plt.legend(loc='lower right',fontsize=fontsize)
-    plt.tick_params(labelsize=labelsize)
-    plt.tight_layout()
-    plt.show()
-    '''
-    xgy = 1
