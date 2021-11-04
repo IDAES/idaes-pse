2806f7b0f5bd12b424b1c3344f1d1cfff6140c62
Andrew Lee <andrew.lee@netl.doe.gov>
2021-11-04 17:38:05 -0400

    Adding more tests for hx_ntu model

--- idaes/generic_models/unit_models/tests/test_hx_ntu.py
+++ idaes/generic_models/unit_models/tests/test_hx_ntu.py
@@ -17,74 +17,52 @@ Author: Akula Paul, Andrew Lee
 
 import pytest
 from pyomo.environ import (ConcreteModel,
+                           Constraint,
+                           Expression,
+                           Param,
                            TerminationCondition,
                            SolverStatus,
-                           units,
+                           units as pyunits,
                            value,
                            Var)
-from idaes.core import (FlowsheetBlock,
-                        MaterialBalanceType,
-                        EnergyBalanceType,
-                        MomentumBalanceType)
-from idaes.generic_models.unit_models.plate_heat_exchanger import (
-    PlateHeatExchanger as PHE)
+from pyomo.util.check_units import (assert_units_consistent,
+                                    assert_units_equivalent)
+
+from idaes.core import FlowsheetBlock
+from idaes.generic_models.unit_models.heat_exchanger_ntu import (
+    HeatExchangerNTU as HXNTU)
 
 from idaes.generic_models.properties.core.generic.generic_property import (
     GenericParameterBlock)
 from idaes.power_generation.carbon_capture.mea_solvent_system.properties.MEA_solvent \
     import configuration as aqueous_mea
 from idaes.core.util.model_statistics import degrees_of_freedom
-from idaes.core.util.testing import (PhysicalParameterTestBlock,
-                                     ReactionParameterTestBlock,
-                                     initialization_tester)
 from idaes.core.util import get_solver
-from pyomo.util.check_units import (assert_units_consistent,
-                                    assert_units_equivalent)
+from idaes.core.util.testing import initialization_tester
 
 
 # -----------------------------------------------------------------------------
 # Get default solver for testing
 solver = get_solver()
 
-# @pytest.mark.unit
-# def test_config():
-#     m = ConcreteModel()
-#     m.fs = FlowsheetBlock(default={"dynamic": False})
-
-#     m.fs.hotside_properties = GenericParameterBlock(default=aqueous_mea)
-#     m.fs.coldside_properties = GenericParameterBlock(default=aqueous_mea)
-
-#     m.fs.unit = PHE(default={'passes': 4,
-#                              'channel_list': [12, 12, 12, 12],
-#                              'divider_plate_number': 2,
-#                              "hot_side": {
-#                                  "property_package": m.fs.hotside_properties
-#                              },
-#                              "cold_side": {
-#                                  "property_package": m.fs.coldside_properties
-#                              }})
-
-#     # Check unit config arguments
-#     assert len(m.fs.unit.config) == 16
-#     assert len(m.fs.unit.config.hot_side) == 2
-#     assert len(m.fs.unit.config.cold_side) == 2
-
 
 # -----------------------------------------------------------------------------
-class TestPHE(object):
+class TestHXNTU(object):
     @pytest.fixture(scope="class")
-    def phe(self):
+    def model(self):
         m = ConcreteModel()
         m.fs = FlowsheetBlock(default={"dynamic": False})
 
         m.fs.hotside_properties = GenericParameterBlock(default=aqueous_mea)
         m.fs.coldside_properties = GenericParameterBlock(default=aqueous_mea)
 
-        m.fs.unit = PHE(default={
-            "hot_side": {"property_package": m.fs.hotside_properties},
-            "cold_side": {"property_package": m.fs.coldside_properties}})
+        m.fs.unit = HXNTU(default={
+            "hot_side": {"property_package": m.fs.hotside_properties,
+                         "has_pressure_change": True},
+            "cold_side": {"property_package": m.fs.coldside_properties,
+                          "has_pressure_change": True}})
 
-        # hot fluid
+        # Hot fluid
         m.fs.unit.hot_inlet.flow_mol[0].fix(60.54879)
         m.fs.unit.hot_inlet.temperature[0].fix(392.23)
         m.fs.unit.hot_inlet.pressure[0].fix(202650)
@@ -92,7 +70,7 @@ class TestPHE(object):
         m.fs.unit.hot_inlet.mole_frac_comp[0, "H2O"].fix(0.8747)
         m.fs.unit.hot_inlet.mole_frac_comp[0, "MEA"].fix(0.1095)
 
-        # cold fluid
+        # Cold fluid
         m.fs.unit.cold_inlet.flow_mol[0].fix(63.01910)
         m.fs.unit.cold_inlet.temperature[0].fix(326.36)
         m.fs.unit.cold_inlet.pressure[0].fix(202650)
@@ -100,119 +78,146 @@ class TestPHE(object):
         m.fs.unit.cold_inlet.mole_frac_comp[0, "H2O"].fix(0.8509)
         m.fs.unit.cold_inlet.mole_frac_comp[0, "MEA"].fix(0.1077)
 
+        # Unit design variables
+        m.fs.unit.area.fix(100)
+        m.fs.unit.heat_transfer_coefficient.fix(200)
+        m.fs.unit.effectiveness.fix(0.7)
+
+        m.fs.unit.hot_side.deltaP.fix(-2000)
+        m.fs.unit.cold_side.deltaP.fix(-2000)
+
         return m
 
     @pytest.mark.build
     @pytest.mark.unit
-    def test_build(self, phe):
+    def test_build(self, model):
 
-        assert hasattr(phe.fs.unit, "hot_inlet")
-        assert len(phe.fs.unit.hot_inlet.vars) == 4
-        assert hasattr(phe.fs.unit.hot_inlet, "flow_mol")
-        assert hasattr(phe.fs.unit.hot_inlet, "mole_frac_comp")
-        assert hasattr(phe.fs.unit.hot_inlet, "temperature")
-        assert hasattr(phe.fs.unit.hot_inlet, "pressure")
+        assert hasattr(model.fs.unit, "hot_inlet")
+        assert len(model.fs.unit.hot_inlet.vars) == 4
+        assert hasattr(model.fs.unit.hot_inlet, "flow_mol")
+        assert hasattr(model.fs.unit.hot_inlet, "mole_frac_comp")
+        assert hasattr(model.fs.unit.hot_inlet, "temperature")
+        assert hasattr(model.fs.unit.hot_inlet, "pressure")
 
-        assert hasattr(phe.fs.unit, "hot_outlet")
-        assert len(phe.fs.unit.hot_outlet.vars) == 4
-        assert hasattr(phe.fs.unit.hot_outlet, "flow_mol")
-        assert hasattr(phe.fs.unit.hot_outlet, "mole_frac_comp")
-        assert hasattr(phe.fs.unit.hot_outlet, "temperature")
-        assert hasattr(phe.fs.unit.hot_outlet, "pressure")
+        assert hasattr(model.fs.unit, "hot_outlet")
+        assert len(model.fs.unit.hot_outlet.vars) == 4
+        assert hasattr(model.fs.unit.hot_outlet, "flow_mol")
+        assert hasattr(model.fs.unit.hot_outlet, "mole_frac_comp")
+        assert hasattr(model.fs.unit.hot_outlet, "temperature")
+        assert hasattr(model.fs.unit.hot_outlet, "pressure")
 
-        assert hasattr(phe.fs.unit, "cold_inlet")
-        assert len(phe.fs.unit.cold_inlet.vars) == 4
-        assert hasattr(phe.fs.unit.cold_inlet, "flow_mol")
-        assert hasattr(phe.fs.unit.cold_inlet, "mole_frac_comp")
-        assert hasattr(phe.fs.unit.cold_inlet, "temperature")
-        assert hasattr(phe.fs.unit.cold_inlet, "pressure")
+        assert hasattr(model.fs.unit, "cold_inlet")
+        assert len(model.fs.unit.cold_inlet.vars) == 4
+        assert hasattr(model.fs.unit.cold_inlet, "flow_mol")
+        assert hasattr(model.fs.unit.cold_inlet, "mole_frac_comp")
+        assert hasattr(model.fs.unit.cold_inlet, "temperature")
+        assert hasattr(model.fs.unit.cold_inlet, "pressure")
 
-        assert hasattr(phe.fs.unit, "cold_outlet")
-        assert len(phe.fs.unit.cold_outlet.vars) == 4
-        assert hasattr(phe.fs.unit.cold_outlet, "flow_mol")
-        assert hasattr(phe.fs.unit.cold_outlet, "mole_frac_comp")
-        assert hasattr(phe.fs.unit.cold_outlet, "temperature")
-        assert hasattr(phe.fs.unit.cold_outlet, "pressure")
+        assert hasattr(model.fs.unit, "cold_outlet")
+        assert len(model.fs.unit.cold_outlet.vars) == 4
+        assert hasattr(model.fs.unit.cold_outlet, "flow_mol")
+        assert hasattr(model.fs.unit.cold_outlet, "mole_frac_comp")
+        assert hasattr(model.fs.unit.cold_outlet, "temperature")
+        assert hasattr(model.fs.unit.cold_outlet, "pressure")
 
-        assert hasattr(phe.fs.unit, "heat_duty")
-        assert hasattr(phe.fs.unit.cold_side, "heat")
-        assert hasattr(phe.fs.unit.hot_side, "heat")
+        assert hasattr(model.fs.unit, "heat_duty")
+        assert hasattr(model.fs.unit.cold_side, "heat")
+        assert hasattr(model.fs.unit.hot_side, "heat")
 
-        assert hasattr(phe.fs.unit.cold_side, "deltaP")
-        assert hasattr(phe.fs.unit.hot_side, "deltaP")
+        assert hasattr(model.fs.unit.cold_side, "deltaP")
+        assert hasattr(model.fs.unit.hot_side, "deltaP")
+
+        assert isinstance(model.fs.unit.area, Var)
+        assert isinstance(model.fs.unit.heat_transfer_coefficient, Var)
+        assert isinstance(model.fs.unit.effectiveness, Var)
+
+        assert isinstance(model.fs.unit.eps_cmin, Param)
+        assert value(model.fs.unit.eps_cmin) == 1e-3
+
+        assert isinstance(model.fs.unit.Cmin, Expression)
+        assert isinstance(model.fs.unit.Cmax, Expression)
+        assert isinstance(model.fs.unit.NTU, Expression)
+
+        assert isinstance(model.fs.unit.energy_balance_constraint, Constraint)
+        assert isinstance(model.fs.unit.heat_duty_constraint, Constraint)
 
     @pytest.mark.component
-    def test_units(self, phe):
-        assert_units_consistent(phe)
-        # assert_units_equivalent(phe.fs.unit.plate_length, units.m)
-        # assert_units_equivalent(phe.fs.unit.cold_fluid.deltaP[0], units.Pa)
-        # assert_units_equivalent(phe.fs.unit.hot_fluid.deltaP[0], units.Pa)
+    def test_units(self, model):
+        assert_units_consistent(model)
+
+        assert_units_equivalent(model.fs.unit.area, pyunits.m**2)
+        assert_units_equivalent(model.fs.unit.heat_transfer_coefficient,
+                                pyunits.W/pyunits.m**2/pyunits.K)
+        assert_units_equivalent(model.fs.unit.effectiveness[0],
+                                pyunits.dimensionless)
+        assert_units_equivalent(model.fs.unit.NTU[0],
+                                pyunits.dimensionless)
 
     @pytest.mark.unit
-    def test_dof(self, phe):
-        assert degrees_of_freedom(phe) == 3
+    def test_dof(self, model):
+        assert degrees_of_freedom(model) == 0
+
+    @pytest.mark.solver
+    @pytest.mark.skipif(solver is None, reason="Solver not available")
+    @pytest.mark.component
+    def test_initialize(self, model):
+        initialization_tester(model)
+
+    @pytest.mark.solver
+    @pytest.mark.skipif(solver is None, reason="Solver not available")
+    @pytest.mark.component
+    def test_solve(self, model):
+        results = solver.solve(model)
+
+        # Check for optimal solution
+        assert results.solver.termination_condition == \
+            TerminationCondition.optimal
+        assert results.solver.status == SolverStatus.ok
 
     # @pytest.mark.solver
     # @pytest.mark.skipif(solver is None, reason="Solver not available")
     # @pytest.mark.component
-    # def test_initialize(self, phe):
-    #     initialization_tester(phe)
-
-    # @pytest.mark.solver
-    # @pytest.mark.skipif(solver is None, reason="Solver not available")
-    # @pytest.mark.component
-    # def test_solve(self, phe):
-    #     results = solver.solve(phe)
-
-    #     # Check for optimal solution
-    #     assert results.solver.termination_condition == \
-    #         TerminationCondition.optimal
-    #     assert results.solver.status == SolverStatus.ok
-
-    # @pytest.mark.solver
-    # @pytest.mark.skipif(solver is None, reason="Solver not available")
-    # @pytest.mark.component
-    # def test_solution(self, phe):
+    # def test_solution(self, model):
     #     assert (pytest.approx(182282.48, abs=1e-2) ==
-    #             value(phe.fs.unit.hot_outlet.pressure[0]))
+    #             value(model.fs.unit.hot_outlet.pressure[0]))
     #     assert (pytest.approx(177774.85, abs=1e-2) ==
-    #             value(phe.fs.unit.cold_outlet.pressure[0]))
+    #             value(model.fs.unit.cold_outlet.pressure[0]))
 
     #     assert (pytest.approx(329.54, abs=1e-2) ==
-    #             value(phe.fs.unit.hot_outlet.temperature[0]))
+    #             value(model.fs.unit.hot_outlet.temperature[0]))
     #     assert (pytest.approx(385.32, abs=1e-2) ==
-    #             value(phe.fs.unit.cold_outlet.temperature[0]))
+    #             value(model.fs.unit.cold_outlet.temperature[0]))
 
     #     assert (pytest.approx(0.015800, abs=1e-4) ==
-    #             value(phe.fs.unit.hot_outlet.mole_frac_comp[0, "CO2"]))
+    #             value(model.fs.unit.hot_outlet.mole_frac_comp[0, "CO2"]))
     #     assert (pytest.approx(0.10950, abs=1e-4) ==
-    #             value(phe.fs.unit.hot_outlet.mole_frac_comp[0, "MEA"]))
+    #             value(model.fs.unit.hot_outlet.mole_frac_comp[0, "MEA"]))
     #     assert (pytest.approx(0.87470, abs=1e-4) ==
-    #             value(phe.fs.unit.hot_outlet.mole_frac_comp[0, "H2O"]))
+    #             value(model.fs.unit.hot_outlet.mole_frac_comp[0, "H2O"]))
 
     #     assert (pytest.approx(0.041400, abs=1e-4) ==
-    #             value(phe.fs.unit.cold_outlet.mole_frac_comp[0, "CO2"]))
+    #             value(model.fs.unit.cold_outlet.mole_frac_comp[0, "CO2"]))
     #     assert (pytest.approx(0.10770, abs=1e-4) ==
-    #             value(phe.fs.unit.cold_outlet.mole_frac_comp[0, "MEA"]))
+    #             value(model.fs.unit.cold_outlet.mole_frac_comp[0, "MEA"]))
     #     assert (pytest.approx(0.85090, abs=1e-4) ==
-    #             value(phe.fs.unit.cold_outlet.mole_frac_comp[0, "H2O"]))
+    #             value(model.fs.unit.cold_outlet.mole_frac_comp[0, "H2O"]))
 
     # @pytest.mark.solver
     # @pytest.mark.skipif(solver is None, reason="Solver not available")
     # @pytest.mark.component
-    # def test_conservation(self, phe):
+    # def test_conservation(self, model):
     #     # Mass conservation test
-    #     assert abs(value(phe.fs.unit.hot_inlet.flow_mol[0] -
-    #                      phe.fs.unit.hot_outlet.flow_mol[0])) <= 1e-6
+    #     assert abs(value(model.fs.unit.hot_inlet.flow_mol[0] -
+    #                      model.fs.unit.hot_outlet.flow_mol[0])) <= 1e-6
 
-    #     assert abs(value(phe.fs.unit.cold_inlet.flow_mol[0] -
-    #                      phe.fs.unit.cold_outlet.flow_mol[0])) <= 1e-6
+    #     assert abs(value(model.fs.unit.cold_inlet.flow_mol[0] -
+    #                      model.fs.unit.cold_outlet.flow_mol[0])) <= 1e-6
 
     #     # Energy conservation test
-    #     assert abs(value(phe.fs.unit.heat_lost[0] -
-    #                      phe.fs.unit.heat_gain[0])) <=1e-6
+    #     assert abs(value(model.fs.unit.heat_lost[0] -
+    #                      model.fs.unit.heat_gain[0])) <=1e-6
 
-    # @pytest.mark.ui
-    # @pytest.mark.unit
-    # def test_report(self, phe):
-    #     phe.fs.unit.report()
+    @pytest.mark.ui
+    @pytest.mark.unit
+    def test_report(self, model):
+        model.fs.unit.report()
