8ec87721045219d9cb5dc6d705b3223276c70ed2
John Siirola <jsiirola@users.noreply.github.com>
2021-11-17 12:13:49 -0700

    Bumping to a RC tag to test proposed Pyomo regression fixes

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def rglob(path, glob):
 
 
 DEPENDENCIES_FOR_PRERELEASE_VERSION = [
-    "pyomo @ https://github.com/IDAES/pyomo/archive/6.2.zip",
+    "pyomo @ https://github.com/IDAES/pyomo/archive/6.2.0.rc1.idaes.2021.11.17.zip",
 ]
 
 
