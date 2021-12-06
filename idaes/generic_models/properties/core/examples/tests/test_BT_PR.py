c359ff49a6ca988f8ebc10ddaceaa54495063e47
Doug Allan <dallan@keylogic.com>
2021-12-06 10:26:45 -0500

    temporary commit so I can merge

--- idaes/generic_models/properties/core/examples/tests/test_BT_PR.py
+++ idaes/generic_models/properties/core/examples/tests/test_BT_PR.py
@@ -31,7 +31,10 @@ from pyomo.environ import (ConcreteModel,
                            TerminationCondition,
                            value)
 
+import idaes
 from idaes.core.util import get_solver
+from idaes.core.solvers import use_idaes_solver_configuration_defaults
+import idaes.core.util.scaling as iscale
 
 import idaes.logger as idaeslog
 SOUT = idaeslog.INFO
@@ -43,31 +46,34 @@ prop_available = cubic_roots_available()
 
 # -----------------------------------------------------------------------------
 # Get default solver for testing
+use_idaes_solver_configuration_defaults()
+idaes.cfg.ipopt["options"]["nlp_scaling_method"] = "user-scaling"
 solver = get_solver()
 # Limit iterations to make sure sweeps aren;t getting out of hand
-solver.options["max_iter"] = 50
+solver.options["max_iter"] = 60
 
 
 # -----------------------------------------------------------------------------
 # Test robustness and some outputs
 class TestBTExample(object):
-    @pytest.fixture()
+    #@pytest.fixture()
     def m(self):
         m = ConcreteModel()
 
         m.fs = FlowsheetBlock(default={'dynamic': False})
 
         m.fs.props = GenericParameterBlock(default=configuration)
+        m.fs.props.set_default_scaling("mole_frac_comp", 1e1)
 
         m.fs.state = m.fs.props.build_state_block(
                 [1],
                 default={"defined_state": True})
 
-        m.fs.state[1].calculate_scaling_factors()
-
+        iscale.calculate_scaling_factors(m.fs.props)
+        iscale.calculate_scaling_factors(m.fs.state[1])
         return m
 
-    @pytest.mark.integration
+    #@pytest.mark.integration
     def test_T_sweep(self, m):
         assert_units_consistent(m)
 
@@ -75,6 +81,7 @@ class TestBTExample(object):
         m.fs.state[1].temperature.setub(600)
 
         for logP in range(8, 13, 1):
+            print(logP)
             m.fs.obj.deactivate()
 
             m.fs.state[1].flow_mol.fix(100)
@@ -82,13 +89,20 @@ class TestBTExample(object):
             m.fs.state[1].mole_frac_comp["toluene"].fix(0.5)
             m.fs.state[1].temperature.fix(300)
             m.fs.state[1].pressure.fix(10**(0.5*logP))
-
+            
             m.fs.state.initialize()
 
             m.fs.state[1].temperature.unfix()
             m.fs.obj.activate()
-
-            results = solver.solve(m)
+            
+            iscale.calculate_scaling_factors(m.fs.state[1])
+            iscale.constraint_autoscale_large_jac(m)
+            
+            if logP == 10:
+                #import pdb; pdb.set_trace()
+                results = solver.solve(m, tee=True)
+            else:
+                results = solver.solve(m, tee=True)
 
             assert results.solver.termination_condition == \
                 TerminationCondition.optimal
@@ -540,3 +554,7 @@ class TestBTExample(object):
             m.fs.state[1].temperature_bubble["Vap", "Liq"]] == 1e-2
         assert m.fs.state[1].scaling_factor[
             m.fs.state[1].temperature_dew["Vap", "Liq"]] == 1e-2
+
+if __name__ == "__main__":
+    m =  TestBTExample.m(None)
+    TestBTExample.test_T_sweep(None, m)
