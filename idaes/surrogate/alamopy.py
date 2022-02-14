6072ebd9ab8f69c368f2b889968b12493d5645da
Andrew Lee <andrew.lee@netl.doe.gov>
2022-02-14 11:50:01 -0500

    Fixing another typo

--- idaes/surrogate/alamopy.py
+++ idaes/surrogate/alamopy.py
@@ -860,7 +860,7 @@ class AlamoTrainer(SurrogateTrainer):
                             f"Mismatch when reading ALAMO trace file. "
                             f"Expected OUTPUT = {j+1}, found {trace_val}.")
                 elif header == "SET":
-                    # SET should always be 0 - higher numebrs are for
+                    # SET should always be 0 - higher numbers are for
                     # valdiation data sets
                     if trace_val != "0":
                         raise RuntimeError(
