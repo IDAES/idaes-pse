0a47ebc9368e8880e7713a32161100606e9098dd
John Siirola <jsiirola@users.noreply.github.com>
2022-02-24 08:25:06 -0700

    Bump to Pyomo 6.3.0 release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def rglob(path, glob):
 
 
 DEPENDENCIES_FOR_PRERELEASE_VERSION = [
-    "pyomo @ https://github.com/IDAES/pyomo/archive/6.3.0.rc1.idaes.2022.02.23.zip"
+    "pyomo @ https://github.com/IDAES/pyomo/archive/6.3.0.zip"
 ]
 
 
