657e6d0dde173c5ad0c6b688401f101545b66733
joshuahaddad <joshuahaddad@ufl.edu>
2022-03-07 10:32:18 -0500

    Changing organization and start of error warnings

--- idaes/surrogate/keras/prune.py
+++ idaes/surrogate/keras/prune.py
@@ -11,15 +11,18 @@
 # license information.
 #################################################################################
 """
-Utility function to prune inactive nodes from keras sequential models
+Utility functions to prune nodes from keras models
 """
 
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential
 import numpy as np
 
-# Finds all nodes in a layer that contribute nothing to the NN
-def get_inactive_nodes(l_w, l_w_forward):
-
+"""
+Finds all nodes which have constant output in a layer.
+These nodes may affect NN output (i.e. non-zero bias, activation(0) != 0, activation_forward(0) != 0, etc) but do not
+change with the inputs of the neural network.
+"""
+def get_constant_nodes(l_w, l_w_forward):
     # Reshape the layer so it is in the form [node #, node weights]
     l_w = np.transpose(l_w)
 
@@ -34,10 +37,9 @@ def get_inactive_nodes(l_w, l_w_forward):
     for node_idx, node_w in enumerate(l_w):
         node_w = node_w.flatten()
 
-        # If the weight array is filled with 0s, the node is inactive
+        # If the weight array is filled with 0s it has a constant output and should be pruned
         if np.count_nonzero(node_w) == 0:
             inactive_nodes.append(node_idx)
-            continue
 
         # Else check the upstream nodes. If all upstream nodes have w = 0 for the downstream node, it is inactive
         elif l_w_forward is not None:
@@ -58,23 +60,24 @@ def get_inactive_nodes(l_w, l_w_forward):
 
     return inactive_nodes
 
+
 # Construct weight matrix for new layer
 def rebuild_weights(w, remove_nodes):
-
     for l, removal_indexes in enumerate(remove_nodes.values()):
         # Remove biases
-        w[2*l+1] = np.delete(w[2*l+1], removal_indexes, axis=0)
+        w[2 * l + 1] = np.delete(w[2 * l + 1], removal_indexes, axis=0)
 
         # Remove weight columns (current layer)
-        w[2*l] = np.delete(w[2*l], removal_indexes, axis=1)
+        w[2 * l] = np.delete(w[2 * l], removal_indexes, axis=1)
 
         # Remove weights of nodes from previous layer that were removed
         if l != 0:
-            w[2*l] = np.delete(w[2*l], last_index, axis=0)
+            w[2 * l] = np.delete(w[2 * l], last_index, axis=0)
 
         last_index = removal_indexes
 
     return w
+
 
 # Constructs a new compiled neural net with fewer nodes using an updated weight matrix
 def build_new_NN(model_old, w_new):
@@ -83,12 +86,11 @@ def build_new_NN(model_old, w_new):
 
         # Check if the layer has a units key
         if 'config' in layer_config and 'units' in layer_config['config']:
-
             # Config has extra input layer (not in w matrix) so adjust the index to match previous functions
-            l_idx = i-1
+            l_idx = i - 1
 
             # Get the new number of neurons by looking at the weights matrix
-            neurons = w_new[l_idx*2].shape[-1]
+            neurons = w_new[l_idx * 2].shape[-1]
 
             # Change the layer config
             layer_config['config']['units'] = neurons
@@ -100,24 +102,73 @@ def build_new_NN(model_old, w_new):
     model.set_weights(w_new)
     return model
 
-# Deletes nodes which have no impact on the output
-def prune_NN(model):
+def count_nodes(model):
+    cfg = model.get_config()
+    N_nodes = 0
+    for i, layer_config in enumerate(cfg['layers']):
+        if 'config' in layer_config and 'units' in layer_config['config']:
+            N_nodes += layer_config['config']['units']
+    return N_nodes
+
+def pruning_step(model):
     w = model.get_weights()
 
     # Container to keep track of all nodes within a layer that must be removed, indexed by [layer][idx node to remove]
     nodes_to_remove = {}
 
-    # Find the nodes indexes were wi = 0 for all weights in the node
+    # Conduct one pruning step on the model
     for l in range(0, len(w), 2):
+        l_w = w[l]
+        l_b = w[l + 1]
 
-        # If there is a layer ahead of this layer, grab the layers weights
-        l_w_forward = w[l+2] if l < len(w)-2 else None
+        # If there is a layer ahead of this layer, get the forward layer weights
+        l_w_forward = w[l + 2] if l < len(w) - 2 else None
 
         # Get an array of indexes for the nodes that must be removed and append to the main list
-        nodes_to_remove[l] = get_inactive_nodes(w[l], l_w_forward)
+        nodes_to_remove[l] = get_constant_nodes(l_w, l_w_forward)
+
+        # Check for removal of input layer nodes (l=0, non-empty nodes_to_remove)
+        """if l == 0 and nodes_to_remove:
+            print(f"Warning: input nodes {nodes_to_remove} have constant output and are being removed."
+                  f"The returned NN will have a different input shape.")
+
+        # Check for the removal of output layer nodes (l=len(w)-2, non-empty nodes_to_remove)
+        if l == len(w) - 2 and nodes_to_remove:
+            # Get bias value for removed nodes as this is the constant value of the output
+            biases_removed_nodes = [l_b[idx] for idx in nodes_to_remove]
+
+            print(
+                f"Warning: output nodes {nodes_to_remove} have constant output and are being removed from the NN."
+                f"This indicates the output is constant with the given input variables and is equal to the bias.\n"
+                f"The constant value for these outputs was: {biases_removed_nodes}")"""
 
     w_new = rebuild_weights(w, nodes_to_remove)
 
-    model = build_new_NN(model, w_new)
+    return build_new_NN(model, w_new)
+
+"""
+Prunes nodes from a keras sequential model by removing nodes with constant outputs.
+While inactive nodes have an affect on the output, they are pruned because their contribution can be shifted to active
+nodes. The model should be retrained after pruning to recover accuracy after removal of nodes.
+"""
+def prune_sequential(model, max_steps=100, verbose=0):
+
+    pruning_steps = 0
+
+    # Do while loop to continue pruning until model nodes are constant or max pruning steps occur to prevent inf loop
+    while True:
+
+        initial_nodes = count_nodes(model)
+        model = pruning_step(model)
+        final_nodes = count_nodes(model)
+
+        pruning_steps += 1
+        removed_nodes = initial_nodes - final_nodes
+
+        if verbose == 1:
+            print(f"Pruning step {pruning_steps} removed {removed_nodes} nodes")
+
+        if removed_nodes == 0 or pruning_steps >= max_steps:
+            break
 
     return model
