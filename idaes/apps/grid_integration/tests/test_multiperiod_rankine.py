de4f73170f7f086dca0aaddb222b40540cdfa23d
Xian <xgao1@nd.edu>
2022-03-25 15:24:02 -0400

    reformat test for rankine cycle with black

--- idaes/apps/grid_integration/tests/test_multiperiod_rankine.py
+++ idaes/apps/grid_integration/tests/test_multiperiod_rankine.py
@@ -2,19 +2,21 @@ import pytest
 import pyomo.environ as pyo
 from idaes.apps.grid_integration.tracker import Tracker
 from idaes.apps.grid_integration.bidder import Bidder
-from idaes.apps.grid_integration.examples.multiperiod_rankine.multiperiod_double_loop_rankine import MultiPeriodRankine
+from idaes.apps.grid_integration.examples.multiperiod_rankine.multiperiod_double_loop_rankine import (
+    MultiPeriodRankine,
+)
 from test_bidder import TestingForecaster
 from pyomo.common.unittest import assertStructuredAlmostEqual
 
-#test tracking with MultiPeriodRankine model
+# test tracking with MultiPeriodRankine model
 @pytest.mark.component
 def test_track_market_dispatch():
     tracking_horizon = 4
     n_tracking_hour = 1
     solver = pyo.SolverFactory("ipopt")
 
-    pmin = 20.0 
-    pmax = 100.0 
+    pmin = 20.0
+    pmax = 100.0
     tracking_model_object = MultiPeriodRankine(tracking_horizon, pmin=pmin, pmax=pmax)
 
     tracker_object = Tracker(
@@ -31,8 +33,8 @@ def test_track_market_dispatch():
     assert len(blks) == tracking_horizon
 
     for blk in blks:
-        assert blk.rankine.fs.eq_min_power.lb/1e6 == pmin
-        assert blk.rankine.fs.eq_max_power.ub/1e6 == pmax 
+        assert blk.rankine.fs.eq_min_power.lb / 1e6 == pmin
+        assert blk.rankine.fs.eq_max_power.ub / 1e6 == pmax
 
     for t, dispatch in zip(range(tracking_horizon), market_dispatch):
         assert (
@@ -46,7 +48,8 @@ def test_track_market_dispatch():
         == last_delivered_power
     )
 
-#test bidding with MultiPeriodRankine model
+
+# test bidding with MultiPeriodRankine model
 @pytest.mark.component
 def test_compute_bids():
     bidding_horizon = 4
@@ -55,9 +58,11 @@ def test_compute_bids():
     solver = pyo.SolverFactory("ipopt")
     forecaster = TestingForecaster(horizon=bidding_horizon, n_sample=n_scenario)
 
-    pmin = 20.0 
-    pmax = 100.0 
-    bidding_model_object = MultiPeriodRankine(horizon=bidding_horizon, pmin=pmin, pmax=pmax)
+    pmin = 20.0
+    pmax = 100.0
+    bidding_model_object = MultiPeriodRankine(
+        horizon=bidding_horizon, pmin=pmin, pmax=pmax
+    )
 
     bidder_object = Bidder(
         bidding_model_object=bidding_model_object,
@@ -65,7 +70,7 @@ def test_compute_bids():
         solver=solver,
         forecaster=forecaster,
     )
-    
+
     date = "2021-08-20"
     shift = 1
     fixed_forecast = 30.0 - shift
@@ -74,12 +79,48 @@ def test_compute_bids():
     blks = bidding_model_object.mp_rankine.get_active_process_blocks()
     assert len(blks) == bidding_horizon
 
-    #test against known solution with ipopt
+    # test against known solution with ipopt
     known_solution = {
-    0: {'gen': {20.0:600.0, 40.0:1200.0, 60.0:1800.0, 70.42:2112.6, 80.0:2400.0, 100.0:3000.0}},
-    1: {'gen': {20.0:600.0, 40.0:1200.0, 60.0:1800.0, 80.0:2400.0, 91.34:2740.2, 100.0:3000.0}},
-    2: {'gen': {20.0:600.0, 40.0:1200.0, 60.0:1800.0, 80.0:2400.0, 100.0:3000.0, 108.66:3259.8}},
-    3: {'gen': {20.0:600.0, 40.0:1200.0, 60.0:1800.0, 80.0:2400.0, 100.0:3000.0, 129.58:3887.4}}
+        0: {
+            "gen": {
+                20.0: 600.0,
+                40.0: 1200.0,
+                60.0: 1800.0,
+                70.42: 2112.6,
+                80.0: 2400.0,
+                100.0: 3000.0,
+            }
+        },
+        1: {
+            "gen": {
+                20.0: 600.0,
+                40.0: 1200.0,
+                60.0: 1800.0,
+                80.0: 2400.0,
+                91.34: 2740.2,
+                100.0: 3000.0,
+            }
+        },
+        2: {
+            "gen": {
+                20.0: 600.0,
+                40.0: 1200.0,
+                60.0: 1800.0,
+                80.0: 2400.0,
+                100.0: 3000.0,
+                108.66: 3259.8,
+            }
+        },
+        3: {
+            "gen": {
+                20.0: 600.0,
+                40.0: 1200.0,
+                60.0: 1800.0,
+                80.0: 2400.0,
+                100.0: 3000.0,
+                129.58: 3887.4,
+            }
+        },
     }
 
-    assertStructuredAlmostEqual(bids,known_solution)
+    assertStructuredAlmostEqual(bids, known_solution)
