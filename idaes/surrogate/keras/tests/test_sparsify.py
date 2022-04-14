2a022354c3b61b1f0eb3cf98f2b623f4ded4d90d
Joshua Haddad <joshuahaddad@ufl.edu>
2022-04-14 16:03:36 -0400

    Update headers for tests

--- idaes/surrogate/keras/tests/test_sparsify.py
+++ idaes/surrogate/keras/tests/test_sparsify.py
@@ -1,9 +1,24 @@
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021
+# by the software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia University
+# Research Corporation, et al.  All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
+# license information.
+#################################################################################
+"""
+Tests for Keras Sparsification Utils
+"""
+
 import numpy as np
 import pytest
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Sequential
 from idaes.surrogate.keras.sparsify import *
-
 
 @pytest.mark.unit
 def test_sparsification():
