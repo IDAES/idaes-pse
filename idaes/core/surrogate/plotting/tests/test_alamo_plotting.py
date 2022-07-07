c5e6c3e8113eeeb464334c91b70c792f8f1624cd
Brandon Paul <bpaul@keylogic.com>
2022-07-07 08:49:02 -0700

    Running black

--- idaes/core/surrogate/plotting/tests/test_alamo_plotting.py
+++ idaes/core/surrogate/plotting/tests/test_alamo_plotting.py
@@ -23,10 +23,12 @@ from pyomo.common.tempfiles import TempfileManager
 
 from idaes.core.surrogate.alamopy import AlamoSurrogate
 from idaes.core.surrogate.sampling.data_utils import split_training_validation
-from idaes.core.surrogate.plotting.sm_plotter import (surrogate_scatter2D,
-                                                      surrogate_scatter3D,
-                                                      surrogate_parity,
-                                                      surrogate_residual)
+from idaes.core.surrogate.plotting.sm_plotter import (
+    surrogate_scatter2D,
+    surrogate_scatter3D,
+    surrogate_parity,
+    surrogate_residual,
+)
 
 
 @pytest.fixture
@@ -34,7 +36,8 @@ def alamo_surrogate():
     # import surrogates from external JSON
 
     alamo_surrogate = AlamoSurrogate.load_from_file(
-            os.path.join(this_file_dir(), 'alamo_surrogate.json'))
+        os.path.join(this_file_dir(), "alamo_surrogate.json")
+    )
 
     return alamo_surrogate
 
@@ -44,17 +47,17 @@ def data_validation():
     # import validation data and create dataframe here as well
     # random data subset will change every time, but should be fine
 
-    csv_data = pd.read_csv(os.path.join(this_file_dir(), 'reformer-data.csv'))
+    csv_data = pd.read_csv(os.path.join(this_file_dir(), "reformer-data.csv"))
     data = csv_data.sample(n=100)  # randomly sample points for validation
 
     input_data = data.iloc[:, :2]
     input_labels = input_data.columns
 
     n_data = data[input_labels[0]].size  # size = 100
-    data_training, data_validation = split_training_validation(
-        data, 0.8, seed=n_data)
+    data_training, data_validation = split_training_validation(data, 0.8, seed=n_data)
 
     return data_validation
+
 
 # --------------------------------------------------------------------------
 
@@ -69,9 +72,10 @@ def test_scatter2D_alamo(alamo_surrogate, data_validation):
 
         # create and step into new temporary directory
         dname = tf.mkdtemp()
-        filename = os.path.join(dname, 'results.pdf')
-        surrogate_scatter2D(alamo_surrogate, data_validation,
-                            filename=filename, show=False)
+        filename = os.path.join(dname, "results.pdf")
+        surrogate_scatter2D(
+            alamo_surrogate, data_validation, filename=filename, show=False
+        )
 
         assert os.path.exists(filename)  # PDF results file
 
@@ -86,9 +90,10 @@ def test_scatter3D_alamo(alamo_surrogate, data_validation):
 
         # create and step into new temporary directory
         dname = tf.mkdtemp()
-        filename = os.path.join(dname, 'results.pdf')
-        surrogate_scatter3D(alamo_surrogate, data_validation,
-                            filename=filename, show=False)
+        filename = os.path.join(dname, "results.pdf")
+        surrogate_scatter3D(
+            alamo_surrogate, data_validation, filename=filename, show=False
+        )
 
         assert os.path.exists(filename)  # PDF results file
 
@@ -103,9 +108,10 @@ def test_parity_alamo(alamo_surrogate, data_validation):
 
         # create and step into new temporary directory
         dname = tf.mkdtemp()
-        filename = os.path.join(dname, 'results.pdf')
-        surrogate_parity(alamo_surrogate, data_validation,
-                         filename=filename, show=False)
+        filename = os.path.join(dname, "results.pdf")
+        surrogate_parity(
+            alamo_surrogate, data_validation, filename=filename, show=False
+        )
 
         assert os.path.exists(filename)  # PDF results file
 
@@ -120,9 +126,10 @@ def test_residual_alamo(alamo_surrogate, data_validation):
 
         # create and step into new temporary directory
         dname = tf.mkdtemp()
-        filename = os.path.join(dname, 'results.pdf')
-        surrogate_residual(alamo_surrogate, data_validation,
-                           filename=filename, show=False)
+        filename = os.path.join(dname, "results.pdf")
+        surrogate_residual(
+            alamo_surrogate, data_validation, filename=filename, show=False
+        )
 
         assert os.path.exists(filename)  # PDF results file
 
@@ -140,7 +147,7 @@ def test_scatter2D_noPDF_alamo(alamo_surrogate, data_validation):
         surrogate_scatter2D(alamo_surrogate, data_validation, show=False)
 
         for file in list(os.walk(dname)):  # check entire temp directory
-            assert file[-4:] != '.pdf'  # no PDF files should be created
+            assert file[-4:] != ".pdf"  # no PDF files should be created
 
 
 @pytest.mark.unit
@@ -156,7 +163,7 @@ def test_scatter3D_noPDF_alamo(alamo_surrogate, data_validation):
         surrogate_scatter3D(alamo_surrogate, data_validation, show=False)
 
         for file in list(os.walk(dname)):  # check entire temp directory
-            assert file[-4:] != '.pdf'  # no PDF files should be created
+            assert file[-4:] != ".pdf"  # no PDF files should be created
 
 
 @pytest.mark.unit
@@ -172,7 +179,7 @@ def test_parity_noPDF_alamo(alamo_surrogate, data_validation):
         surrogate_parity(alamo_surrogate, data_validation, show=False)
 
         for file in list(os.walk(dname)):  # check entire temp directory
-            assert file[-4:] != '.pdf'  # no PDF files should be created
+            assert file[-4:] != ".pdf"  # no PDF files should be created
 
 
 @pytest.mark.unit
@@ -188,4 +195,4 @@ def test_residual_noPDF_alamo(alamo_surrogate, data_validation):
         surrogate_residual(alamo_surrogate, data_validation, show=False)
 
         for file in list(os.walk(dname)):  # check entire temp directory
-            assert file[-4:] != '.pdf'  # no PDF files should be created
+            assert file[-4:] != ".pdf"  # no PDF files should be created
