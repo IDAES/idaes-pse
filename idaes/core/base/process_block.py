acc23a5614f9a6d2f615c4452422bc63502421cf
Andrew Lee <andrew.lee@netl.doe.gov>
2022-09-15 18:25:29 -0400

    Revert "Removing backwards compatibility for default argument (#959)"
    This reverts commit 297295e75384a5d43ade35d7eee744b819a84bab.

--- idaes/core/base/process_block.py
+++ idaes/core/base/process_block.py
@@ -90,13 +90,27 @@ def _process_kwargs(o, kwargs):
     o._block_data_config_initialize = ConfigBlock(implicit=True)
     o._block_data_config_initialize.set_value(kwargs.pop("initialize", None))
     o._idx_map = kwargs.pop("idx_map", None)
-
+    _default = kwargs.pop("default", None)
+    if _default is not None:
+        deprecation_warning(
+            "The default argument for the ProcessBlock class is deprecated. "
+            "Arguments can now be passed directly as keyword arguments."
+        )
+    _block_data_config_default = _default
     _pyomo_kwargs = {}
     for arg in _pyomo_block_keywords:
         if arg in kwargs:
             _pyomo_kwargs[arg] = kwargs.pop(arg)
-
-    o._block_data_config_default = kwargs
+    if kwargs:
+        # left over args for IDAES
+        if _block_data_config_default is None:
+            _block_data_config_default = kwargs
+        else:
+            raise RuntimeError(
+                "Do not supply both keyword arguments and the "
+                "'default' argument to ProcessBlock init. Default is deprecated."
+            )
+    o._block_data_config_default = _block_data_config_default
     return _pyomo_kwargs
 
 
