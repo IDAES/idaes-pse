ebf46ad91bb96597db0505966123d4406e7c4f44
Andrew Lee <andrew.lee@netl.doe.gov>
2023-01-31 09:14:47 -0500

    Update Pyomo tag with bug fix

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def rglob(path, glob):
 
 
 DEPENDENCIES_FOR_PRERELEASE_VERSION = [
-    "pyomo @ https://github.com/IDAES/pyomo/archive/6.4.5.idaes.2023.01.30.zip"
+    "pyomo @ https://github.com/IDAES/pyomo/archive/6.4.5.idaes.2023.01.31.zip"
 ]
 
 # For included DMF data
