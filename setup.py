7d56bb94cb286c6dcba51356b38dac91500337d4
Ludovico Bianchi <lbianchi@lbl.gov>
2023-02-27 19:02:20 -0600

    Format with Black

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ def rglob(path, glob):
     return list(map(str, p.rglob(glob)))
 
 
-DEPENDENCIES_FOR_PRERELEASE_VERSION = [
-]
+DEPENDENCIES_FOR_PRERELEASE_VERSION = []
 
 # For included DMF data
 DMF_DATA_ROOT = "data"
