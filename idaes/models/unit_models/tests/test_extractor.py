5b2287efaeade173f677e6f76f22c7e073f4d296
Andrew Lee <andrew.lee@netl.doe.gov>
2023-03-16 10:44:04 -0400

    Refactoring test for unit testing

--- idaes/models/unit_models/tests/test_extractor.py
+++ idaes/models/unit_models/tests/test_extractor.py
@@ -185,6 +185,49 @@ class StateBlock2Data(StateBlockData):
         }
 
 
+@declare_process_block_class("Parameters3")
+class Parameter3Data(PhysicalParameterBlock):
+    def build(self):
+        super().build()
+
+        self.phase1 = Phase()
+
+        self.solvent4 = Component()
+
+        self._state_block_class = StateBlock3
+
+    @classmethod
+    def define_metadata(cls, obj):
+        obj.add_default_units(
+            {
+                "time": units.s,
+                "length": units.m,
+                "mass": units.kg,
+                "amount": units.mol,
+                "temperature": units.K,
+            }
+        )
+
+
+class SBlock3Base(StateBlock):
+    def initialize(blk, **kwargs):
+        pass
+
+    def release_state(blk, **kwargs):
+        pass
+
+
+@declare_process_block_class("StateBlock3", block_class=SBlock1Base)
+class StateBlock3Data(StateBlockData):
+    CONFIG = ConfigBlock(implicit=True)
+
+    def build(self):
+        super().build()
+
+    def get_material_flow_basis(self):
+        return MaterialFlowBasis.mass
+
+
 # -----------------------------------------------------------------------------
 # Frame class for unit testing
 @declare_process_block_class("ECFrame")
@@ -195,53 +238,7 @@ class ECFrameData(ExtractorCascadeData):
 
 # -----------------------------------------------------------------------------
 class TestBuild:
-    @pytest.mark.unit
-    def test_verify_inputs_too_few_streams(self):
-        m = ConcreteModel()
-        m.fs = FlowsheetBlock(dynamic=False)
-
-        m.fs.properties1 = Parameters1()
-        m.fs.properties2 = Parameters2()
-
-        m.fs.unit = ECFrame(
-            number_of_stages=2,
-            streams={
-                "stream1": {"property_package": m.fs.properties1},
-            },
-        )
-
-        with pytest.raises(
-            ConfigurationError,
-            match="ExtractorCascade models must define at least two streams; received "
-            "\['stream1'\]",
-        ):
-            m.fs.unit._verify_inputs()
-
-    @pytest.mark.unit
-    def test_verify_inputs_dynamic(self):
-        m = ConcreteModel()
-        m.fs = FlowsheetBlock(dynamic=True, time_units=units.s)
-
-        m.fs.properties1 = Parameters1()
-        m.fs.properties2 = Parameters2()
-
-        m.fs.unit = ECFrame(
-            number_of_stages=2,
-            streams={
-                "stream1": {"property_package": m.fs.properties1},
-                "stream2": {"property_package": m.fs.properties1},
-            },
-        )
-
-        with pytest.raises(
-            NotImplementedError,
-            match="ExtractorCascade model does not support dynamics yet.",
-        ):
-            m.fs.unit._verify_inputs()
-
-
-class TestBuild2:
-    @pytest.fixture(scope="class")
+    @pytest.fixture
     def model(self):
         m = ConcreteModel()
         m.fs = FlowsheetBlock(dynamic=False)
@@ -249,7 +246,7 @@ class TestBuild2:
         m.fs.properties1 = Parameters1()
         m.fs.properties2 = Parameters2()
 
-        m.fs.unit = ExtractorCascade(
+        m.fs.unit = ECFrame(
             number_of_stages=2,
             streams={
                 "stream1": {"property_package": m.fs.properties1},
@@ -272,7 +269,75 @@ class TestBuild2:
         assert model.fs.unit.config.interacting_streams is None
 
     @pytest.mark.unit
-    def test_states(self, model):
+    def test_verify_inputs_too_few_streams(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=False)
+
+        m.fs.properties1 = Parameters1()
+
+        m.fs.unit = ECFrame(
+            number_of_stages=2,
+            streams={
+                "stream1": {"property_package": m.fs.properties1},
+            },
+        )
+
+        with pytest.raises(
+            ConfigurationError,
+            match="ExtractorCascade models must define at least two streams; received "
+            "\['stream1'\]",
+        ):
+            m.fs.unit._verify_inputs()
+
+    @pytest.mark.unit
+    def test_verify_inputs_dynamic(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=True, time_units=units.s)
+
+        m.fs.properties1 = Parameters1()
+
+        m.fs.unit = ECFrame(
+            number_of_stages=2,
+            streams={
+                "stream1": {"property_package": m.fs.properties1},
+                "stream2": {"property_package": m.fs.properties1},
+            },
+        )
+
+        with pytest.raises(
+            NotImplementedError,
+            match="ExtractorCascade model does not support dynamics yet.",
+        ):
+            m.fs.unit._verify_inputs()
+
+    @pytest.mark.unit
+    def test_verify_inputs_no_common_components(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=False)
+
+        m.fs.properties1 = Parameters1()
+        m.fs.properties3 = Parameters3()
+
+        m.fs.unit = ECFrame(
+            number_of_stages=2,
+            streams={
+                "stream1": {"property_package": m.fs.properties1},
+                "stream2": {"property_package": m.fs.properties3},
+            },
+        )
+
+        with pytest.raises(
+            ConfigurationError,
+            match="No common components found in property packages. Extractor "
+            "model assumes mass transfer occurs between components with the "
+            "same name in different streams.",
+        ):
+            m.fs.unit._verify_inputs()
+
+    @pytest.mark.unit
+    def test_verify_inputs_construct_components(self, model):
+        model.fs.unit._verify_inputs()
+
         assert isinstance(model.fs.unit.stages, RangeSet)
         assert len(model.fs.unit.stages) == 2
 
@@ -284,6 +349,14 @@ class TestBuild2:
                 ("stream1", "stream2", "solute1"),
                 ("stream1", "stream2", "solute2"),
             ]
+
+    @pytest.mark.unit
+    def test_build_state_blocks(self, model):
+        model.fs.unit._verify_inputs()
+        flow_basis, uom = model.fs.unit._build_state_blocks()
+
+        assert flow_basis == MaterialFlowBasis.molar
+        assert uom == model.fs.properties1.get_metadata().derived_units
 
         assert isinstance(model.fs.unit.stream1, StateBlock1)
         assert len(model.fs.unit.stream1) == 2
@@ -304,7 +377,39 @@ class TestBuild2:
         assert model.fs.unit.stream2_inlet_state[0].config.defined_state
 
     @pytest.mark.unit
+    def test_build_state_blocks_different_flow_basis(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=False)
+
+        m.fs.properties1 = Parameters1()
+        m.fs.properties3 = Parameters3()
+
+        m.fs.unit = ECFrame(
+            number_of_stages=2,
+            streams={
+                "stream1": {"property_package": m.fs.properties1},
+                # Properties3 has a different flow basis
+                "stream2": {"property_package": m.fs.properties3},
+            },
+        )
+
+        m.fs.unit.stages = Set(initialize=[1, 2])
+
+        with pytest.raises(
+            ConfigurationError,
+            match="Property packages use different flow bases: ExtractionCascade "
+            "requires all property packages to use the same basis. stream2 uses "
+            "MaterialFlowBasis.mass, whilst first stream uses "
+            "MaterialFlowBasis.molar.",
+        ):
+            m.fs.unit._build_state_blocks()
+
+    @pytest.mark.unit
     def test_material_balances(self, model):
+        model.fs.unit._verify_inputs()
+        flow_basis, uom = model.fs.unit._build_state_blocks()
+        model.fs.unit._build_material_balance_constraints(flow_basis, uom)
+
         assert isinstance(model.fs.unit.material_transfer_term, Var)
         # One stream pair with two common components over two stages and 1 time point
         assert len(model.fs.unit.material_transfer_term) == 4
@@ -371,6 +476,9 @@ class TestBuild2:
 
     @pytest.mark.unit
     def test_energy_balances(self, model):
+        model.fs.unit._verify_inputs()
+        _, uom = model.fs.unit._build_state_blocks()
+        model.fs.unit._build_energy_balance_constraints(uom)
         # TODO: Check transfer terms
 
         assert isinstance(model.fs.unit.stream1_energy_balance, Constraint)
@@ -417,6 +525,9 @@ class TestBuild2:
 
     @pytest.mark.unit
     def test_pressure_balances(self, model):
+        model.fs.unit._verify_inputs()
+        _, uom = model.fs.unit._build_state_blocks()
+        model.fs.unit._build_pressure_balance_constraints(uom)
         # TODO: Check deltaP terms
 
         assert isinstance(model.fs.unit.stream1_pressure_balance, Constraint)
@@ -463,6 +574,10 @@ class TestBuild2:
 
     @pytest.mark.unit
     def test_ports(self, model):
+        model.fs.unit._verify_inputs()
+        model.fs.unit._build_state_blocks()
+        model.fs.unit._build_ports()
+
         assert isinstance(model.fs.unit.stream1_inlet, Port)
         for p, j in model.fs.unit.stream1.phase_component_set:
             assert (
@@ -507,6 +622,29 @@ class TestBuild2:
             is model.fs.unit.stream2[0, 1].enth_flow
         )
 
+
+class TestToyProblem:
+    @pytest.fixture(scope="class")
+    def model(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=False)
+
+        m.fs.properties1 = Parameters1()
+        m.fs.properties2 = Parameters2()
+
+        m.fs.unit = ExtractorCascade(
+            number_of_stages=2,
+            streams={
+                "stream1": {"property_package": m.fs.properties1},
+                "stream2": {
+                    "property_package": m.fs.properties2,
+                    "flow_direction": FlowDirection.backward,
+                },
+            },
+        )
+
+        return m
+
     @pytest.mark.unit
     def test_degrees_of_freedom(self, model):
         # Expect 15 DoF: 6 stream1 inlets, 5 stream2 inlets and 2x2 mass transfer terms
@@ -516,92 +654,76 @@ class TestBuild2:
     def test_unit_consistency(self, model):
         assert_units_consistent(model)
 
+    @pytest.mark.component
+    def test_toy_problem(self, model):
+        model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solvent1"].fix(2)
+        model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute1"].fix(3)
+        model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute2"].fix(4)
+        model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute3"].fix(5)
+        model.fs.unit.stream1_inlet.enth_flow[0].fix(5000)
+        model.fs.unit.stream1_inlet.pressure[0].fix(1.2e5)
 
-@pytest.mark.component
-def test_toy_problem():
-    model = ConcreteModel()
-    model.fs = FlowsheetBlock(dynamic=False)
+        model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solvent2"].fix(11)
+        model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solute1"].fix(12)
+        model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solute2"].fix(13)
+        model.fs.unit.stream2_inlet.enth_flow[0].fix(7000)
+        model.fs.unit.stream2_inlet.pressure[0].fix(2e5)
 
-    model.fs.properties1 = Parameters1()
-    model.fs.properties2 = Parameters2()
+        model.fs.unit.material_transfer_term[0, :, "stream1", "stream2", "solute1"].fix(
+            0.5
+        )
+        model.fs.unit.material_transfer_term[0, :, "stream1", "stream2", "solute2"].fix(
+            -0.5
+        )
 
-    model.fs.unit = ExtractorCascade(
-        number_of_stages=2,
-        streams={
-            "stream1": {"property_package": model.fs.properties1},
-            "stream2": {
-                "property_package": model.fs.properties2,
-                "flow_direction": FlowDirection.backward,
-            },
-        },
-    )
+        assert (degrees_of_freedom(model)) == 0
 
-    model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solvent1"].fix(2)
-    model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute1"].fix(3)
-    model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute2"].fix(4)
-    model.fs.unit.stream1_inlet.flow_mol_phase_comp[0, "phase1", "solute3"].fix(5)
-    model.fs.unit.stream1_inlet.enth_flow[0].fix(5000)
-    model.fs.unit.stream1_inlet.pressure[0].fix(1.2e5)
+        solver = get_solver()
+        results = solver.solve(model)
 
-    model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solvent2"].fix(11)
-    model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solute1"].fix(12)
-    model.fs.unit.stream2_inlet.flow_mol_phase_comp[0, "phase1", "solute2"].fix(13)
-    model.fs.unit.stream2_inlet.enth_flow[0].fix(7000)
-    model.fs.unit.stream2_inlet.pressure[0].fix(2e5)
+        assert_optimal_termination(results)
 
-    model.fs.unit.material_transfer_term[0, :, "stream1", "stream2", "solute1"].fix(0.5)
-    model.fs.unit.material_transfer_term[0, :, "stream1", "stream2", "solute2"].fix(
-        -0.5
-    )
+        assert value(
+            model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solvent1"]
+        ) == pytest.approx(2, rel=1e-5)
+        assert value(
+            model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute1"]
+        ) == pytest.approx(
+            4, rel=1e-5
+        )  # 3 + 0.5 + 0.5
+        assert value(
+            model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute2"]
+        ) == pytest.approx(
+            3, rel=1e-5
+        )  # 3 - 0.5 - 0.5
+        assert value(
+            model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute3"]
+        ) == pytest.approx(5, rel=1e-5)
 
-    assert (degrees_of_freedom(model)) == 0
+        assert value(
+            model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solvent2"]
+        ) == pytest.approx(11, rel=1e-5)
+        assert value(
+            model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solute1"]
+        ) == pytest.approx(
+            11, rel=1e-5
+        )  # 12 - 0.5 - 0.5
+        assert value(
+            model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solute2"]
+        ) == pytest.approx(
+            14, rel=1e-5
+        )  # 13 + 0.5 + 0.5
 
-    solver = get_solver()
-    results = solver.solve(model)
+        assert value(model.fs.unit.stream1_outlet.enth_flow[0]) == pytest.approx(
+            5000, rel=1e-5
+        )
+        assert value(model.fs.unit.stream2_outlet.enth_flow[0]) == pytest.approx(
+            7000, rel=1e-5
+        )
 
-    assert_optimal_termination(results)
-
-    assert value(
-        model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solvent1"]
-    ) == pytest.approx(2, rel=1e-5)
-    assert value(
-        model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute1"]
-    ) == pytest.approx(
-        4, rel=1e-5
-    )  # 3 + 0.5 + 0.5
-    assert value(
-        model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute2"]
-    ) == pytest.approx(
-        3, rel=1e-5
-    )  # 3 - 0.5 - 0.5
-    assert value(
-        model.fs.unit.stream1_outlet.flow_mol_phase_comp[0, "phase1", "solute3"]
-    ) == pytest.approx(5, rel=1e-5)
-
-    assert value(
-        model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solvent2"]
-    ) == pytest.approx(11, rel=1e-5)
-    assert value(
-        model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solute1"]
-    ) == pytest.approx(
-        11, rel=1e-5
-    )  # 12 - 0.5 - 0.5
-    assert value(
-        model.fs.unit.stream2_outlet.flow_mol_phase_comp[0, "phase1", "solute2"]
-    ) == pytest.approx(
-        14, rel=1e-5
-    )  # 13 + 0.5 + 0.5
-
-    assert value(model.fs.unit.stream1_outlet.enth_flow[0]) == pytest.approx(
-        5000, rel=1e-5
-    )
-    assert value(model.fs.unit.stream2_outlet.enth_flow[0]) == pytest.approx(
-        7000, rel=1e-5
-    )
-
-    assert value(model.fs.unit.stream1_outlet.pressure[0]) == pytest.approx(
-        1.2e5, rel=1e-5
-    )
-    assert value(model.fs.unit.stream2_outlet.pressure[0]) == pytest.approx(
-        2e5, rel=1e-5
-    )
+        assert value(model.fs.unit.stream1_outlet.pressure[0]) == pytest.approx(
+            1.2e5, rel=1e-5
+        )
+        assert value(model.fs.unit.stream2_outlet.pressure[0]) == pytest.approx(
+            2e5, rel=1e-5
+        )
