735a429e095ea856be86b2dee74bdb2a8b23e997
John Eslick <john.eslick@netl.doe.gov>
2023-03-31 09:12:58 -0500

    Update biniary version

--- idaes/config.py
+++ idaes/config.py
@@ -22,7 +22,7 @@ import pyomo.common.config
 
 _log = logging.getLogger(__name__)
 # Default release version if no options provided for get-extensions
-default_binary_release = "3.2.0"
+default_binary_release = "3.3.0"
 # Where to download releases from get-extensions
 release_base_url = "https://github.com/IDAES/idaes-ext/releases/download"
 # Where to get release checksums
