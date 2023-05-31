05896b4f823fbc03ef75a949319bf6b174eb8046
John Eslick <john.eslick@netl.doe.gov>
2023-05-31 17:07:26 -0500

    Surface tension fix for over critical (#1192)
    * Surface tension fix for over ciritical
    
    * Remove unused varaible

--- idaes/models/properties/general_helmholtz/expressions/surface_tension_type01.py
+++ idaes/models/properties/general_helmholtz/expressions/surface_tension_type01.py
@@ -15,6 +15,8 @@
 
 __author__ = "John Eslick"
 
+from idaes.core.util.math import smooth_max
+
 
 def surface_tension_type01(model, parameters):
     """Type01 expression for the surface tension
@@ -29,4 +31,9 @@ def surface_tension_type01(model, parameters):
     s = parameters["transport"]["surface_tension"]["s"]
     n = parameters["transport"]["surface_tension"]["n"]
     tc = parameters["transport"]["surface_tension"]["Tc"]
-    return sum(s[i] * (1 - model.T_star / model.tau / tc) ** n[i] for i in s)
+    eps = 1e-4
+    # Use smooth max to avoid evaluation errors on negative numbers
+    # this means the surface tension will go to zero at and above the
+    # critical temperature, rather than cause error over critical.
+    omTr = smooth_max(1 - model.T_star / model.tau / tc, 0, eps=eps)
+    return sum(s[i] * omTr ** n[i] for i in s)
