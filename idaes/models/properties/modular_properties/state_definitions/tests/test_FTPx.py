4bbc68ccae96e5f4129a041c0124c26916bf007c
Radhakrishna <radhakrishnatg@gmail.com>
2023-06-15 15:53:06 -0400

    Formatted files with black

--- idaes/models/properties/modular_properties/state_definitions/tests/test_FTPx.py
+++ idaes/models/properties/modular_properties/state_definitions/tests/test_FTPx.py
@@ -1390,19 +1390,29 @@ class TestCommon(object):
 
         # Test get_material_flow_terms method
         for p, j in blk.params._phase_component_set:
-            assert blk.props[1].get_material_flow_terms(p, j).parent_block() is blk.props[1]
+            assert (
+                blk.props[1].get_material_flow_terms(p, j).parent_block()
+                is blk.props[1]
+            )
 
         # Test get_enthalpy_flow_terms method
         for p in blk.params.phase_list:
-            assert blk.props[1].get_enthalpy_flow_terms(p).parent_block() is blk.props[1]
+            assert (
+                blk.props[1].get_enthalpy_flow_terms(p).parent_block() is blk.props[1]
+            )
 
         # Test get_material_density_terms
         for p, j in blk.params._phase_component_set:
-            assert blk.props[1].get_material_density_terms(p, j).parent_block() is blk.props[1]
+            assert (
+                blk.props[1].get_material_density_terms(p, j).parent_block()
+                is blk.props[1]
+            )
 
         # Test get_energy_Density_terms
         for p in blk.params.phase_list:
-            assert blk.props[1].get_energy_density_terms(p).parent_block() is blk.props[1]
+            assert (
+                blk.props[1].get_energy_density_terms(p).parent_block() is blk.props[1]
+            )
 
         for i in blk.props[1].define_state_vars().values():
             assert i.parent_block() is blk.props[1]
