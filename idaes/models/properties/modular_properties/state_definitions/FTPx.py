d755d34e2ebc6b0c88873de372f83e4390cbe4bd
radhakrishnatg <42144353+radhakrishnatg@users.noreply.github.com>
2023-06-16 12:38:07 -0400

    Fix bug related to cloning modular properties methods (#1209)
    * Fixed bug and added a test for cloning
    
    * Formatted files with black
    
    ---------
    
    Co-authored-by: Andrew Lee <andrew.lee@netl.doe.gov>
    (cherry picked from commit 601319d7e973e02e3a941b3fc857cc52ebcf253b)

--- idaes/models/properties/modular_properties/state_definitions/FTPx.py
+++ idaes/models/properties/modular_properties/state_definitions/FTPx.py
@@ -20,6 +20,7 @@ package
 # TODO: Look into protected access issues
 # pylint: disable=protected-access
 
+from types import MethodType
 from pyomo.environ import (
     Constraint,
     Expression,
@@ -264,13 +265,13 @@ def define_state(b):
 
     # -------------------------------------------------------------------------
     # General Methods
-    def get_material_flow_terms_FTPx(p, j):
+    def get_material_flow_terms_FTPx(b, p, j):
         """Create material flow terms for control volume."""
         return b.flow_mol_phase_comp[p, j]
 
-    b.get_material_flow_terms = get_material_flow_terms_FTPx
+    b.get_material_flow_terms = MethodType(get_material_flow_terms_FTPx, b)
 
-    def get_enthalpy_flow_terms_FTPx(p):
+    def get_enthalpy_flow_terms_FTPx(b, p):
         """Create enthalpy flow terms."""
         # enth_mol_phase probably does not exist when this is created
         # Use try/except to build flow term if not present
@@ -284,9 +285,9 @@ def define_state(b):
             eflow = b._enthalpy_flow_term = Expression(b.phase_list, rule=rule_eflow)
         return eflow[p]
 
-    b.get_enthalpy_flow_terms = get_enthalpy_flow_terms_FTPx
+    b.get_enthalpy_flow_terms = MethodType(get_enthalpy_flow_terms_FTPx, b)
 
-    def get_material_density_terms_FTPx(p, j):
+    def get_material_density_terms_FTPx(b, p, j):
         """Create material density terms."""
         # dens_mol_phase probably does not exist when this is created
         # Use try/except to build term if not present
@@ -302,9 +303,9 @@ def define_state(b):
             )
         return mdens[p, j]
 
-    b.get_material_density_terms = get_material_density_terms_FTPx
+    b.get_material_density_terms = MethodType(get_material_density_terms_FTPx, b)
 
-    def get_energy_density_terms_FTPx(p):
+    def get_energy_density_terms_FTPx(b, p):
         """Create energy density terms."""
         # Density and energy terms probably do not exist when this is created
         # Use try/except to build term if not present
@@ -318,7 +319,7 @@ def define_state(b):
             edens = b._energy_density_term = Expression(b.phase_list, rule=rule_edens)
         return edens[p]
 
-    b.get_energy_density_terms = get_energy_density_terms_FTPx
+    b.get_energy_density_terms = MethodType(get_energy_density_terms_FTPx, b)
 
     def default_material_balance_type_FTPx():
         return MaterialBalanceType.componentTotal
@@ -335,7 +336,7 @@ def define_state(b):
 
     b.get_material_flow_basis = get_material_flow_basis_FTPx
 
-    def define_state_vars_FTPx():
+    def define_state_vars_FTPx(b):
         """Define state vars."""
         return {
             "flow_mol": b.flow_mol,
@@ -344,9 +345,9 @@ def define_state(b):
             "pressure": b.pressure,
         }
 
-    b.define_state_vars = define_state_vars_FTPx
+    b.define_state_vars = MethodType(define_state_vars_FTPx, b)
 
-    def define_display_vars_FTPx():
+    def define_display_vars_FTPx(b):
         """Define display vars."""
         return {
             "Total Molar Flowrate": b.flow_mol,
@@ -355,7 +356,7 @@ def define_state(b):
             "Pressure": b.pressure,
         }
 
-    b.define_display_vars = define_display_vars_FTPx
+    b.define_display_vars = MethodType(define_display_vars_FTPx, b)
 
 
 def state_initialization(b):
