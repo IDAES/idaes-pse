28aed850178c940cd861c362c7e39866a39766ea
Liu <xliu3@nrel.gov>
2023-06-16 09:10:51 -0600

    Reverse changes for lower bounds in state_definition

--- idaes/models_extra/column_models/tests/test_plate_heat_exchanger.py
+++ idaes/models_extra/column_models/tests/test_plate_heat_exchanger.py
@@ -113,6 +113,7 @@ class TestPHE(object):
     @pytest.mark.build
     @pytest.mark.unit
     def test_build(self, phe):
+
         assert hasattr(phe.fs.unit, "hot_side_inlet")
         assert len(phe.fs.unit.hot_side_inlet.vars) == 4
         assert hasattr(phe.fs.unit.hot_side_inlet, "flow_mol")
@@ -162,8 +163,7 @@ class TestPHE(object):
     @pytest.mark.component
     def test_initialize(self, phe):
         initialization_tester(
-            phe,
-            duty=(245000, pyunits.W),  # , optarg={"bound_push": 1e-8, "mu_init": 1e-8}
+            phe, duty=(245000, pyunits.W), optarg={"bound_push": 1e-8, "mu_init": 1e-8}
         )
 
     @pytest.mark.solver
