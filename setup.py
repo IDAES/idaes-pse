e106e2e2bec33715576e24561cf83f5c6aafbc54
Bethany Nicholson <blnicho@users.noreply.github.com>
2023-12-07 13:18:24 -0500

    Update the Pyomo tag to the 6.7.0 release (#1292)
    * Bumping Pyomo tag to check IDAES tests in preparation for Pyomo 6.7.0
    
    * Require Pyomo 6.7.0
    
    ---------
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ kwargs = dict(
     # Put abstract (non-versioned) deps here.
     # Concrete dependencies go in requirements[-dev].txt
     install_requires=[
-        "pyomo @ git+https://github.com/IDAES/Pyomo@6.7.0.idaes.2023.11.6",
+        "pyomo >= 6.7.0",
         "pint",  # required to use Pyomo units
         "networkx",  # required to use Pyomo network
         "numpy",
