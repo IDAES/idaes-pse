cc3da96c87e7b569e2087125e95cc9f8ff932d22
Bethany Nicholson <blnicho@users.noreply.github.com>
2024-02-21 13:44:39 -0700

    Update pyomo requirement to 6.7.1

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ kwargs = dict(
     # Put abstract (non-versioned) deps here.
     # Concrete dependencies go in requirements[-dev].txt
     install_requires=[
-        "pyomo @ git+https://github.com/IDAES/Pyomo@6.7.1.idaes.2024.02.21",
+        "pyomo >= 6.7.1",
         "pint",  # required to use Pyomo units
         "networkx",  # required to use Pyomo network
         "numpy",
