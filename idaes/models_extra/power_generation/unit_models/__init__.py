1c03d2310ce139cda9dcaa17120cc3cad1a11338
Doug A <douglas.allan@keylogic.com>
2024-03-19 16:07:50 -0400

    Moving towards heater testing

--- idaes/models_extra/power_generation/unit_models/__init__.py
+++ idaes/models_extra/power_generation/unit_models/__init__.py
@@ -15,12 +15,13 @@ from .balance import BalanceBlockData, BalanceBlock
 from .boiler_fireside import BoilerFireside
 from .boiler_heat_exchanger import BoilerHeatExchanger
 from .boiler_heat_exchanger_2D import HeatExchangerCrossFlow2D_Header
+from .cross_flow_heat_exchanger_1D import CrossFlowHeatExchanger1D
 from .downcomer import Downcomer
 from .drum import Drum
 from .drum1D import Drum1D
 from .feedwater_heater_0D_dynamic import FWH0DDynamic
+from .heater_1D import Heater1D
 from .heat_exchanger_3streams import HeatExchangerWith3Streams
-from .cross_flow_heat_exchanger_1D import CrossFlowHeatExchanger1D
 from .steamheater import SteamHeater
 from .waterpipe import WaterPipe
 from .watertank import WaterTank
