b24616b79cfa88084d18ac75d32cf546623c5162
Bethany Nicholson <blnicho@sandia.gov>
2024-05-08 11:18:46 -0600

    Bumping Pyomo tag

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ kwargs = dict(
     # Put abstract (non-versioned) deps here.
     # Concrete dependencies go in requirements[-dev].txt
     install_requires=[
-        "pyomo @ https://github.com/IDAES/pyomo/archive/6.7.2.idaes.2024.05.07.zip",
+        "pyomo @ https://github.com/IDAES/pyomo/archive/6.7.2.idaes.2024.05.08.zip",
         "pint",  # required to use Pyomo units
         "networkx",  # required to use Pyomo network
         "numpy<2",
