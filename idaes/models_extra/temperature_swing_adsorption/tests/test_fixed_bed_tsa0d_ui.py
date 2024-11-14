8ab63fbff6dcc1cfc2313aa3864dd6265d7800c6
Sheng Pang <shengdevelop@gmail.com>
2024-11-14 12:48:27 -0800

    update test set test for default build params

--- idaes/models_extra/temperature_swing_adsorption/tests/test_fixed_bed_tsa0d_ui.py
+++ idaes/models_extra/temperature_swing_adsorption/tests/test_fixed_bed_tsa0d_ui.py
@@ -1,6 +1,14 @@
 import pytest
+import logging
 from pyomo.environ import check_optimal_termination
-from ..fixed_bed_tsa0d_ui import export_to_ui, build
+from idaes.models_extra.temperature_swing_adsorption.fixed_bed_tsa0d_ui import (
+    export_to_ui,
+    build,
+    initialize,
+    solve,
+)
+
+_log = logging.getLogger(__name__)
 
 
 @pytest.mark.component
@@ -15,3 +23,22 @@ def test_solve_base():
     ui.build(build_options=ui.fs_exp.build_options)
     r = ui.solve()
     assert check_optimal_termination(r)
+
+
+@pytest.mark.component
+def test_default_build_options():
+    """test default build options, option values from jupyter notebook example"""
+    default_build_options = {
+        "adsorbent": "zeolite_13x",
+        "number_of_beds": 1,
+        "transformation_method": "dae.collocation",
+        "transformation_scheme": "lagrangeRadau",
+        "finite_elements": 20,
+        "collocation_points": 6,
+    }
+    ui = export_to_ui()
+    ui.build(build_options=ui.fs_exp.build_options)
+
+    for key, val in ui.fs_exp.build_options.items():
+        assert default_build_options[key] == val.value
+    assert True
