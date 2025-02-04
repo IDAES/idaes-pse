06bd145b78dabab650754ef75445e10afe4701b8
radhakrishnatg <radhakrishnatg@gmail.com>
2025-02-04 08:13:20 -0500

    Updated clustering methods

--- idaes/apps/grid_integration/multiperiod/clustering.py
+++ idaes/apps/grid_integration/multiperiod/clustering.py
@@ -15,20 +15,18 @@
 Contains functions for clustering the price signal into representative days/periods.
 """
 
+from typing import Union
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import idaes.logger as idaeslog
 from pyomo.common.dependencies import attempt_import
+import idaes.logger as idaeslog
 
 sklearn, sklearn_avail = attempt_import("sklearn")
-kneed, kneed_avail = attempt_import("kneed")
 
 if sklearn_avail:
     from sklearn.cluster import KMeans
-
-if kneed_avail:
-    from kneed import KneeLocator
+    from sklearn.metrics import silhouette_score
 
 _logger = idaeslog.getLogger(__name__)
 
@@ -39,10 +37,15 @@ def generate_daily_data(raw_data: list, horizon_length: int):
     from the raw data provided.
 
     Args:
-        raw_data:   Columnar data for a given LMP signal
+        raw_data : list,
+            Columnar data for a given LMP signal
+
+        horizon_length : int,
+            Length of each representative day/period
 
     Returns:
-        daily_data: Correctly formatted daily LMP data for later use
+        daily_data: pd.DataFrame,
+            Correctly formatted daily LMP data for later use
     """
     if horizon_length > len(raw_data):
         raise ValueError(
@@ -56,26 +59,18 @@ def generate_daily_data(raw_data: list, horizon_length: int):
             f"\tIgnoring the last {elements_ignored} elements in the price signal."
         )
 
-    day_list = list(range(1, (len(raw_data) // horizon_length) + 1))
+    daily_data = {
+        j: raw_data[((j - 1) * horizon_length) : (j * horizon_length)]
+        for j in range(1, (len(raw_data) // horizon_length) + 1)
+    }
 
-    daily_data = pd.DataFrame(columns=day_list)
-
-    # Extracting data to populate empty dataframe
-    i = 0
-    j = horizon_length
-    day = 1
-
-    while j <= len(raw_data):
-        daily_data[day] = raw_data[i:j].reset_index(drop=True)
-        i = j
-        j = j + horizon_length
-        day = day + 1
-
-    return daily_data
+    # DataFrame arranges the data for each day as a column. Since most clustering techniques
+    # require different samples as rows, we take the transpose before returning the data.
+    return pd.DataFrame(daily_data).transpose()
 
 
 def cluster_lmp_data(
-    raw_data: list, n_clusters: int, horizon_length: int, seed: int = 42
+    raw_data: list, horizon_length: int, n_clusters: int, seed: int = 42
 ):
     """
     Clusters the given price signal into n_clusters using the k-means clustering
@@ -85,11 +80,11 @@ def cluster_lmp_data(
         raw_data: list,
             Columnar data for a given LMP signal.
 
-        n_clusters: int,
-            Number of clusters desired for the data (representative days).
-
         horizon_length: int,
             Length of each cluster (representative day/period)
+
+        n_clusters: int,
+            Number of clusters desired for the data (representative days).
 
         seed: int,
             Seed value for initializing random number generator within Kmeans
@@ -113,7 +108,7 @@ def cluster_lmp_data(
     daily_data = generate_daily_data(raw_data, horizon_length)
 
     # KMeans clustering with the optimal number of clusters
-    kmeans = KMeans(n_clusters=n_clusters).fit(daily_data.transpose())
+    kmeans = KMeans(n_clusters=n_clusters, random_state=seed).fit(daily_data)
     centroids = kmeans.cluster_centers_
     labels = kmeans.labels_
 
@@ -131,29 +126,11 @@ def cluster_lmp_data(
     return lmp_data_clusters, weights
 
 
-def _compute_sse(data, centroids, idx):
-    """
-    Function used to compute the inertia (sum of square errors) for k clusters.
-
-    Args:
-        data:      Columnar data for a given LMP signal
-        centroids: Array of k centroids
-        idx:       Index for data
-
-    Returns:
-        inertia: Sum of square errors for k clusters
-    """
-    inertia = 0
-    for i, centroid in enumerate(centroids):
-        cluster_points = data[idx == i]
-        inertia += np.sum((cluster_points - centroid) ** 2)
-    return inertia
-
-
 def get_optimal_num_clusters(
-    daily_data,
-    kmin: int = 4,
+    samples: Union[pd.DataFrame, np.array],
+    kmin: int = 1,
     kmax: int = 30,
+    method: str = "elbow",
     generate_elbow_plot: bool = False,
     seed: int = 42,
 ):
@@ -162,43 +139,70 @@ def get_optimal_num_clusters(
     given price signal.
 
     Args:
-        daily_data: LMP signal grouped by days (output of generate_daily_data function)
-        kmin:       minimum number of clusters
-        kmax:       maximum number of clusters
+        samples: pd.DataFrame | np.array,
+            Set of points with rows containing samples and columns
+            containing features
+
+        kmin : int,
+            Minimum number of clusters
+
+        kmax : int,
+            Maximum number of clusters
+
+        generate_elbow_plot : bool,
+            If True, generates an elbow plot for inertia as a function of
+            number of clusters
+
+        seed : int,
+            Seed value for random number generator
 
     Returns:
-        n_clusters:     the optimal number of clusters for the given data
-        inertia_values: within-cluster sum-of-squares
+        n_clusters: int,
+            The optimal number of clusters for the given data
     """
     if kmin >= kmax:
         raise ValueError(f"kmin must be less than kmax, but {kmin} >= {kmax}")
 
+    # For silhouette method, kmin must be 2. So, we require kmin >= 2 in general
+    if kmin <= 1:
+        raise ValueError(f"kmin must be > 1. Received kmin = {kmin}")
+
+    if kmax >= len(samples):
+        raise ValueError(f"kmax must be < len(samples). Received kmax = {kmax}")
+
     k_values = list(range(kmin, kmax + 1))
     inertia_values = []
-
-    np.random.seed(seed)
+    mean_silhouette = []
 
     for k in k_values:
-        kmeans = KMeans(n_clusters=k).fit(daily_data.transpose())
+        kmeans = KMeans(n_clusters=k, random_state=seed).fit(samples)
         inertia_values.append(kmeans.inertia_)
 
-    # Identify the "elbow point"
-    elbow_point = KneeLocator(
-        k_values, inertia_values, curve="convex", direction="decreasing"
-    )
-    n_clusters = elbow_point.knee
+        if method == "silhouette":
+            # Calculate the average silhouette score, if the chosen method
+            # is silhouette
+            mean_silhouette.append(silhouette_score(samples, kmeans.labels_))
 
-    if n_clusters is None:
+    # Identify the optimal number of clusters
+    if method == "elbow":
+        n_clusters = locate_elbow(k_values, inertia_values)
+
+    elif method == "silhouette":
+        max_index = mean_silhouette.index(max(mean_silhouette))
+        n_clusters = k_values[max_index]
+
+    else:
         raise ValueError(
-            "Could not find elbow point for the given kmin, kmax. "
-            "Consider increasing the range of kmin, kmax."
+            f"Unrecognized method {method} for optimal number of clusters."
+            f"\tSupported methods include elbow and silhouette."
         )
 
     _logger.info(f"Optimal number of clusters is: {n_clusters}")
 
-    if int(n_clusters) + 2 >= kmax:
+    if n_clusters + 2 >= kmax:
         _logger.warning(
-            f"Optimal number of clusters is close to kmax: {kmax}. Consider increasing kmax."
+            f"Optimal number of clusters is close to kmax: {kmax}. "
+            f"Consider increasing kmax."
         )
 
     if generate_elbow_plot:
@@ -208,6 +212,29 @@ def get_optimal_num_clusters(
         plt.ylabel("Inertia")
         plt.title("Elbow Method")
         plt.xlim(kmin, kmax)
-        plt.grid()
+        plt.show()
 
-    return int(n_clusters), inertia_values
+    return n_clusters
+
+
+def locate_elbow(x: list, y: list):
+    """
+    Identifies the elbow/knee for the input/output data
+
+    Args:
+        x : list
+            List of independent variables
+        y : list
+            List of dependent variables
+
+    Returns:
+        opt_x : float
+            Optimal x at which curvature changes significantly
+    """
+    # The implementation is based on
+    # Ville Satopaa, Jeannie Albrecht, David Irwin, Barath Raghavan
+    # Finding a “Kneedle” in a Haystack:
+    # Detecting Knee Points in System Behavior
+    # https://raghavan.usc.edu/papers/kneedle-simplex11.pdf
+
+    return len(np.array(x)) + len(np.array(y))
--- idaes/apps/grid_integration/multiperiod/tests/test_clustering.py
+++ idaes/apps/grid_integration/multiperiod/tests/test_clustering.py
@@ -11,167 +11,265 @@
 # for full copyright and license information.
 #################################################################################
 
-import pytest
 from pathlib import Path
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import pytest
 
 from idaes.apps.grid_integration.multiperiod.clustering import (
     generate_daily_data,
     get_optimal_num_clusters,
     cluster_lmp_data,
     sklearn_avail,
-    kneed_avail,
+    locate_elbow,
 )
 import idaes.logger as idaeslog
 
 pytest.importorskip("sklearn", reason="sklearn not available")
-pytest.importorskip("kneed", reason="kneed not available")
 
 
-@pytest.fixture
-def sample_data():
-    DATA_DIR = Path(__file__).parent
-    file_path = DATA_DIR / "FLECCS_princeton.csv"
+@pytest.fixture(name="sample_data")
+def sample_data_fixture():
+    """Returns a sample price signal for testing"""
+    file_path = Path(__file__).parent / "FLECCS_princeton.csv"
     data = pd.read_csv(file_path)
     return data
 
 
-@pytest.mark.unit
-def test_daily_data_size(sample_data):
-    # Generate price data for each hour of every day in the data
-    daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-
-    # Check that there is a row for each horizon length in a representative day (24 x 365)
-    assert len(daily_data) == 24
-    assert len(daily_data.transpose()) == 365
+@pytest.fixture(name="dummy_data")
+def dummy_data_fixture():
+    """Returns dummy data for clustering"""
+    # Test Data Set: [0,0], [0,1], [1,1], [1, 0], [10, 0], [10, 1],
+    # [11, 1], [11, 0], [5,10], [5, 11], [6, 11], [6, 10]
+    # Centroids for this set must be [0.5, 0.5], [10.5, 0.5], [5.5, 10.5]
+    data = [
+        0,
+        0,
+        0,
+        1,
+        1,
+        1,
+        1,
+        0,
+        10,
+        0,
+        10,
+        1,
+        11,
+        1,
+        11,
+        0,
+        5,
+        10,
+        5,
+        11,
+        6,
+        11,
+        6,
+        10,
+    ]
+    return data
 
 
 @pytest.mark.unit
 def test_daily_data_logger_message1():
-    horizon_length = 24
-    raw_data = [1, 2, 3, 4]
+    """Tests ValueError in generate_daily_data function"""
     with pytest.raises(
         ValueError,
-        match=f"Horizon length {horizon_length} exceeds the price signal length of {len(raw_data)}",
+        match="Horizon length 24 exceeds the price signal length of 4",
     ):
-        generate_daily_data(raw_data, horizon_length=horizon_length)
+        generate_daily_data(raw_data=[1, 2, 3, 4], horizon_length=24)
 
 
 @pytest.mark.unit
-def test_daily_data_logger_message2(sample_data, caplog):
-    horizon_length = 23
+def test_daily_data_logger_message2(caplog):
+    """Tests non-integer-multiple length warning in generate_daily_data function"""
+
     with caplog.at_level(idaeslog.WARNING):
-        generate_daily_data(sample_data["BaseCaseTax"], horizon_length=horizon_length)
-        elements_ignored = len(sample_data) % horizon_length
+        daily_data = generate_daily_data(raw_data=list(range(8)), horizon_length=3)
         assert (
-            f"Length of the price signal is not an integer multiple of horizon_length.\n"
-            f"\tIgnoring the last {elements_ignored} elements in the price signal."
+            "Length of the price signal is not an integer multiple of horizon_length.\n"
+            "\tIgnoring the last 2 elements in the price signal."
         ) in caplog.text
 
-
-@pytest.mark.unit
-@pytest.mark.skipif(
-    not sklearn_avail, reason="sklearn (optional dependency) not available"
-)
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-def test_determine_optimal_num_clusters(sample_data):
-
-    daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-    n_clusters, inertia_values = get_optimal_num_clusters(daily_data, kmax=30, seed=20)
-
-    assert n_clusters == 11
-
-
-@pytest.mark.skipif(
-    not sklearn_avail, reason="sklearn (optional dependency) not available"
-)
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-@pytest.mark.unit
-def test_optimal_clusters_logger_message1(sample_data):
-    kmax = 10
-    kmin = 15
-    with pytest.raises(
-        ValueError,
-        match=f"kmin must be less than kmax, but {kmin} >= {kmax}",
-    ):
-
-        daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-        get_optimal_num_clusters(daily_data, kmin=kmin, kmax=kmax, seed=20)
-
-
-@pytest.mark.skipif(
-    not sklearn_avail, reason="sklearn (optional dependency) not available"
-)
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-@pytest.mark.unit
-def test_optimal_clusters_logger_message2(sample_data, caplog):
-    kmin = 9
-    kmax = 14
-    with caplog.at_level(idaeslog.WARNING):
-        daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-        get_optimal_num_clusters(daily_data, kmin=kmin, kmax=kmax, seed=20)
-        assert (
-            f"Optimal number of clusters is close to kmax: {kmax}. Consider increasing kmax."
-            in caplog.text
-        )
-
-
-@pytest.mark.skipif(
-    not sklearn_avail, reason="sklearn (optional dependency) not available"
-)
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-@pytest.mark.unit
-def test_optimal_clusters_logger_message3(sample_data):
-    kmin = 11
-    kmax = 12
-    with pytest.raises(
-        ValueError,
-        match="Could not find elbow point for the given kmin, kmax. "
-        "Consider increasing the range of kmin, kmax.",
-    ):
-
-        daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-        get_optimal_num_clusters(daily_data, kmin=kmin, kmax=kmax, seed=20)
+    assert isinstance(daily_data, pd.DataFrame)
+    assert daily_data.shape == (2, 3)
 
 
 @pytest.mark.unit
-@pytest.mark.skipif(
-    not sklearn_avail, reason="sklearn (optional dependency) not available"
-)
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-def test_elbow_plot(sample_data):
+def test_generate_daily_data():
+    """Tests the generate_daily_data function"""
 
-    daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-    n_clusters, inertia_values = get_optimal_num_clusters(
-        daily_data, kmax=30, generate_elbow_plot=True
+    raw_data = list(range(9))
+    daily_data = generate_daily_data(raw_data, horizon_length=3)
+    assert daily_data.shape == (3, 3)
+    assert daily_data.equals(
+        pd.DataFrame({1: [0, 1, 2], 2: [3, 4, 5], 3: [6, 7, 8]}).transpose()
     )
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_cluster_lmp_data(dummy_data):
+    """Tests the cluster_lmp_data function"""
+
+    data_clusters, weights = cluster_lmp_data(
+        raw_data=dummy_data, horizon_length=2, n_clusters=3, seed=42
+    )
+    assert weights == {1: 4, 2: 4, 3: 4}
+    assert data_clusters == {
+        1: {1: 10.5, 2: 0.5},
+        2: {1: 5.5, 2: 10.5},
+        3: {1: 0.5, 2: 0.5},
+    }
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_num_clusters_elbow(dummy_data):
+    """Tests the get_optimal_num_clusters function"""
+    samples = generate_daily_data(dummy_data, horizon_length=2)
+    n_clusters = get_optimal_num_clusters(
+        samples=samples,
+        kmin=2,
+        kmax=7,
+        method="silhouette",
+        generate_elbow_plot=True,
+        seed=20,
+    )
+
+    assert n_clusters == 3
 
     # Test that a figure was created
     assert plt.gcf() is not None
     # Test that axes were created
     assert plt.gca() is not None
     # Test that the plot has data
-    assert plt.gca().has_data()
+    # assert plt.gca().has_data()
 
     plt.close("all")
 
 
+@pytest.mark.unit
 @pytest.mark.skipif(
     not sklearn_avail, reason="sklearn (optional dependency) not available"
 )
-@pytest.mark.skipif(not kneed_avail, reason="kneed (optional dependency) not available")
-@pytest.mark.unit
-def test_cluster_lmp_data(sample_data):
-    daily_data = generate_daily_data(sample_data["BaseCaseTax"], horizon_length=24)
-    n_clusters, inertia_values = get_optimal_num_clusters(daily_data, kmax=30, seed=20)
-    lmp_data, weights = cluster_lmp_data(
-        sample_data["BaseCaseTax"],
-        n_clusters=n_clusters,
-        horizon_length=24,
+def test_optimal_num_clusters_silhouette(dummy_data):
+    """Tests the get_optimal_num_clusters function"""
+    samples = generate_daily_data(dummy_data, horizon_length=2)
+    n_clusters = get_optimal_num_clusters(
+        samples=samples,
+        kmin=2,
+        kmax=7,
+        method="silhouette",
     )
 
-    sum_of_weights = sum(weights.values())
-    assert sum_of_weights == 365
+    assert n_clusters == 3
 
-    assert len(lmp_data) == n_clusters
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_clusters_logger_message1(dummy_data):
+    """Tests the error message associated with kmin >= kmax"""
+
+    samples = generate_daily_data(dummy_data, 2)
+    with pytest.raises(
+        ValueError,
+        match="kmin must be less than kmax, but 15 >= 10",
+    ):
+        get_optimal_num_clusters(
+            samples=samples,
+            kmin=15,
+            kmax=10,
+        )
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_clusters_logger_message2(dummy_data):
+    """Tests the error message associated with kmin < 2"""
+
+    samples = generate_daily_data(dummy_data, 2)
+    with pytest.raises(
+        ValueError,
+        match="kmin must be > 1. Received kmin = 1",
+    ):
+        get_optimal_num_clusters(
+            samples=samples,
+            kmin=1,
+            kmax=10,
+        )
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_clusters_logger_message3(dummy_data):
+    """Tests the error message associated with kmax >= len(samples)"""
+
+    samples = generate_daily_data(dummy_data, 2)
+    with pytest.raises(
+        ValueError,
+        match="kmax must be < len\\(samples\\). Received kmax = 12",
+    ):
+        get_optimal_num_clusters(
+            samples=samples,
+            kmin=2,
+            kmax=12,
+        )
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_clusters_logger_message4(dummy_data):
+    """Tests the error message associated with unrecognized method"""
+
+    samples = generate_daily_data(dummy_data, 2)
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Unrecognized method foo for optimal number of clusters."
+            "\tSupported methods include elbow and silhouette."
+        ),
+    ):
+        get_optimal_num_clusters(
+            samples=samples,
+            kmin=2,
+            kmax=5,
+            method="foo",
+        )
+
+
+@pytest.mark.unit
+@pytest.mark.skipif(
+    not sklearn_avail, reason="sklearn (optional dependency) not available"
+)
+def test_optimal_clusters_logger_message5(dummy_data, caplog):
+    """Tests the logger warning associated with n_clusters close to kmax"""
+
+    samples = generate_daily_data(dummy_data, 2)
+    with caplog.at_level(idaeslog.WARNING):
+        get_optimal_num_clusters(samples, kmin=2, kmax=5, seed=20)
+        assert (
+            "Optimal number of clusters is close to kmax: 5. Consider increasing kmax."
+            in caplog.text
+        )
+
+
+@pytest.mark.unit
+def test_locate_elbow():
+    """Tests the locate_elbow function"""
+    opt_x = locate_elbow([1, 2, 3], [4, 5, 6])
+
+    assert opt_x == 6
