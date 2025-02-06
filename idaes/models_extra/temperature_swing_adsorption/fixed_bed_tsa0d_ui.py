5a085ef4f472008073e8627f10980a6bc6528524
Sheng Pang <shengdevelop@gmail.com>
2025-02-06 10:46:33 -0800

    pylint fix import 3rd party package before idaes.core import

--- idaes/models_extra/temperature_swing_adsorption/fixed_bed_tsa0d_ui.py
+++ idaes/models_extra/temperature_swing_adsorption/fixed_bed_tsa0d_ui.py
@@ -19,11 +19,11 @@ UI exports for 0D Fixed Bed TSA unit model.
 """
 import pytest
 from pyomo.environ import ConcreteModel, SolverFactory, units
+from idaes_flowsheet_processor import api
 from idaes.core import FlowsheetBlock
 from idaes.core.util.model_statistics import degrees_of_freedom
 import idaes.core.util as iutil
 import idaes.logger as idaes_log
-from idaes_flowsheet_processor import api
 
 from idaes.models_extra.temperature_swing_adsorption import (
     FixedBedTSA0D,
