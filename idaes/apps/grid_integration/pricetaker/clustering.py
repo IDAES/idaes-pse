abbd4200f509647f835e3e002a5473037fbf4ce9
MarcusHolly <marcus.holly@keylogic.com>
2025-02-24 23:04:38 -0500

    Clean up code and testing

--- idaes/apps/grid_integration/pricetaker/clustering.py
+++ idaes/apps/grid_integration/pricetaker/clustering.py
@@ -206,16 +206,80 @@ def get_optimal_num_clusters(
         max_index = mean_silhouette.index(max(mean_silhouette))
         n_clusters = k_values[max_index]
 
+    # The implementation below is based on
+    # Ville Satopaa, Jeannie Albrecht, David Irwin, Barath Raghavan
+    # Finding a “Kneedle” in a Haystack:
+    # Detecting Knee Points in System Behavior
+    # https://raghavan.usc.edu/papers/kneedle-simplex11.pdf
+
     elif method == "elbow":
         # Invert inertia values such that plot is concave down and increasing
         inverted_inertia_values = [-y for y in inertia_values]
-        n_clusters = locate_elbow(
-            x=k_values,
-            y=inverted_inertia_values,
-            sensitivity=sensitivity,
-            kmin=kmin,
-            kmax=kmax,
-        )
+        # TODO: See if make_splrep works and generates similar results
+        # Use a smoothing spline that retains the data's original shape
+        spline = splrep(k_values, inverted_inertia_values, k=3)
+        k_smooth = np.linspace(kmin, kmax + 1, 100)
+        inertia_smooth = splev(k_smooth, spline)
+
+        k_norm = _normalize_values(k_smooth)
+        inertia_norm = _normalize_values(inertia_smooth)
+
+        # Compute the set of differences (x, y) -> (x, y-x)
+        inertia_diff = []
+        for i in range(len(inertia_norm)):
+            set_of_differences = inertia_norm[i] - k_norm[i]
+            inertia_diff.append(set_of_differences)
+
+        # Identify local maxima
+        local_maxima = [
+            (k_norm[i], inertia_diff[i])
+            for i in range(1, len(inertia_diff) - 1)
+            if inertia_diff[i] > inertia_diff[i - 1]
+            and inertia_diff[i] > inertia_diff[i + 1]
+        ]
+
+        # Calculate optimal # of clusters based on the # of local maxima
+        threshold_values = []
+        threshold_triggered = False
+        n_clusters = 0
+
+        if len(local_maxima) == 0:
+            n_clusters = 0
+            _logger.warning(
+                "The optimal number of cluster cannot be determined for this dataset."
+            )
+        elif len(local_maxima) == 1:
+            n_clusters_norm = local_maxima[0][0]
+            ind = k_norm.index(n_clusters_norm)
+            n_clusters = k_values[ind]
+        else:
+            n = len(local_maxima)
+            summation = 0
+            for i in range(0, n - 1):
+                summation += local_maxima[i + 1][0] - local_maxima[i][0]
+            # For each local maxima, compute the threshold and determine the index of the current and next local maxima
+            for i in range(0, n - 1):
+                threshold = local_maxima[i][1] - (sensitivity * summation / (n - 1))
+                threshold_values.append(threshold)
+                l_max_index = inertia_diff.index(local_maxima[i][1])
+                next_l_max_index = inertia_diff.index(local_maxima[i + 1][1])
+                for j in range(l_max_index + 1, next_l_max_index):
+                    if inertia_diff[j] < threshold:
+                        threshold_triggered = True
+                        normalized_optimal_n_clusters = local_maxima[i][0]
+                        index = k_norm.index(normalized_optimal_n_clusters)
+                        n_clusters = x[index]
+                if threshold_triggered:
+                    break
+            # If optimal # of clusters cannot be identified, use the first local maxima
+            if not threshold_triggered:
+                normalized_optimal_n_clusters = local_maxima[0][0]
+                index = k_norm.index(normalized_optimal_n_clusters)
+                n_clusters = k_values[index]
+                _logger.warning(
+                    "The number of optimal clusters could not be accurately identified. "
+                    "Consider lowering the sensitivity."
+                )
 
     else:
         raise ValueError(
@@ -239,111 +303,6 @@ def get_optimal_num_clusters(
         plt.title("Elbow Method")
         plt.xlim(kmin, kmax)
         plt.show()
-
-    return n_clusters
-
-
-def locate_elbow(
-    x: list,
-    y: list,
-    sensitivity: int = 1,
-    kmin: int = 2,
-    kmax: int = 30,
-):
-    """
-    Identifies the elbow/knee for the input/output data that is concave down and increasing
-
-    Args:
-        x : list,
-            List of independent variables (i.e. k_values)
-
-        y : list,
-            List of dependent variables (i.e. inertia values)
-
-        sensitivity : int,
-            difficulty of detecting knees, where 0 detects knees the easiest
-
-        kmin : int,
-            Minimum number of clusters
-
-        kmax : int,
-            Maximum number of clusters
-
-    Returns:
-        opt_x : float
-            Optimal x at which curvature changes significantly
-    """
-    # The implementation is based on
-    # Ville Satopaa, Jeannie Albrecht, David Irwin, Barath Raghavan
-    # Finding a “Kneedle” in a Haystack:
-    # Detecting Knee Points in System Behavior
-    # https://raghavan.usc.edu/papers/kneedle-simplex11.pdf
-
-    # TODO: See if make_splrep works and generates similar results
-    # Use a smoothing spline that retains the data's original shape
-    spline = splrep(x, y, k=3)
-    k_smooth = np.linspace(kmin, kmax + 1, 100)
-    inertia_smooth = splev(k_smooth, spline)
-
-    k_norm = _normalize_values(k_smooth)
-    inertia_norm = _normalize_values(inertia_smooth)
-
-    # Compute the set of differences (x, y) -> (x, y-x)
-    inertia_diff = []
-    for i in range(len(inertia_norm)):
-        set_of_differences = inertia_norm[i] - k_norm[i]
-        inertia_diff.append(set_of_differences)
-
-    # Identify local maxima
-    local_maxima = [
-        (k_norm[i], inertia_diff[i])
-        for i in range(1, len(inertia_diff) - 1)
-        if inertia_diff[i] > inertia_diff[i - 1]
-        and inertia_diff[i] > inertia_diff[i + 1]
-    ]
-
-    # Calculate optimal # of clusters based on the # of local maxima
-    threshold_values = []
-    threshold_triggered = False
-    n_clusters = 0
-
-    if len(local_maxima) == 0:
-        n_clusters = 0
-        _logger.warning(
-            "The optimal number of cluster cannot be determined for this dataset."
-        )
-    elif len(local_maxima) == 1:
-        n_clusters_norm = local_maxima[0][0]
-        ind = k_norm.index(n_clusters_norm)
-        n_clusters = x[ind]
-    else:
-        n = len(local_maxima)
-        summation = 0
-        for i in range(0, n - 1):
-            summation += local_maxima[i + 1][0] - local_maxima[i][0]
-        # For each local maxima, compute the threshold and determine the index of the current and next local maxima
-        for i in range(0, n - 1):
-            threshold = local_maxima[i][1] - (sensitivity * summation / (n - 1))
-            threshold_values.append(threshold)
-            l_max_index = inertia_diff.index(local_maxima[i][1])
-            next_l_max_index = inertia_diff.index(local_maxima[i + 1][1])
-            for j in range(l_max_index + 1, next_l_max_index):
-                if inertia_diff[j] < threshold:
-                    threshold_triggered = True
-                    normalized_optimal_n_clusters = local_maxima[i][0]
-                    index = k_norm.index(normalized_optimal_n_clusters)
-                    n_clusters = x[index]
-            if threshold_triggered:
-                break
-        # If optimal # of clusters cannot be identified, use the first local maxima
-        if not threshold_triggered:
-            normalized_optimal_n_clusters = local_maxima[0][0]
-            index = k_norm.index(normalized_optimal_n_clusters)
-            n_clusters = x[index]
-            _logger.warning(
-                "The number of optimal clusters could not be accurately identified. "
-                "Consider lowering the sensitivity."
-            )
 
     return n_clusters
 
