e6401fafaf5d60a7796988e28034897158c02095
Ali Zaidi <41302741+azaidi06@users.noreply.github.com>
2025-02-25 13:50:26 -0500

    2.8.0rc1

--- setup.py
+++ setup.py
@@ -38,8 +38,7 @@ class ExtraDependencies:
 
     ui = [
         # FIXME this must be changed to the PyPI distribution for the release
-        # "idaes-ui",
-        "idaes-ui @ git+https://github.com/IDAES/idaes-ui@main",
+        "idaes-ui",
     ]
     omlt = [
         "omlt==1.1",  # fix the version for now as package evolves
