243423a3496d5784c937afced2d113ab2c2dd332
Ali Zaidi <41302741+azaidi06@users.noreply.github.com>
2025-03-06 11:26:26 -0500

    2.8.0rc2

--- idaes/ver.py
+++ idaes/ver.py
@@ -184,7 +184,7 @@ except NameError:  # eg, if invoked from setup.py
     pass
 
 #: Package's version as an object
-package_version = Version(2, 8, 0, "candidate", 1, gh)
+package_version = Version(2, 8, 0, "candidate", 2, gh)
 
 #: Package's version as a simple string
 __version__ = str(package_version)
