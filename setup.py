4270d836ac9a0c22f185de317019f119e90b0f39
Bethany Nicholson <blnicho@users.noreply.github.com>
2025-03-06 09:16:23 -0700

    Update Pyomo to the 6.9.1 release (#1587)
    * Bumping the Pyomo tag to check for issues
    
    * Update setup.py
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    
    ---------
    
    Co-authored-by: Ludovico Bianchi <lbianchi@lbl.gov>
    (cherry picked from commit 6cea367e53f7cae5615eb9f627708f35379ce1e3)

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ kwargs = dict(
     # Put abstract (non-versioned) deps here.
     # Concrete dependencies go in requirements[-dev].txt
     install_requires=[
-        "pyomo >= 6.9.0",
+        "pyomo >=6.9.1",
         "pint >= 0.24.1",  # required to use Pyomo units. Pint 0.24.1 needed for Python 3.9 support
         "networkx",  # required to use Pyomo network
         "numpy>=1,<3",
