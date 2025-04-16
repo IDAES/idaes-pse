9ab4b4877a467411930216248a81ec5dafcf6fb8
Bethany Nicholson <blnicho@users.noreply.github.com>
2025-04-16 09:07:40 -0600

    Bumping Pyomo tag ahead of the 6.9.2 release

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ kwargs = dict(
     # Put abstract (non-versioned) deps here.
     # Concrete dependencies go in requirements[-dev].txt
     install_requires=[
-        "pyomo @ https://github.com/IDAES/pyomo/archive/6.9.1.idaes.2025.03.20.zip",
+        "pyomo @ https://github.com/IDAES/pyomo/archive/6.9.1.idaes.2025.04.16.zip",
         "pint >= 0.24.1",  # required to use Pyomo units. Pint 0.24.1 needed for Python 3.9 support
         "networkx",  # required to use Pyomo network
         "numpy>=1,<3",
