1cdb864202593829cf526abf9772ca0fc1c064b8
Dan Gunter <dkgunter@lbl.gov>
2026-03-15 10:17:52 -0700

    more stuff

--- idaes/core/util/compute_diagnostics.py
+++ idaes/core/util/compute_diagnostics.py
@@ -37,6 +37,7 @@ __author__ = "Dan Gunter (LBNL)"
 # stdlib
 from enum import StrEnum
 from math import log
+from typing import Callable, TypeVar
 
 # third party
 from pydantic import BaseModel, Field
@@ -47,7 +48,7 @@ from pyomo.environ import (
     # check_optimal_termination,
     # ComponentMap,
     # ConcreteModel,
-    # Constraint,
+    Constraint,
     # Expression,
     # Objective,
     # Param,
@@ -84,10 +85,12 @@ from idaes.core.util.model_statistics import (
     # unfixed_greybox_variables,
     # greybox_variables,
     # large_residuals_set,
+    large_residuals_set,
     variables_near_bounds_set,
 )
 from .model_diagnostics import (
     DiagnosticsToolbox,
+    _extreme_jacobian_rows,
     _extreme_jacobian_columns,
     _var_in_block,
     _vars_fixed_to_zero,
@@ -114,20 +117,32 @@ def _block_list_names(blocks) -> list[str]:
     return b_items
 
 
-class VariableListData(BaseModel):
-    """List of variables that satisfy some condition."""
-
-    #: short description of condition these variables satisfy
+class ComponentListData(BaseModel):
+    #: flag for whether this has any data
+    empty: bool = True
+    #: short description of condition these components satisfy
     tag: str
-    #: longer description of condition these variables satisfy
+    #: longer description of condition these components satisfy
     description: str
-    #: names of variables
-    variables: list[str] = Field(default_factory=list)
-    #: optional values of variables
+    #: names of components
+    names: list[str] = Field(default_factory=list)
+    #: optional values of components
     values: list[float | None] = Field(default_factory=list)
     value_format: str = ".6E"
-    #: optional descriptive details for variables
+    #: optional bounds
+    bounds: dict = Field(default_factory=dict)
+    bounds_desc: str | None = None
+    #: optional descriptive details for components
     details: list[str] = Field(default_factory=list)
+
+
+class VariableListData(ComponentListData):
+    #: range of each variable (optional)
+    ranges: list[tuple[float | None, float | None]] = Field(default_factory=list)
+
+
+class ComponentList(BaseModel):
+    components: list[ComponentListData]
 
 
 class VCSet(BaseModel):
@@ -184,6 +199,13 @@ class StructuralIssuesData(BaseModel):
     cautions: StructuralCautionsData
 
 
+# class NumericalIssuesData(BaseModel):
+#     """Numerical issues: warnings and cautions."""
+
+#     warnings: NumericalWarningsData
+#     cautions: NumericalCautionsData
+
+
 # -------------------------------------------------------------------------------
 # Interface
 # -------------------------------------------------------------------------------
@@ -201,6 +223,14 @@ class VariableCondition(StrEnum):
     extreme_jacobians = "corresponding to Jacobian columns with extreme norms"
 
 
+class ConstraintCondition(StrEnum):
+    large_residuals = "have residuals greater than specified tolerance"
+    no_free_variables = "do not have any free variables"
+    canceling_terms = "have additive terms which potentially cancel each other"
+    mismatched_terms = "have additive terms of different magnitude"
+    extreme_jacobians = "corresponding to Jacobian rows with extreme L2 norms"
+
+
 class DiagnosticsData:
     """Interface to get diagnostics data"""
 
@@ -210,37 +240,55 @@ class DiagnosticsData:
         self._toolbox = toolbox
 
     def variables(
-        self, conditions: list[VariableCondition] | None = None, dump=True
-    ) -> list[VariableListData] | list[dict]:
-        """Compute the list of variables meeting some condition and return as Pydantic object.
+        self, conditions: list[VariableCondition] | None = None
+    ) -> ComponentList | list[dict]:
+        """Compute the list of variables meeting some condition(s).
 
         Args:
             conditions: Zero or more conditions. If zero, look for all
                   conditions. If one, just return one. If multiple, return one per condition.
-            dump: If True (the default), return dicts instead of Pydantic objects
 
         Returns:
-            Selected variables and associated metadata, as a list of length 1 or greater
+            Selected variables and associated metadata
         """
-        results = []
-        if conditions is None or len(conditions) == 0:
-            conditions = [v for v in VariableCondition]
+        return self._get_components_for_conditions(
+            conditions, VariableCondition, self._get_variables_for_condition
+        )
+
+    def constraints(
+        self, conditions: list[ConstraintCondition] | None = None
+    ) -> ComponentList | list[dict]:
+        """Compute the list of constraints meeting some condition(s).
+
+        Args:
+            conditions: Zero or more conditions. If zero, look for all
+                  conditions. If one, just return one. If multiple, return one per condition.
+
+        Returns:
+            Selected constraints and associated metadata
+        """
+        return self._get_components_for_conditions(
+            conditions, ConstraintCondition, self._get_constraints_for_condition
+        )
+
+    def _get_components_for_conditions(self, conditions, cond_enum, getter):
+        if not conditions:
+            conditions = list(cond_enum)
+        components = []
         for cond in conditions:
-            results.append(self._get_variables_for_condition(cond))
-        if dump:
-            return [x.model_dump() for x in results]
-        else:
-            return results
+            item = getter(cond)
+            item.empty = not bool(item.names)
+            components.append(item)
+        return ComponentList(components=components)
 
     def structural_issues(
-        self, evaluation_errors=True, unit_consistency=True, dump=True
-    ) -> StructuralIssuesData | dict:
+        self, evaluation_errors=True, unit_consistency=True
+    ) -> StructuralIssuesData:
         """Compute structural warnings and cautions.
 
         Args:
             evaluation_errors: Include potential evaluation errors
             unit_consistency: Include unit consistency checks
-            dump: If True (the default), return dicts instead of Pydantic objects
 
         Returns:
             Found structural issues
@@ -294,100 +342,186 @@ class DiagnosticsData:
             if uv_free:
                 c.unused_vars_free = _block_list_names(uv_free)
 
-        result = StructuralIssuesData(warnings=w, cautions=c)
-        return result.model_dump() if dump else result
+        return StructuralIssuesData(warnings=w, cautions=c)
 
-    def _get_variables_for_condition(self, cond: VariableCondition) -> VariableListData:
+    def _get_variables_for_condition(
+        self, cond: VariableCondition
+    ) -> ComponentListData:
         tbx = self._toolbox  # alias
-        kwargs = {}  # additional kw for VariableListData
+        kwargs = {}  # additional keywords for return value constructor
         desc = str(cond)  # default description
         details, values = None, None
+        bounds, bounds_desc = {}, None
+        names, ranges, values, details = [], [], [], []
+
+        def _set_variables(
+            it,
+            nm=names,
+            rg=ranges,
+            vl=values,
+            dt=details,
+            nm_func=None,
+            vl_func=None,
+            dt_func=None,
+            rg_func=None,
+        ):
+            for v in it:
+                nm.append(v.name if nm_func is None else nm_func(v))
+                vl.append(v.value if vl_func is None else vl_func(v))
+                dt.append("" if dt_func is None else dt_func(v))
+                rg.append((v.lb, v.ub) if rg_func is None else rg_func(v))
+
         if cond == VariableCondition.external:
-            cvars = variables_in_activated_constraints_set(tbx._model)
-            variables = [v.name for v in cvars if not _var_in_block(v, tbx._model)]
+            _set_variables(variables_in_activated_constraints_set(tbx._model))
         elif cond == VariableCondition.unused:
-            variables = [
-                str(v) for v in variables_not_in_activated_constraints_set(tbx._model)
-            ]
+            _set_variables(variables_not_in_activated_constraints_set(tbx._model))
         elif cond == VariableCondition.fixed_to_zero:
-            variables = [str(v) for v in _vars_fixed_to_zero(tbx._model)]
+            _set_variables(_vars_fixed_to_zero(tbx._model))
         elif cond == VariableCondition.at_or_outside_bounds:
-            variables, details = [], []
-            for v in _vars_violating_bounds(
-                tbx._model,
-                tolerance=tbx.config.variable_bounds_violation_tolerance,
-            ):
-                variables.append(f"{v.name} ({'fixed' if v.fixed else 'free'})")
-                details.append(f"bounds={v.bounds}")
+            tol = tbx.config.variable_bounds_violation_tolerance
+            _set_variables(
+                _vars_violating_bounds(tbx._model, tolerance=tol),
+                nm_func=lambda v: f"{v.name} ({'fixed' if v.fixed else 'free'})",
+            )
+            bounds = {"tol": tol}
+            bounds_desc = "value range"
         elif cond == VariableCondition.with_none_value:
-            variables = [
-                v.name
-                for v in variables_with_none_value_in_activated_equalities_set(
-                    tbx._model
-                )
-            ]
+            _set_variables(
+                variables_with_none_value_in_activated_equalities_set(tbx._model)
+            )
         elif cond == VariableCondition.value_near_zero:
             variables, values = [], []
-            for v in _vars_near_zero(
-                tbx._model, tbx.config.variable_zero_value_tolerance
-            ):
-                variables.append(v.name)
-                values.append(value(v))
+            _set_variables(
+                _vars_near_zero(tbx._model, tbx.config.variable_zero_value_tolerance)
+            )
+            bounds = {"zero": tbx.config.variable_zero_value_tolerance}
+            bounds_desc = "zero value"
         elif cond == VariableCondition.extreme_values:
-            desc += f" (<{tbx.config.variable_small_value_tolerance:.1E} or "
-            desc += f"> {tbx.config.variable_large_value_tolerance:.1E})"
-            variables, values = [], []
-            for v in _vars_with_extreme_values(
-                model=tbx._model,
-                large=tbx.config.variable_large_value_tolerance,
-                small=tbx.config.variable_small_value_tolerance,
-                zero=tbx.config.variable_zero_value_tolerance,
-            ):
-                variables.append(v.name)
-                values.append(value(v))
+            t_small, t_large, t_zero = (
+                tbx.config.variable_small_value_tolerance,
+                tbx.config.variable_large_value_tolerance,
+                tbx.config.variable_zero_value_tolerance,
+            )
+            _set_variables(
+                _vars_with_extreme_values(
+                    model=tbx._model, large=t_large, small=t_small, zero=t_zero
+                )
+            )
+            bounds = {"small": t_small, "large": t_large, "zero": t_zero}
+            bonds_desc = "extreme values"
         elif cond == VariableCondition.near_bounds:
-            desc += f" (abs={tbx.config.variable_bounds_absolute_tolerance:.1E}, "
-            desc += f"rel={tbx.config.variable_bounds_relative_tolerance:.1E})"
-            variables, values = [], []
-            for v in variables_near_bounds_set(
-                tbx._model,
-                abs_tol=tbx.config.variable_bounds_absolute_tolerance,
-                rel_tol=tbx.config.variable_bounds_relative_tolerance,
-            ):
-                variables.append(v.name)
-                values.append(value(v))
+            t_abs = tbx.config.variable_bounds_absolute_tolerance
+            t_rel = tbx.config.variable_bounds_relative_tolerance
+            _set_variables(
+                variables_near_bounds_set(tbx._model, abs_tol=t_abs, rel_tol=t_rel)
+            )
+            bounds = {"abs": t_abs, "rel": t_rel}
+            bounds_desc = "near bounds"
         elif cond == VariableCondition.extreme_jacobians:
             tbx._verify_active_variables_initialized()
-            desc += f" (<{tbx.config.jacobian_small_value_caution:.1E} or "
-            desc += f">{tbx.config.jacobian_large_value_caution:.1E})"
+            t_small, t_large = (
+                tbx.config.jacobian_small_value_caution,
+                tbx.config.jacobian_large_value_caution,
+            )
             # compute the extreme jacobians
             jac, nlp = get_jacobian(tbx._model)
             xjc = _extreme_jacobian_columns(
-                jac=jac,
-                nlp=nlp,
-                large=tbx.config.jacobian_large_value_caution,
-                small=tbx.config.jacobian_small_value_caution,
+                jac=jac, nlp=nlp, large=t_large, small=t_small
             )
             xjc.sort(key=lambda i: abs(log(i[0])), reverse=True)
-            # place in output object
-            variables, values = [], []
-            for v in xjc:
-                variables.append(v[1].name)
-                values.append(v[0])
+            # each value is (variable, extreme-value)
+            _set_variables(
+                xjc,
+                nm_func=lambda v: str(v[1]),
+                vl_func=lambda v: v[0],
+                rg_func=lambda v: (None, None),
+            )
             kwargs["value_format"] = ".3E"
-
-        # Fill out optional parts, if missing
-        if details is None:
-            details = [""] * len(variables)
-        if values is None:
-            values = [None] * len(variables)
+            bounds = {"small": t_small, "large": t_large}
+            bounds_desc = "extreme jacobians"
 
         # return as Pydantic data object
         return VariableListData(
             tag=cond.value,
             description=desc,
-            variables=variables,
+            names=names,
             details=details,
             values=values,
+            bounds=bounds,
+            bounds_desc=bounds_desc,
+            ranges=ranges,
+            **kwargs,
+        )
+
+    def _get_constraints_for_condition(
+        self, cond: ConstraintCondition
+    ) -> ComponentListData:
+        tbx = self._toolbox  # alias
+        kwargs = {}  # additional keywords for return value constructor
+        desc = str(cond)  # default description
+        details, values = None, None
+        bounds, bounds_desc = {}, None
+        names, values, details = [], [], []
+
+        if cond == ConstraintCondition.large_residuals:
+            residuals = large_residuals_set(
+                tbx._model,
+                tol=tbx.config.constraint_residual_tolerance,
+                return_residual_values=True,
+            )
+            for c, residual in residuals.items():
+                names.append(c.name)
+                values.append(residual)
+            kwargs["value_format"] = ".3E"
+            bounds = {"tol": tbx.config.constraint_residual_tolerance}
+            bounds_desc = "residual tolerance"
+        elif cond == ConstraintCondition.no_free_variables:
+            tbx._verify_active_variables_initialized()
+            _, _, names = tbx._collect_constraint_mismatches()
+        elif cond == ConstraintCondition.canceling_terms:
+            tbx._verify_active_variables_initialized()
+            _, cancellation, _ = tbx._collect_constraint_mismatches()
+            for item in cancellation:
+                name, _, detail = item.partition(": ")
+                names.append(name)
+                details.append(detail)
+        elif cond == ConstraintCondition.mismatched_terms:
+            tbx._verify_active_variables_initialized()
+            mismatch, _, _ = tbx._collect_constraint_mismatches()
+            for item in mismatch:
+                name, _, detail = item.partition(": ")
+                names.append(name)
+                details.append(detail)
+        elif cond == ConstraintCondition.extreme_jacobians:
+            tbx._verify_active_variables_initialized()
+            t_small, t_large = (
+                tbx.config.jacobian_small_value_caution,
+                tbx.config.jacobian_large_value_caution,
+            )
+            jac, nlp = get_jacobian(tbx._model)
+            xjr = _extreme_jacobian_rows(jac=jac, nlp=nlp, large=t_large, small=t_small)
+            xjr.sort(key=lambda i: abs(log(i[0])), reverse=True)
+            for c in xjr:
+                names.append(c[1].name)
+                values.append(c[0])
+            kwargs["value_format"] = ".3E"
+            bounds = {"small": t_small, "large": t_large}
+            bounds_desc = "extreme jacobians"
+        else:
+            raise ValueError(f"Unhandled constraint condition: {cond}")
+
+        if not details:
+            details = [""] * len(names)
+        if not values:
+            values = [None] * len(names)
+
+        return ComponentListData(
+            tag=cond.value,
+            description=desc,
+            names=names,
+            details=details,
+            values=values,
+            bounds=bounds,
+            bounds_desc=bounds_desc,
             **kwargs,
         )
